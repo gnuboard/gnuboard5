321599792c6864544148305be74d958a206d106b
whitedot <whitedot@naver.com>
2012-11-14 14:43:45 +0900

    관리자:메일발송목록,메일발송추가

--- adm/mail_form.php
+++ adm/mail_form.php
@@ -12,7 +12,7 @@ if ($w == 'u') {
     $html_title .= '수정';
     $readonly = ' readonly';
 
-    $sql = " select * from {$g4['mail_table']} where ma_id = '$ma_id' ";
+    $sql = " select * from {$g4['mail_table']} where ma_id = '{$ma_id}' ";
     $ma = sql_fetch($sql);
     if (!$ma['ma_id'])
         alert('등록된 자료가 없습니다.');
@@ -24,40 +24,31 @@ $g4['title'] = $html_title;
 include_once('./admin.head.php');
 ?>
 
-<form id="fmailform" name="fmailform" method=post action="./mail_update.php" onsubmit="return fmailform_check(this);">
-<input type="hidden" id="w" name="w"     value='<?=$w?>'>
-<input type="hidden" id="ma_id" name="ma_id" value='<?=$ma['ma_id']?>'>
-<input type="hidden" id="token" name="token" value='<?=$token?>'>
-<table cellpadding=0 cellspacing=0 width=100%>
-<colgroup width=20% class='col1 pad1 bold right'>
-<colgroup width=80% class='col2 pad2'>
+<form id="fmailform" name="fmailform" method="post" action="./mail_update.php" onsubmit="return fmailform_check(this);">
+<input type="hidden" id="w" name="w" value="<?=$w?>">
+<input type="hidden" id="ma_id" name="ma_id" value="<?=$ma['ma_id']?>">
+<input type="hidden" id="token" name="token" value="<?=$token?>">
+<table>
 <tr>
-    <td colspan=2 class=title align=left><img src='<?=$g4['admin_path']?>/img/icon_title.gif'> <?=$html_title?></td>
-</tr>
-<tr><td colspan=2 class='line1'></td></tr>
-<tr class='ht'>
-    <td>메일 제목</td>
-    <td><input type="text" class='ed w99' id="ma_subject" name="ma_subject" value='<?=$ma['ma_subject']?>' required itemname='메일 제목'></td>
+    <th scope="row"><label for="ma_subject">메일 제목</label></th>
+    <td><input type="text" id="ma_subject" name="ma_subject" value="<?=$ma['ma_subject']?>" required></td>
 </tr>
 <tr>
-    <td>메일 내용</td>
-    <td class=lh>
+    <th scope="row"><label for="ma_content">메일 내용</label></th>
+    <td>
+        <?=help('{이름} , {별명} , {회원아이디} , {이메일} , {생일} 처럼 HTML 코드에 삽입하면 해당 내용에 맞게 변환하여 메일 발송합니다.')?>
         <?=textarea_size('ma_content')?>
-        <textarea id=ma_content id="ma_content" name="ma_content" rows=20 class='ed w99'  required itemname='메일 내용'><?=$ma['ma_content']?></textarea>
-        <br>{이름} , {별명} , {회원아이디} , {이메일} , {생일}
-        <br>위와 같이 HTML 코드에 삽입하면 해당 내용에 맞게 변환하여 메일 발송합니다.
+        <textarea id="ma_content" name="ma_content" rows="20" required><?=$ma['ma_content']?></textarea>
     </td>
 </tr>
-<tr><td colspan=2 class='line1'></td></tr>
 </table>
 
-<p align=center>
-    <input type=submit class=btn1 accesskey='s' value='  확  인  '>
+<div class="btn_confirm">
+    <input type="submit" accesskey="s" value="확인">
+</div>
 </form>
 
-
-
-<script type="text/javascript">
+<script>
 function fmailform_check(f)
 {
     errmsg = "";
--- adm/mail_list.php
+++ adm/mail_list.php
@@ -22,52 +22,53 @@ include_once('./admin.head.php');
 $colspan = 6;
 ?>
 
-<table width=100%>
+<a href="./mail_form.php" id="mail_add">메일내용추가</a>
+
+건수 : <?=$total_count ?>
+
+<table>
+<caption>
+    등록된 메일내용 목록
+    <p>
+        <strong>테스트</strong>를 클릭하시면 등록된 최고관리자님에게 테스트메일 발송<br>
+        <strong>보내기</strong>를 클릭하시면 해당 메일을 발송할 회원님 선택페이지로 이동
+    </p>
+</caption>
+<thead>
 <tr>
-    <td width=20%>&nbsp;</td>
-    <td width=60% align=center>&nbsp;</td>
-    <td width=20% align=right>건수 : <?=$total_count ?>&nbsp;</td>
+    <th scope="col">ID</th>
+    <th scope="col">제목</th>
+    <th scope="col">작성일시</th>
+    <th scope="col">테스트</th>
+    <th scope="col">보내기</th>
+    <th scope="col">관리</th>
 </tr>
-</table>
-
-
-<table cellpadding=0 cellspacing=0 width=100%>
-<tr><td colspan="<?=$colspan?>" class="line1"></td></tr>
-<tr class="bgcol1 bold col1 ht center">
-    <td width=40>ID</td>
-    <td width="">제목</td>
-    <td width=120>작성일시</td>
-    <td width=50>테스트</td>
-    <td width=50>보내기</td>
-    <td width=80><a href="./mail_form.php"><img src="<?=$g4['admin_path']?>/img/icon_insert.gif" border=0></a></td>
-</tr>
-<tr><td colspan="<?=$colspan?>" class="line2"></td></tr>
-
+</thead>
+<tbody>
 <?
 for ($i=0; $row=mysql_fetch_array($result); $i++) {
-    $s_mod = icon('수정', './mail_form.php?w=u&ma_id='.$row['ma_id']);
-    //$s_del = icon('삭제', 'javascript:del("./mail_update.php?w=d&ma_id=$row[ma_id]");');
-    $s_del = '<a href="javascript:post_delete(\"mail_update.php\", '.$row['ma_id'].');"><img src="img/icon_delete.gif" border=0 title="삭제" align="absmiddle"></a>';
-    $s_vie = icon('보기', './mail_preview.php?ma_id='.$row['ma_id'], '_blank');
+    $s_mod = '<a href="./mail_form.php?w=u&ma_id='.$row['ma_id'].'">수정</a>';
+    $s_del = '<a href="javascript:post_delete(\'mail_update.php\', '.$row['ma_id'].');">삭제</a>';
+    $s_vie = '<a href="./mail_preview.php?ma_id='.$row['ma_id'].'" target="_blank">미리보기</a>';
 
     $num = number_format($total_count - ($page - 1) * $config['cf_page_rows'] - $i);
-
-    $list = $i%2;
-    echo "
-    <tr class=\"list{$list} col1 ht center\">
-        <td>$num</td>
-        <td align=left>{$row['ma_subject']}</td>
-        <td>{$row['ma_time']}</td>
-        <td><a href=\"./mail_test.php?ma_id=$row[ma_id]\">테스트</a></td>
-        <td><a href=\"./mail_select_form.php?ma_id={$row['ma_id']}\">보내기</a></td>
-        <td>$s_mod $s_del $s_vie</td>
-    </tr>";
-}
-
-if (!$i)
-    echo "<tr><td colspan=\"$colspan\" height=100 align=center bgcolor=\"#FFFFFF\">자료가 없습니다.</td></tr>";
 ?>
-<tr><td colspan="<?=$colspan?>" class="line2"></td></tr>
+
+<tr>
+    <td><?=$num?></td>
+    <td><?=$row['ma_subject']?></td>
+    <td><?=$row['ma_time']?></td>
+    <td><a href="./mail_test.php?ma_id=<?=$row[ma_id]?>">테스트</a></td>
+    <td><a href="./mail_select_form.php?ma_id=<?=$row[ma_id]?>">보내기</a></td>
+    <td><?=$s_mod?> <?=$s_del?> <?=$s_vie?></td>
+</tr>
+
+<?
+}
+if (!$i)
+    echo '<tr><td colspan="'.$colspan.'" class="empty_table">자료가 없습니다.</td></tr>';
+?>
+</tbody>
 </table>
 
 <script>
@@ -76,22 +77,22 @@ function post_delete(action_url, val)
 {
 	var f = document.fpost;
 
-	if(confirm('한번 삭제한 자료는 복구할 방법이 없습니다.\n\n정말 삭제하시겠습니까?')) {
+	if(confirm('한번 삭제한 자료는 복구할 방법이 없습니다. 정말 삭제하시겠습니까?')) {
         f.ma_id.value = val;
-		f.action      = action_url;
+		f.action = action_url;
 		f.submit();
 	}
 }
 </script>
 
-<form id='fpost' id="fpost" name="fpost" method="post">
-<input type="hidden" id='sst' id="sst" name="sst"  value="<?=$sst?>">
-<input type="hidden" id='sod' id="sod" name="sod"  value="<?=$sod?>">
-<input type="hidden" id='sfl' id="sfl" name="sfl"  value="<?=$sfl?>">
-<input type="hidden" id='stx' id="stx" name="stx"  value="<?=$stx?>">
-<input type="hidden" id='page' id="page" name="page" value="<?=$page?>">
-<input type="hidden" id='w' id="w" name="w"    value='d'>
-<input type="hidden" id='ma_id' id="ma_id" name="ma_id">
+<form id="fpost" id="fpost" name="fpost" method="post">
+<input type="hidden" name="sst" value="<?=$sst?>">
+<input type="hidden" name="sod" value="<?=$sod?>">
+<input type="hidden" name="sfl" value="<?=$sfl?>">
+<input type="hidden" name="stx" value="<?=$stx?>">
+<input type="hidden" name="page" value="<?=$page?>">
+<input type="hidden" name="w" value='d'>
+<input type="hidden" name="ma_id">
 </form>
 
 <?
