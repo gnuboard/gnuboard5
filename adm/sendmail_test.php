e543c86f043ccfd4fa3184dc451112365174e202
whitedot <whitedot@naver.com>
2012-11-15 16:05:00 +0900

    마크업:관리자>메일테스트

--- adm/sendmail_test.php
+++ adm/sendmail_test.php
@@ -21,29 +21,34 @@ if ($mail) {
     $email = explode(',', $mail);
     for ($i=0; $i<count($email); $i++)
         mailer($from_name, $from_email, trim($email[$i]), '[메일검사] 제목', '<span style="font-size:9pt;">[메일검사] 내용<p>이 내용이 제대로 보인다면 보내는 메일 서버에는 이상이 없는것입니다.<p>'.date("Y-m-d H:i:s").'<p>이 메일 주소로는 회신되지 않습니다.</span>', 1);
+?>
+    <script>
+        alert('<?=$mail?>(으)로 메일을 발송 하였습니다.\\n\\n해당 주소로 메일이 왔는지 확인하여 주십시오.\\n\\n메일이 오지 않는다면 프로그램의 오류가 아닌 메일 서버(sendmail)의 오류일 가능성이 있습니다.\\n\\n이런 경우에는 웹 서버관리자에게 문의하여 주십시오.');
+    </script>
 
-    echo <<<HEREDOC
-    <SCRIPT type="text/javascript">
-        alert($mail.' (으)로 메일을 발송 하였습니다.\\n\\n해당 주소로 메일이 왔는지 확인하여 주십시오.\\n\\n메일이 오지 않는다면 프로그램의 오류가 아닌 메일 서버(sendmail)의 오류일 가능성이 있습니다.\\n\\n이런 경우에는 웹 서버관리자에게 문의하여 주십시오.');
-    </SCRIPT>
-HEREDOC;
+<?
 }
 
 $token = get_token();
 ?>
 
-<img src='<?=$g4['admin_path']?>/img/icon_title.gif'> <span class=title><?=$g4['title']?></span>
 <p>
 
-<form id="fsendmailtest" name="fsendmailtest" method=post>
+<form id="fsendmailtest" name="fsendmailtest" method="post">
 <input type="hidden" id="token" name="token" value='<?=$token?>'>
-<p>고객님들께서 메일이 오지 않는다고 하면 사용하는 메뉴입니다.
-<p>입력한 메일주소로 테스트 메일을 발송합니다.
-<p>만약 [메일검사] 라는 내용으로 메일이 도착하지 않는다면 보내는 메일서버와 받는 메일 서버중 문제가 발생했을 가능성이 있습니다.
-<p>메일을 보냈는데도 도착하지 않는다면 다른 여러곳으로도 메일을 발송하여 주십시오.
-<p>여러곳으로 메일을 발송하시려면 , 로 메일을 구분하십시오.
-<p>받는 메일주소 : <input type="text" id="mail" name="mail" size=40 required itemid="E-mail" name="E-mail" value="<?=$member['mb_email']?>">
-<input type=submit value="  발  송  " class=btn1>
+<p>
+    <span></span>
+    고객님들께서 메일이 오지 않는다고 하면 사용하는 메뉴입니다.<br>
+    입력한 메일주소로 테스트 메일을 발송합니다.<br>
+    만약 [메일검사] 라는 내용으로 메일이 도착하지 않는다면 보내는 메일서버와 받는 메일 서버중 문제가 발생했을 가능성이 있습니다.<br>
+    메일을 보냈는데도 도착하지 않는다면 다른 여러곳으로도 메일을 발송하여 주십시오.<br>
+    여러곳으로 메일을 발송하시려면 , 로 메일을 구분하십시오.
+</p>
+<fieldset>
+    <legend>테스트메일 발송</legend>
+    받는 메일주소 <input type="text" id="mail" name="mail" required value="<?=$member['mb_email']?>">
+    <input type="submit" value="발송">
+</fieldset>
 </form>
 
 <?
