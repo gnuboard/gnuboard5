aa5af847d9e9ef46a5c251640a7383cf3d635f29
whitedot <whitedot@naver.com>
2012-11-15 15:49:29 +0900

    마크업:관리자>인기검색어 관리, 순위

--- adm/popular_list.php
+++ adm/popular_list.php
@@ -21,13 +21,13 @@ if ($stx) {
     $sql_search .= " and ( ";
     switch ($sfl) {
         case "pp_word" :
-            $sql_search .= " ($sfl like '$stx%') ";
+            $sql_search .= " ({$sfl} like '{$stx}%') ";
             break;
         case "pp_date" :
-            $sql_search .= " ($sfl = '$stx') ";
+            $sql_search .= " ({$sfl} = '{$stx}') ";
             break;
         default :
-            $sql_search .= " ($sfl like '%$stx%') ";
+            $sql_search .= " ({$sfl} like '%{$stx}%') ";
             break;
     }
     $sql_search .= " ) ";
@@ -37,14 +37,14 @@ if (!$sst) {
     $sst  = "pp_id";
     $sod = "desc";
 }
-$sql_order = " order by $sst $sod ";
+$sql_order = " order by {$sst} {$sod} ";
 
 $sql = " select count(*) as cnt
-         $sql_common
-         $sql_search
-         $sql_order ";
+            {$sql_common}
+            {$sql_search}
+            {$sql_order} ";
 $row = sql_fetch($sql);
-$total_count = $row['cnt'];
+$total_count = $row[cnt];
 
 $rows = $config['cf_page_rows'];
 $total_page  = ceil($total_count / $rows);  // 전체 페이지 계산
@@ -52,10 +52,10 @@ if ($page == '') { $page = 1; } // 페이지가 없으면 첫 페이지 (1 페�
 $from_record = ($page - 1) * $rows; // 시작 열을 구함
 
 $sql = " select *
-          $sql_common
-          $sql_search
-          $sql_order
-          limit $from_record, $rows ";
+            {$sql_common}
+            {$sql_search}
+            {$sql_order}
+            limit {$from_record}, {$rows} ";
 $result = sql_query($sql);
 
 $listall = '<a href="'.$_SERVER['PHP_SELF'].'">처음</a>';
@@ -66,83 +66,87 @@ include_once('./admin.head.php');
 $colspan = 4;
 ?>
 
-<script type="text/javascript">
+<script>
 var list_update_php = '';
 var list_delete_php = 'popular_list.php';
 </script>
 
-<table width=100% cellpadding=3 cellspacing=1>
-<form id="fsearch" name="fsearch" method=get>
-<tr>
-    <td><?=$listall?> (건수 : <?=number_format($total_count)?>개)</td>
-    <td>
-        <select id="sfl" name="sfl">
-            <option value='pp_word'>검색어</option>
-            <option value='pp_date'>등록일</option>
-        </select>
-        <input type="text" id="stx" name="stx" required itemname='검색어' value='<?=$stx?>'>
-        <input type="image" src='<?=$g4['admin_path']?>/img/btn_search.gif' align=absmiddle></td>
-</tr>
+<form id="fsearch" name="fsearch" method="get">
+<fieldset>
+    <legend>인기검색어 검색</legend>
+    <div>
+        <span><?=$listall?></span>
+        건수 : <?=number_format($total_count)?>개
+    </div>
+    <select id="sfl" name="sfl">
+        <option value="pp_word">검색어</option>
+        <option value="pp_date">등록일</option>
+    </select>
+    <input type="text" id="stx" name="stx" required value="<?=$stx?>">
+    <input type="submit" value="검색">
+</fieldset>
 </form>
-</table>
 
-<form id="fpopularlist" name="fpopularlist" method=post>
-<input type="hidden" id="sst" name="sst"   value="<?=$sst?>">
-<input type="hidden" id="sod" name="sod"   value="<?=$sod?>">
-<input type="hidden" id="sfl" name="sfl"   value="<?=$sfl?>">
-<input type="hidden" id="stx" name="stx"   value="<?=$stx?>">
-<input type="hidden" id="page" name="page"  value="<?=$page?>">
-<input type="hidden" id="token" name="token" value="<?=$token?>">
-<table width=100% cellpadding=0 cellspacing=1>
-<colgroup width=30>
-<colgroup width=>
-<colgroup width=150>
-<colgroup width=150>
-<tr><td colspan='<?=$colspan?>' class='line1'></td></tr>
-<tr class='bgcol1 bold col1 ht center'>
-    <td><input type=checkbox id="chkall" name="chkall" value="1" onclick="check_all(this.form)"></td>
-    <td><?=subject_sort_link('pp_word')?>검색어</a></td>
-    <td>등록일</td>
-    <td>등록IP</td>
+<form id="fpopularlist" name="fpopularlist" method="post">
+<input type="hidden" name="sst" value="<?=$sst?>">
+<input type="hidden" name="sod" value="<?=$sod?>">
+<input type="hidden" name="sfl" value="<?=$sfl?>">
+<input type="hidden" name="stx" value="<?=$stx?>">
+<input type="hidden" name="page" value="<?=$page?>">
+<input type="hidden" name="token" value="<?=$token?>">
+<table>
+<thead>
+<tr>
+    <th scope="col"><?=subject_sort_link('pp_word')?>검색어</a></th>
+    <th scope="col">등록일</th>
+    <th scope="col">등록IP</th>
+    <th scope="col"><input type="checkbox" id="chkall" name="chkall" value="1" onclick="check_all(this.form)"></th>
 </tr>
-<tr><td colspan='<?=$colspan?>' class='line2'></td></tr>
+</thead>
+<tbody>
 <?
 for ($i=0; $row=sql_fetch_array($result); $i++) {
 
     $word = get_text($row['pp_word']);
+?>
 
-    $list = $i % 2;
-    echo "<input type="hidden" id="pp_id" name="pp_id"[$i] value='{$row['pp_id']}'>";
-    echo "<tr class='list$list col1 ht center'>";
-    echo "<td height=25><input type=checkbox id="chk" name="chk"[] value='$i'></td>";
-    echo "<td align='left'>&nbsp; <a href='{$_SERVER['PHP_SELF']}?sfl=pp_word&stx=$word'>$word</a></td>";
-    echo "<td>{$row['pp_date']}</td>";
-    echo "<td>{$row['pp_ip']}</td>";
-    echo "</tr>";
-    echo "<tr class='list$list col1 ht center'>";
-    echo "</tr>\n";
+<tr>
+    <td>&nbsp; <a href="<?=$_SERVER['PHP_SELF']?>?sfl=pp_word&amp;stx=<?=$word?>"><?=$word?></a></td>
+    <td><?=$row['pp_date']?></td>
+    <td><?=$row['pp_ip']?></td>
+    <td>
+        <input type="hidden" id="pp_id" name="pp_id[<?=$i?>]" value="<?=$row[pp_id]?>">
+        <input type="checkbox" id="chk" name="chk[]" value="<?=$i?>">
+    </td>
+</tr>
+
+<?
 }
 
 if ($i == 0)
-    echo "<tr><td colspan='$colspan' align=center height=100 bgcolor=#ffffff>자료가 없습니다.</td></tr>";
-
-echo "<tr><td colspan='$colspan' class='line2'></td></tr>";
-echo "</table>";
-
-$pagelist = get_paging($config['cf_write_pages'], $page, $total_page, "$_SERVER[PHP_SELF]?$qstr&page=");
-echo "<table width=100% cellpadding=3 cellspacing=1>";
-echo "<tr><td width=50%>";
-
-if ($is_admin == 'super')
-    echo "<input type=button class='btn1' value='선택삭제' onclick=\"btn_check(this.form, 'delete')\">";
-
-echo "</td>";
-echo "<td>$pagelist</td></tr></table>\n";
-
-if ($stx)
-    echo "<script>document.fsearch.sfl.value = '$sfl';</script>";
+    echo '<tr><td colspan="'.$colspan.'" class="empty_table">자료가 없습니다.</td></tr>';
 ?>
+</tbody>
+</table>
+
+<?if ($is_admin == 'super'){ ?>
+<div>
+    <input type="button" value="선택삭제" onclick="btn_check(this.form, 'delete')">
+</div>
+<?}?>
+
+<?
+$pagelist = get_paging($config['cf_write_pages'], $page, $total_page, "$_SERVER[PHP_SELF]?$qstr&amp;page=");
+?>
+<div class="paginate">
+    <?=$pagelist?>
+</div>
 </form>
+
+<?
+if ($stx)
+    echo '<script>document.fsearch.sfl.value = \''.$sfl.'\';</script>';
+?>
 
 <?
 include_once('./admin.tail.php');
