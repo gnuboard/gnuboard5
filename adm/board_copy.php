a582885ab93848e9fa0ad8b84023fb2a76509e4e
whitedot <whitedot@naver.com>
2012-11-19 11:09:23 +0900

    관리자:게시판복사

--- adm/board_copy.php
+++ adm/board_copy.php
@@ -15,33 +15,39 @@ include_once($g4['path'].'/head.sub.php');
 <input type="hidden" id="bo_table" name="bo_table" value="<?=$bo_table?>">
 <input type="hidden" id="token" name="token" value="<?=$token?>">
 <table>
+<tbody>
 <tr>
-	<th scope="col" id="th1">원본 테이블</th>
-	<td headers="th1"><?=$bo_table?></td>
+    <th scope="col">원본 테이블</th>
+    <td><?=$bo_table?></td>
 </tr>
 <tr>
-	<th scope="col" id="th2">복사할 TABLE</th>
-	<td headers="th2"><input type="text" id="target_table" name="target_table" maxlength="20" required alphanumericunderline> 영문자, 숫자, _ 만 가능 (공백없이)</td>
+    <th scope="col"><label for="target_table">복사할 TABLE</label></th>
+    <td><input type="text" id="target_table" name="target_table" maxlength="20" required alphanumericunderline> 영문자, 숫자, _ 만 가능 (공백없이)</td>
 </tr>
 <tr>
-	<th scope="col" id="th3">게시판 제목</th>
-	<td headers="th3"><input type="text" id="target_subject" name="target_subject" maxlength=120 required value="[복사본] <?=$board["bo_subject"]?>"></td>
+    <th scope="col"><label for="target_subject">게시판 제목</label></th>
+    <td><input type="text" id="target_subject" name="target_subject" maxlength="120" required value="[복사본] <?=$board["bo_subject"]?>"></td>
 </tr>
 <tr>
-	<th scope="col" id="th4">복사 유형</th>
-	<td headers="th4">
-        <input type="radio" id="copy_case" name="copy_case" value="schema_only" checked>구조만
-        <input type="radio" id="copy_case" name="copy_case" value="schema_data_both">구조와 데이터
+    <th scope="col">복사 유형</th>
+    <td>
+        <input type="radio" id="copy_case" name="copy_case" value="schema_only" checked>
+        <label for="copy_case">구조만</label>
+        <input type="radio" id="copy_case2" name="copy_case" value="schema_data_both">
+        <label for="copy_case2">구조와 데이터</label>
     </td>
 </tr>
+</tbody>
 </table>
 
-<input type="submit" value="복사">
-<input type="button" value="창닫기" onclick="window.close();">
+<div class="btn_confirm">
+    <input type="submit" value="복사">
+    <input type="button" value="창닫기" onclick="window.close();">
+</div>
 
 </form>
 
-<script type="text/javascript">
+<script>
 function fboardcopy_check(f)
 {
     f.action = "./board_copy_update.php";
