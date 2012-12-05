5c025ebaf1d34314367a9f990bc5b50c8db6bda7
whitedot <whitedot@naver.com>
2012-12-05 16:21:34 +0900

    검색결과 스킨

--- skin/search/neo/search.skin.php
+++ skin/search/neo/search.skin.php
@@ -1,30 +1,27 @@
 <?
 if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가 
 ?>
-2
-<form name=fsearch method=get onsubmit="return fsearch_submit(this);" style="margin:0px;">
-<table align=center width=95% cellpadding=2 cellspacing=0>
-<input type="hidden" name="srows" value="<?=$srows?>">
-<tr>
-    <td align=center>
-        <?=$group_select?>
-        <script type="text/javascript">document.getElementById("gr_id").value = "<?=$gr_id?>";</script>
 
-        <select name=sfl class=select>
-        <option value="wr_subject||wr_content">제목+내용</option>
-        <option value="wr_subject">제목</option>
-        <option value="wr_content">내용</option>
-        <option value="mb_id">회원아이디</option>
-        <option value="wr_name">이름</option>
+<form name="fsearch" method="get" onsubmit="return fsearch_submit(this);">
+<input type="hidden" name="srows" value="<?=$srows?>">
+    <fieldset>
+        <?=$group_select?>
+        <script>document.getElementById("gr_id").value = "<?=$gr_id?>";</script>
+
+        <select name="sfl">
+            <option value="wr_subject||wr_content">제목+내용</option>
+            <option value="wr_subject">제목</option>
+            <option value="wr_content">내용</option>
+            <option value="mb_id">회원아이디</option>
+            <option value="wr_name">이름</option>
         </select>
 
-        <input type=text name=stx class=ed maxlength=20 required itemname="검색어" value='<?=$text_stx?>'> 
+        <input type="text" name="stx" value="<?=$text_stx?>" maxlength="20" required>
 
-        <input type=submit value=" 검 색 ">
+        <input type="submit" value="검색">
 
-        <script type="text/javascript">
+        <script>
         document.fsearch.sfl.value = "<?=$sfl?>";
-
         function fsearch_submit(f)
         {
             if (f.stx.value.length < 2) {
@@ -52,69 +49,58 @@ if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
             return true;
         }
         </script>
-    </td>
-</tr>
-<tr>
-    <td align=center>
-        연산자 &nbsp; 
-        <input type="radio" name="sop" value="or" <?=($sop == "or") ? "checked" : "";?>>OR &nbsp;
-        <input type="radio" name="sop" value="and" <?=($sop == "and") ? "checked" : "";?>>AND
-    </td>
-</tr>
-</table>
+        <input type="radio" id="sop_or" name="sop" value="or" <?=($sop == "or") ? "checked" : "";?>> <label for="sop_or">OR</label>
+        <input type="radio" name="sop_and" name="sop" value="and" <?=($sop == "and") ? "checked" : "";?>> <label for="sop_and">AND</label>
+    </fieldset>
 </form>
-<p>
 
+<? 
+if ($stx) {
+    echo "검색된 게시판 리스트 (".$board_count."개의 게시판, ".number_format($total_count)."개의 게시글, ".number_format($page)."/".number_format($total_page)." 페이지)";
+    if ($board_count) {
+        echo "<ul>";
+        if ($onetable)
+            echo "<li><a href=\"?".$search_query."&amp;gr_id=".$gr_id."\">전체게시판 검색</a>";
+            echo $str_board_list;
+        echo "</ul>";
+    }
+    else
+    {
+        echo "<p>검색된 자료가 하나도 없습니다.</p>";
+    }
+}
+?>
 
-<table align=center width=95% cellpadding=2 cellspacing=0>
-<tr>
-    <td style='word-break:break-all;'>
-
-        <? 
-        if ($stx) 
-        { 
-            echo "<ul type=circle><li><b>검색된 게시판 리스트</b> (<b>{$board_count}</b>개의 게시판, <b>".number_format($total_count)."</b>개의 게시글, <b>".number_format($page)."/".number_format($total_page)."</b> 페이지)</ul>";
-            if ($board_count)
+<?
+$k=0;
+for ($idx=$table_index, $k=0; $idx<count($search_table) && $k<$rows; $idx++) {
+   $comment_href = "";
+?>
+<ul>
+    <li>
+        <a href="./board.php?bo_table=<?=$search_table[$idx]?>&amp;<?=$search_query?>"><?=$bo_subject[$idx]?></a>에서의 검색결과
+        <?
+        for ($i=0; $i<count($list[$idx]) && $k<$rows; $i++, $k++) {
+            if ($list[$idx][$i][wr_is_comment]) 
             {
-                echo "<ul><ul type=square style='line-height:130%;'>";
-                if ($onetable)
-                    echo "<li><a href='?$search_query&gr_id=$gr_id'>전체게시판 검색</a>";
-                echo $str_board_list;
-                echo "</ul></ul>";
+                echo "댓글 ";
+                $comment_href = "#c_".$list[$idx][$i][wr_id];
             }
-            else
-            {
-                echo "<ul style='line-height:130%;'><li>검색된 자료가 하나도 없습니다.</ul>";
-            }
-        }
         ?>
+         <ul>
+            <li>
+                <a href="<?=$list[$idx][$i][href]?><?=$comment_href?>"><?=$list[$idx][$i][subject]?></a>
+                <a href="<?=$list[$idx][$i][href]?><?=$comment_href?>" target="_blank">새창</a>
+                <p><?=$list[$idx][$i][content]?></p>
+                <?=$list[$idx][$i][wr_datetime]?>
+                <?=$list[$idx][$i][name]?>
+            </li>
+        </ul>
+        <? } ?>
+    </li>
+</ul>
+<? } ?>
 
-
-        <? 
-        $k=0;
-        for ($idx=$table_index, $k=0; $idx<count($search_table) && $k<$rows; $idx++) 
-        { 
-            echo "<ul type=circle><li><b><a href='./board.php?bo_table={$search_table[$idx]}&{$search_query}'><u>{$bo_subject[$idx]}</u></a>에서의 검색결과</b></ul>";
-            $comment_href = "";
-            for ($i=0; $i<count($list[$idx]) && $k<$rows; $i++, $k++) 
-            {
-                echo "<ul><ul type=square><li style='line-height:130%;'>";
-                if ($list[$idx][$i][wr_is_comment]) 
-                {
-                    echo "<font color=999999>[코멘트]</font> ";
-                    $comment_href = "#c_".$list[$idx][$i][wr_id];
-                }
-                echo "<a href='{$list[$idx][$i][href]}{$comment_href}'><u>";
-                echo $list[$idx][$i][subject];
-                echo "</u></a> [<a href='{$list[$idx][$i][href]}{$comment_href}' target=_blank>새창</a>]<br>";
-                echo $list[$idx][$i][content];
-                echo "<br><font color=#999999>{$list[$idx][$i][wr_datetime]}</font>&nbsp;&nbsp;&nbsp;";
-                echo $list[$idx][$i][name];
-                echo "</ul></ul>";
-            }
-        }
-        ?>
-
-        <p align=center><?=$write_pages?>
-
-</td></tr></table>
+<div id="pg">
+    <?=$write_pages?>
+</div>
