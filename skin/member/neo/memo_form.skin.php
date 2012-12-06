b86427d39d93b2a40a8ed2e76d99537d740f11d8
whitedot <whitedot@naver.com>
2012-12-06 15:14:07 +0900

    회원스킨/쪽지보내기

--- skin/member/neo/memo_form.skin.php
+++ skin/member/neo/memo_form.skin.php
@@ -4,87 +4,46 @@ if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
 
 <h1>쪽지보내기</h1>
 
-<table width="600" border="0" cellspacing="0" cellpadding="0">
+<ul>
+    <li><a href="./memo.php?kind=recv">받은쪽지</a></li>
+    <li><a href="./memo.php?kind=send">보낸쪽지</a></li>
+    <li><a href="./memo_form.php">쪽지보내기</a></li>
+</ul>
+
+<form name="fmemoform" method=post onsubmit="return fmemoform_submit(this);" autocomplete="off">
+<table>
+<caption>쪽지쓰기</caption>
+<tbody>
 <tr>
-    <td width="600" height="20" colspan="14"></td>
+    <th scope="row">받는 회원아이디</th>
+    <td>
+        <input type=text name="me_recv_mb_id" required value="<?=$me_recv_mb_id?>">
+        여러 회원에게 보낼때는 컴마(,)로 구분하세요.
+    </td>
 </tr>
 <tr>
-    <td width="30" height="24"></td>
-    <td width="99" align="center" valign="middle"><a href="./memo.php?kind=recv"><img src="<?=$member_skin_path?>/img/btn_recv_paper_off.gif" width="99" height="24" border="0"></a></td>
-    <td width="2"  align="center" valign="middle">&nbsp;</td>
-    <td width="99" align="center" valign="middle"><a href="./memo.php?kind=send"><img src="<?=$member_skin_path?>/img/btn_send_paper_off.gif" width="99" height="24" border="0"></a></td>
-    <td width="2"  align="center" valign="middle">&nbsp;</td>
-    <td width="99" align="center" valign="middle"><a href="./memo_form.php"><img src="<?=$member_skin_path?>/img/btn_write_paper_on.gif" width="99" height="24" border="0"></a></td>
-    <td width="2"  valign="middle">&nbsp;</td>
-    <td width="60" bgcolor="#EFEFEF">&nbsp;</td>
-    <td width="4"  bgcolor="#EFEFEF"">&nbsp;</td>
-    <td width="18" bgcolor="#EFEFEF">&nbsp;</td>
-    <td width="148" bgcolor="#EFEFEF">&nbsp;</td>
-    <td width="4" bgcolor="#EFEFEF">&nbsp;</td>
-    <td width="3" bgcolor="#EFEFEF"></td>
-    <td width="30" height="24"></td>
+    <th scope="row">내용</th>
+    <td><textarea name=me_memo rows=10 style='width:95%;' required itemname='내용'><?=$content?></textarea></td>
 </tr>
+</tbody>
 </table>
 
-<table width="600" border="0" cellspacing="0" cellpadding="0">
-<form name=fmemoform method=post onsubmit="return fmemoform_submit(this);" autocomplete="off">
-<tr>
-    <td height="300" align="center" valign="top">
-        <table width="540" border="0" cellspacing="0" cellpadding="0">
-        <tr>
-            <td height="20"></td>
-        </tr>
-        <tr>
-            <td height="2" bgcolor="#808080"></td>
-        </tr>
-        <tr>
-            <td width="540" height="2" align="center" valign="top" bgcolor="#FFFFFF">
-                <table width=100% cellpadding=1 cellspacing=1 border=0>
-                <tr bgcolor=#E1E1E1 align=center> 
-                    <td width="30%" height="24" rowspan="2"><b>받는 회원아이디</b></td>
-                    <td width="70%" align="center"><input type=text name="me_recv_mb_id" required itemname="받는 회원아이디" value="<?=$me_recv_mb_id?>" style="width:95%;"></td>
-                </tr>
-                <tr bgcolor=#E1E1E1 align=center> 
-                    <td>※ 여러 회원에게 보낼때는 컴마(,)로 구분하세요.</td>
-                </tr>
-                </table>
-            </td>
-        </tr>
-        <tr>
-            <td height="180" align="center" valign="middle" bgcolor="#F6F6F6">
-                <textarea name=me_memo rows=10 style='width:95%;' required itemname='내용'><?=$content?></textarea></td>
-        </tr>
-        <tr>
-            <td>
-                <table width=100% cellpadding=1 cellspacing=1 border=0>
-                <tr align=center> 
-                    <td width="30%" height="24" rowspan="2"><img id='kcaptcha_image' /></td>
-                    <td width="70%" align="left">
-                        <input type=input size=10 name=wr_key itemname="자동등록방지" required>&nbsp;&nbsp;왼쪽의 글자를 입력하세요.
-                    </td>
-                </tr>
-                </table>
-            </td>
-        </tr>
-        </table></td>
-</tr>
-<tr>
-    <td height="2" align="center" valign="top" bgcolor="#D5D5D5"></td>
-</tr>
-<tr>
-    <td height="2" align="center" valign="top" bgcolor="#E6E6E6"></td>
-</tr>
-<tr>
-    <td height="40" align="center" valign="bottom">
-        <input id=btn_submit type=image src="<?=$member_skin_path?>/img/btn_paper_send.gif" border=0>&nbsp;&nbsp;
-        <a href="javascript:window.close();"><img src="<?=$member_skin_path?>/img/btn_close.gif" width="48" height="20" border="0"></a></td>
-</tr>
+<fieldset>
+    <legend>자동등록방지</legend>
+    <img id='kcaptcha_image' />
+    <input type="input" name="wr_key" required>
+    왼쪽의 글자를 입력하세요.
+</fieldset>
+
+<div class="btn_confirm">
+    <input type="submit" id="btn_submit" value="보내기">
+    <a href="javascript:window.close();">창닫기</a>
+</div>
 </form>
-</table>
 
-<script type="text/javascript" src="<?=$g4[path]?>/js/md5.js"></script>
-<script type="text/javascript" src="<?="$g4[path]/js/jquery.kcaptcha.js"?>"></script>
-<script type="text/javascript">
+<script src="<?=$g4[path]?>/js/md5.js"></script>
+<script src="<?="$g4[path]/js/jquery.kcaptcha.js"?>"></script>
+<script>
 with (document.fmemoform) {
     if (me_recv_mb_id.value == "")
         me_recv_mb_id.focus();
