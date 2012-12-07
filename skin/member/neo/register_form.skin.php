32e624809a7c4c8c627abcd4ede3745168232c78
whitedot <whitedot@naver.com>
2012-12-07 13:31:04 +0900

    회원스킨/가입,가입완료

--- skin/member/neo/register_form.skin.php
+++ skin/member/neo/register_form.skin.php
@@ -17,59 +17,60 @@ var member_skin_path = "<?=$member_skin_path?>";
 <input type="hidden" id="mb_nick_enabled" name="mb_nick_enabled" value="">
 <input type="hidden" id="mb_email_enabled" name="mb_email_enabled" value="">
 <!-- <input type="hidden" name="token" value="<?=$token?>"> -->
-<? if ($member[mb_sex]) { ?><input type="hidden" id="mb_sex" name="mb_sex" value='<?=$member[mb_sex]?>'><? } ?>
+<? if ($member[mb_sex]) { ?><input type="hidden" name="mb_sex" value="<?=$member[mb_sex]?>"><? } ?>
+<? if ($member[mb_nick_date] <= date("Y-m-d", $g4[server_time] - ($config[cf_nick_modify] * 86400))) { // 별명수정일이 지나지 않았다면 ?>
+<input type="hidden" name="mb_nick_default" value="<?=$member[mb_nick]?>">
+<input type="hidden" name="mb_nick" value="<?=$member[mb_nick]?>">
+<? } ?>
 
 <table>
 <caption>사이트 이용정보 입력</caption>
 <tr>
-    <th scope="row"><label for="">아이디</label></th>
+    <th scope="row"><label for="reg_mb_id">아이디</label></th>
     <td>
-        <input type="text" id="reg_mb_id" name="mb_id" value="<?=$member[mb_id]?>" maxlength="20" size="20" <? if ($w=='u') { echo "readonly style='background-color:#dddddd;'"; } ?> <? if ($w=='') { echo "onblur='reg_mb_id_check();'"; } ?>>
-        <span id='msg_mb_id'></span>
+        <input type="text" id="reg_mb_id" name="mb_id" value="<?=$member[mb_id]?>" maxlength="20" <? if ($w=='u') { echo "readonly style='background-color:#dddddd;'"; } ?> <? if ($w=='') { echo "onblur='reg_mb_id_check();'"; } ?>>
+        <span id="msg_mb_id"></span>
         <span>영문자, 숫자, _ 만 입력 가능. 최소 3자이상 입력하세요.</span>
     </td>
 </tr>
 <tr>
-    <th scope="row"><label for="">패스워드</label></th>
-    <td><input type="password" id="mb_password" name="mb_password" size=20 maxlength=20 <?=($w=="")?"required":"";?>></td>
+    <th scope="row"><label for="mb_password">패스워드</label></th>
+    <td><input type="password" id="reg_mb_password" name="mb_password" maxlength="20" <?=($w=="")?"required":"";?>></td>
 </tr>
 <tr>
-    <th scope="row"><label for="">패스워드 확인</label></th>
-    <td><input type="password" id="mb_password_re" name="mb_password_re" size=20 maxlength=20 <?=($w=="")?"required":"";?>></td>
+    <th scope="row"><label for="reg_mb_password_re">패스워드 확인</label></th>
+    <td><input type="password" id="reg_mb_password_re" name="mb_password_re" maxlength="20" <?=($w=="")?"required":"";?>></td>
 </tr>
 </table>
 
 <table>
 <caption>개인정보 입력</caption>
 <tr>
-    <th scope="row"><label for="">이름</label></th>
+    <th scope="row"><label for="reg_mb_name">이름</label></th>
     <td>
-        <input id="mb_name" name="mb_name" value="<?=$member[mb_name]?>" <?=$member[mb_name]?"readonly2":"";?>>
+        <input id="reg_mb_name" name="mb_name" value="<?=$member[mb_name]?>" <?=$member[mb_name]?"readonly2":"";?>>
         <? if ($w=='') { echo "(공백없이 한글만 입력 가능)"; } ?>
     </td>
 </tr>
 <? if ($member[mb_nick_date] <= date("Y-m-d", $g4[server_time] - ($config[cf_nick_modify] * 86400))) { // 별명수정일이 지났다면 수정가능 ?>
-<input type="hidden" id="mb_nick_default" name="mb_nick_default" value='<?=$member[mb_nick]?>'>
 <tr>
-    <th scope="row"><label for="">별명</label></th>
+    <th scope="row"><label for="reg_mb_nick">별명</label></th>
     <td>
-        <input type="text" id="reg_mb_nick" name="mb_nick" maxlength="20" value='<?=$member[mb_nick]?>' onblur="reg_mb_nick_check();">
+        <input type="hidden" name="mb_nick_default" value="<?=$member[mb_nick]?>">
+        <input type="text" id="reg_mb_nick" name="mb_nick" maxlength="20" value="<?=$member[mb_nick]?>" onblur="reg_mb_nick_check();">
         <span id="msg_mb_nick"></span>
         <br>공백없이 한글,영문,숫자만 입력 가능 (한글2자, 영문4자 이상)
         <br>별명을 바꾸시면 앞으로 <?=(int)$config[cf_nick_modify]?>일 이내에는 변경 할 수 없습니다.
     </td>
 </tr>
-<? } else { ?>
-<input type="hidden" id="mb_nick_default" name="mb_nick_default" value='<?=$member[mb_nick]?>'>
-<input type="hidden" id="mb_nick" name="mb_nick" value="<?=$member[mb_nick]?>">
 <? } ?>
 
 <tr>
-    <th scope="row"><label for="">E-mail</label></th>
+    <th scope="row"><label for="reg_mb_email">E-mail</label></th>
     <td>
-        <input type="hidden" id="old_email" name="old_email" value='<?=$member[mb_email]?>'>
-        <input type="text" id='reg_mb_email' id="mb_email" name="mb_email" size=38 maxlength=100 value='<?=$member[mb_email]?>' onblur="reg_mb_email_check()">
-        <span id='msg_mb_email'></span>
+        <input type="hidden" name="old_email" value="<?=$member[mb_email]?>">
+        <input type="text" id="reg_mb_email" name="mb_email" maxlength="100" value='<?=$member[mb_email]?>' onblur="reg_mb_email_check()">
+        <span id="msg_mb_email"></span>
         <? if ($config[cf_use_email_certify]) { ?>
             <? if ($w=='') { echo "<br>e-mail 로 발송된 내용을 확인한 후 인증하셔야 회원가입이 완료됩니다."; } ?>
             <? if ($w=='u') { echo "<br>e-mail 주소를 변경하시면 다시 인증하셔야 합니다."; } ?>
@@ -79,20 +80,19 @@ var member_skin_path = "<?=$member_skin_path?>";
 
 <? if ($w=="") { ?>
 <tr>
-    <th scope="row"><label for="">생년월일</label></th>
-    <td><input type="text" id=mb_birth id="mb_birth" name="mb_birth" size=8 maxlength=8 minlength=8 required value='<?=$member[mb_birth]?>' readonly title='옆의 달력 아이콘을 클릭하여 날짜를 입력하세요.'></td>
+    <th scope="row"><label for="reg_mb_birth">생년월일</label></th>
+    <td><input type="text" id="reg_mb_birth" name="mb_birth" maxlength="8" required value="<?=$member[mb_birth]?>"></td>
 </tr>
 <? } ?>
 
-
 <? if (!$member[mb_sex]) { ?>
 <tr>
-    <th scope="row"><label for="">성별</label></th>
+    <th scope="row"><label for="reg_mb_sex">성별</label></th>
     <td>
-        <select id="mb_sex" name="mb_sex" required>
-            <option value=''>선택하세요</option>
-            <option value='F'>여자</option>
-            <option value='M'>남자</option>
+        <select id="reg_mb_sex" name="mb_sex" required>
+            <option value="">선택하세요</option>
+            <option value="F">여자</option>
+            <option value="M">남자</option>
         </select>
         <script>//document.getElementById('mb_sex').value='<?=$member[mb_sex]?>';</script>
     </td>
@@ -101,35 +101,35 @@ var member_skin_path = "<?=$member_skin_path?>";
 
 <? if ($config[cf_use_homepage]) { ?>
 <tr>
-    <th scope="row"><label for="">홈페이지</label></th>
-    <td><input type="text" id="mb_homepage" name="mb_homepage" size=38 maxlength=255 <?=$config[cf_req_homepage]?'required':'';?> value='<?=$member[mb_homepage]?>'></td>
+    <th scope="row"><label for="reg_mb_homepage">홈페이지</label></th>
+    <td><input type="text" id="reg_mb_homepage" name="mb_homepage" maxlength="255" <?=$config[cf_req_homepage]?'required':'';?> value="<?=$member[mb_homepage]?>"></td>
 </tr>
 <? } ?>
 
 <? if ($config[cf_use_tel]) { ?>
 <tr>
-    <th scope="row"><label for="">전화번호</label></th>
-    <td><input type="text" id="mb_tel" name="mb_tel" size=21 maxlength=20 <?=$config[cf_req_tel]?'required':'';?> value='<?=$member[mb_tel]?>'></td>
+    <th scope="row"><label for="reg_mb_tel">전화번호</label></th>
+    <td><input type="text" id="reg_mb_tel" name="mb_tel" maxlength="20" <?=$config[cf_req_tel]?'required':'';?> value="<?=$member[mb_tel]?>"></td>
 </tr>
 <? } ?>
 
 <? if ($config[cf_use_hp]) { ?>
 <tr>
-    <th scope="row"><label for="">핸드폰번호</label></th>
-    <td><input type="text" id="mb_hp" name="mb_hp" size=21 maxlength=20 <?=$config[cf_req_hp]?'required':'';?> value='<?=$member[mb_hp]?>'></td>
+    <th scope="row"><label for="reg_mb_hp">핸드폰번호</label></th>
+    <td><input type="text" id="reg_mb_hp" name="mb_hp" maxlength="20" <?=$config[cf_req_hp]?'required':'';?> value="<?=$member[mb_hp]?>"></td>
 </tr>
 <? } ?>
 
 <? if ($config[cf_use_addr]) { ?>
 <tr>
-    <th scope="row"><label for="">주소</label></th>
+    <th scope="row">주소</th>
     <td>
-        <input type="text" id="mb_zip1" name="mb_zip1" size=4 maxlength=3 readonly <?=$config[cf_req_addr]?'required':'';?> value='<?=$member[mb_zip1]?>'>
+        <input type="text" id="reg_mb_zip1" name="mb_zip1" maxlength="3" readonly <?=$config[cf_req_addr]?'required':'';?> value="<?=$member[mb_zip1]?>" title="우편번호 앞자리">
          -
-        <input type="text" id="mb_zip2" name="mb_zip2" size=4 maxlength=3 readonly <?=$config[cf_req_addr]?'required':'';?> value='<?=$member[mb_zip2]?>'>
-        <a href="javascript:;" onclick="win_zip('fregisterform', 'mb_zip1', 'mb_zip2', 'mb_addr1', 'mb_addr2');"><img width="91" height="20" src="<?=$member_skin_path?>/img/post_search_btn.gif" border=0 align=absmiddle></a>
-        <input type="text" id="mb_addr1" name="mb_addr1" size=60 readonly <?=$config[cf_req_addr]?'required':'';?> value='<?=$member[mb_addr1]?>'>
-        <input type="text" id="mb_addr2" name="mb_addr2" size=60 <?=$config[cf_req_addr]?'required':'';?> value='<?=$member[mb_addr2]?>'>
+        <input type="text" id="reg_mb_zip2" name="mb_zip2" maxlength="3" readonly <?=$config[cf_req_addr]?'required':'';?> value="<?=$member[mb_zip2]?>" title="우편번호 뒷자리">
+        <a href="javascript:;" onclick="win_zip('fregisterform', 'mb_zip1', 'mb_zip2', 'mb_addr1', 'mb_addr2');">주소찾기</a>
+        <input type="text" id="reg_mb_addr1" name="mb_addr1" readonly <?=$config[cf_req_addr]?'required':'';?> value="<?=$member[mb_addr1]?>" title="행정구역주소">
+        <input type="text" id="reg_mb_addr2" name="mb_addr2" <?=$config[cf_req_addr]?'required':'';?> value="<?=$member[mb_addr2]?>" title="상세주소">
     </td>
 </tr>
 <? } ?>
@@ -139,53 +139,55 @@ var member_skin_path = "<?=$member_skin_path?>";
 <caption>기타 개인설정</caption>
 <? if ($config[cf_use_signature]) { ?>
 <tr>
-    <th scope="row"><label for="">서명</label></th>
-    <td><textarea id="mb_signature" name="mb_signature" class=tx rows=3 style='width:95%;' <?=$config[cf_req_signature]?'required':'';?>><?=$member[mb_signature]?></textarea></td>
+    <th scope="row"><label for="reg_mb_signature">서명</label></th>
+    <td><textarea id="reg_mb_signature" name="mb_signature" <?=$config[cf_req_signature]?'required':'';?>><?=$member[mb_signature]?></textarea></td>
 </tr>
 <? } ?>
 
 <? if ($config[cf_use_profile]) { ?>
 <tr>
-    <th scope="row"><label for="">자기소개</label></th>
-    <td><textarea id="mb_profile" name="mb_profile" class=tx rows=3 style='width:95%;' <?=$config[cf_req_profile]?'required':'';?>><?=$member[mb_profile]?></textarea></td>
+    <th scope="row"><label for="reg_mb_profile">자기소개</label></th>
+    <td><textarea id="reg_mb_profile" name="mb_profile" <?=$config[cf_req_profile]?'required':'';?>><?=$member[mb_profile]?></textarea></td>
 </tr>
 <? } ?>
 
 <? if ($member[mb_level] >= $config[cf_icon_level]) { ?>
 <tr>
-    <th scope="row"><label for="">회원아이콘</label></th>
+    <th scope="row"><label for="reg_mb_icon">회원아이콘</label></th>
     <td>
-        <input type="file" id="mb_icon" name="mb_icon">
+        <input type="file" id="reg_mb_icon" name="mb_icon">
         이미지 크기는 가로 <?=$config[cf_member_icon_width]?>픽셀, 세로 <?=$config[cf_member_icon_height]?>픽셀 이하로 해주세요.<br>
         gif만 가능 / 용량:<?=number_format($config[cf_member_icon_size])?>바이트 이하만 등록됩니다.)
-        <? if ($w == "u" && file_exists($mb_icon)) { ?>
-        <input type="checkbox" id="del_mb_icon" name="del_mb_icon" value='1'> 삭제
+        <? if ($w == 'u' && file_exists($mb_icon)) { ?>
+        <input type="checkbox" id="del_mb_icon" name="del_mb_icon" value="1"> 삭제
         <? } ?>
     </td>
 </tr>
 <? } ?>
 
 <tr>
-    <th scope="row"><label for="">메일링서비스</label></th>
-    <td><input type="checkbox" id="mb_mailling" name="mb_mailling" value='1' <?=($w=='' || $member[mb_mailling])?'checked':'';?>>정보 메일을 받겠습니다.</td>
+    <th scope="row"><label for="reg_mb_mailling">메일링서비스</label></th>
+    <td><input type="checkbox" id="reg_mb_mailling" name="mb_mailling" value="1" <?=($w=='' || $member[mb_mailling])?'checked':'';?>>정보 메일을 받겠습니다.</td>
 </tr>
 <tr>
-    <th scope="row"><label for="">SMS 수신여부</label></th>
-    <td><input type="checkbox" id="mb_sms" name="mb_sms" value='1' <?=($w=='' || $member[mb_sms])?'checked':'';?>>핸드폰 문자메세지를 받겠습니다.</td>
+    <th scope="row"><label for="reg_mb_sms">SMS 수신여부</label></th>
+    <td><input type="checkbox" id="reg_mb_sms" name="mb_sms" value="1" <?=($w=='' || $member[mb_sms])?'checked':'';?>>핸드폰 문자메세지를 받겠습니다.</td>
 </tr>
 
 <? if ($member[mb_open_date] <= date("Y-m-d", $g4[server_time] - ($config[cf_open_modify] * 86400))) { // 정보공개 수정일이 지났다면 수정가능 ?>
-<input type="hidden" id="mb_open_default" name="mb_open_default" value='<?=$member[mb_open]?>'>
 <tr>
-    <th scope="row"><label for="">정보공개</label></th>
-    <td><input type="checkbox" id="mb_open" name="mb_open" value='1' <?=($w=='' || $member[mb_open])?'checked':'';?>>다른분들이 나의 정보를 볼 수 있도록 합니다.
-        <br>&nbsp;&nbsp;&nbsp;&nbsp; 정보공개를 바꾸시면 앞으로 <?=(int)$config[cf_open_modify]?>일 이내에는 변경이 안됩니다.</td>
+    <th scope="row"><label for="reg_mb_open">정보공개</label></th>
+    <td>
+        <input type="hidden" name="mb_open_default" value="<?=$member[mb_open]?>">
+        <input type="checkbox" id="reg_mb_open" name="mb_open" value="1" <?=($w=='' || $member[mb_open])?'checked':'';?>>다른분들이 나의 정보를 볼 수 있도록 합니다.<br>
+        정보공개를 바꾸시면 앞으로 <?=(int)$config[cf_open_modify]?>일 이내에는 변경이 안됩니다.
+    </td>
 </tr>
 <? } else { ?>
-<input type="hidden" id="mb_open" name="mb_open" value="<?=$member[mb_open]?>">
 <tr>
-    <th scope="row"><label for="">정보공개</label></th>
+    <th scope="row">정보공개</th>
     <td>
+        <input type="hidden" name="mb_open" value="<?=$member[mb_open]?>">
         정보공개는 수정후 <?=(int)$config[cf_open_modify]?>일 이내, <?=date("Y년 m월 j일", strtotime("$member[mb_open_date] 00:00:00") + ($config[cf_open_modify] * 86400))?> 까지는 변경이 안됩니다.<br>
         이렇게 하는 이유는 잦은 정보공개 수정으로 인하여 쪽지를 보낸 후 받지 않는 경우를 막기 위해서 입니다.
     </td>
@@ -194,8 +196,8 @@ var member_skin_path = "<?=$member_skin_path?>";
 
 <? if ($w == "" && $config[cf_use_recommend]) { ?>
 <tr>
-    <th scope="row"><label for="">추천인아이디</label></th>
-    <td><input type="text" id="mb_recommend" name="mb_recommend"></td>
+    <th scope="row"><label for="reg_mb_recommend">추천인아이디</label></th>
+    <td><input type="text" id="reg_mb_recommend" name="mb_recommend"></td>
 </tr>
 <? } ?>
 </table>
@@ -208,17 +210,12 @@ var member_skin_path = "<?=$member_skin_path?>";
 </fieldset>
 
 <div class="btn_confirm">
-    <input type="submit" value="회원가입" accesskey='s'>
+    <input type="submit" value="회원가입" accesskey="s">
 </div>
 </form>
 
 <script src="<?="$g4[path]/js/jquery.kcaptcha.js"?>"></script>
 <script>
-$(function() {
-    // 폼의 첫번째 입력박스에 포커스 주기 
-    $("#fregisterform :input[type="text"]:visible:enabled:first").focus(); 
-});
-
 // submit 최종 폼체크
 function fregisterform_submit(f) 
 {
