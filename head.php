1e7d984c410ddd8d45aef701a034958fcfe01b2d
whitedot <whitedot@naver.com>
2012-12-14 15:52:02 +0900

    헤더가 안 바뀌고 있다?

--- head.php
+++ head.php
@@ -41,8 +41,6 @@ if ($config['cf_title'] == $g4['title']) $g4['title'] = '';
         <input type="submit" value="검색">
     </fieldset>
     </form>
-
-    <h1><?=$g4['title']?> <?=$config['cf_title']?></h1>
 </header>
 
 <?=outlogin('neo'); // 외부 로그인 ?>
@@ -52,3 +50,4 @@ if ($config['cf_title'] == $g4['title']) $g4['title'] = '';
 <?=connect(); // 현재 접속자수 ?>
 
 <div id="wrapper">
+    <h1><?=$g4['title']?> <?=$config['cf_title']?></h1>
