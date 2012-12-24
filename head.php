c6d75730d63bbcb65a6e77baf559a24b33fb43c1
whitedot <whitedot@naver.com>
2012-12-24 17:50:03 +0900

    #100 에 따른 h1 다중사용 처리

--- head.php
+++ head.php
@@ -39,7 +39,7 @@ include_once($g4['path'].'/lib/popular.lib.php');
         <input type="submit" value="검색">
     </fieldset>
     </form>
-    <? if (!$bo_table) {?><h1><?=$g4['title']?></h1><? } ?>
+    <h1><?=$config['cf_title']?></h1>
 </header>
 
 <div>
@@ -48,3 +48,9 @@ include_once($g4['path'].'/lib/popular.lib.php');
 </div>
 
 <div id="wrapper">
+    <p>
+        g4s 강좌 홈페이지에 오신 것을 환영합니다.
+    </p>
+    <p>
+        <a href="<?=$g4['path']?>/bbs/board.php?bo_table=1">테스트용 게시판</a>
+    </p>
--- skin/poll/neo/poll.skin.php
+++ skin/poll/neo/poll.skin.php
@@ -14,7 +14,8 @@ if (!$po_id)
 $po = sql_fetch(" select * from $g4[poll_table] where po_id = '$po_id' ");
 ?>
 
-<div>
+<section>
+<h2>사이트 설문조사</h2>
 <form name="fpoll" method="post" action="<?=$g4[bbs_path]?>/poll_update.php" onsubmit="return fpoll_submit(this);" target="winPoll">
 <input type="hidden" name="po_id" value="<?=$po_id?>">
 <input type="hidden" name="skin_dir" value="<?=$skin_dir?>">
@@ -28,7 +29,7 @@ $po = sql_fetch(" select * from $g4[poll_table] where po_id = '$po_id' ");
 <input type="submit" value="투표하기">
 <a href="javascript:;" onclick="poll_result('<?=$po_id?>');">결과보기</a>
 </form>
-</div>
+
 <script>
 function fpoll_submit(f)
 {
