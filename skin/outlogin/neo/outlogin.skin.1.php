b636bc203a43b869296fd89f108bca1fb9c05fd4
whitedot <whitedot@naver.com>
2013-01-03 13:36:29 +0900

    사용자: 아웃로그인 로그인 전 완료

--- skin/outlogin/neo/outlogin.skin.1.php
+++ skin/outlogin/neo/outlogin.skin.1.php
@@ -54,6 +54,29 @@ else
 </section>
 
 <script>
+$(document).ready(function(){
+    $omi = $('#ol_mb_id');
+    $omp = $('#ol_mb_pw');
+    $omi_label = $('#ol_mb_id_label');
+    $omp_label = $('#ol_mb_pw_label');
+    $omi.focus(function() {
+        $omi_label.css('visibility','hidden');
+    });
+    $omp.focus(function() {
+        $omp_label.css('visibility','hidden');
+    });
+    $omi.blur(function() {
+        $this = $(this);
+        if($this.attr('id') == "ol_mb_id" && $this.attr('value') == "") $omi_label.css('visibility','visible');
+    });
+    $omp.blur(function() {
+        $this = $(this);
+        if($this.attr('id') == "ol_mb_pw" && $this.attr('value') == "") $omp_label.css('visibility','visible');
+    });
+});
+</script>
+
+<script>
 function fhead_submit(f)
 {
     return true;
