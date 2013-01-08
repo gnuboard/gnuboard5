e0add43a8fb73547e576dc51dbfd0a4e464f95e1
whitedot <whitedot@naver.com>
2013-01-08 13:25:28 +0900

    사용자: 회원패스워드 확인, 비밀글 패스워드 확인

--- skin/member/neo/password.skin.php
+++ skin/member/neo/password.skin.php
@@ -11,12 +11,12 @@ if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
 <input type="hidden" name="stx" value="<?=$stx?>">
 <input type="hidden" name="page" value="<?=$page?>">
 
-<fieldset>
+<fieldset id="password_confirm">
     <legend>패스워드 확인</legend>
-    <p>비밀글 기능으로 보호된 글입니다. 작성자와 관리자만 열람하실 수 있습니다. 작성자 본인이시라면 패스워드를 입력하세요.</p>
+    <p>비밀글 기능으로 보호된 글입니다. 작성자와 관리자만 열람하실 수 있습니다.<br>작성자 본인이시라면 패스워드를 입력하세요.</p>
     <label for="password_wr_password">패스워드</label>
-    <input type="password" id="password_wr_password" name="wr_password" maxLength="20" size="15" required="required" title="패스워드">
-    <input type="submit" value="확인">
+    <input type="password" id="password_wr_password" name="wr_password" class="fieldset_input required" maxLength="20" size="15" required>
+    <input type="submit" class="fieldset_submit" value="확인">
 </fieldset>
 
 </form>
