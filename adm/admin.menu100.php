8191fcd154d889f96827af15b6bf2f3ce11725ba
gnuboard <kagla@naver.com>
2013-01-09 11:49:57 +0900

    세션파일 일괄삭제, 캐시파일 일괄삭제 메뉴 추가, phpMyAdmin 메뉴 삭제

--- adm/admin.menu100.php
+++ adm/admin.menu100.php
@@ -8,8 +8,8 @@ $menu['menu100'] = array (
     array('100500', 'phpinfo()', ''.$g4['admin_path'].'/phpinfo.php', 'cf_phpinfo'),
     //array('100600', '업그레이드', ''.$g4['admin_path'].'/upgrade.php', 'cf_upgrade'),
     //array('100700', '복구/최적화', ''.$g4['admin_path'].'/repair.php', 'cf_repair'),
-    //array('100800', '세션 삭제', ''.$g4['admin_path'].'/session_delete.php', 'cf_delete'),
-    array('100900','캐쉬삭제','#', 'cf_cache'),
-    array('', 'phpMyAdmin', ''.$g4['path'].'/'.$g4['phpmyadmin_dir'].'', 'cf_phpmyadmin')
+    array('100800', '세션파일 일괄삭제', ''.$g4['admin_path'].'/session_delete.php', 'cf_delete'),
+    array('100900','캐쉬파일 일괄삭제','#', 'cf_cache'),
+    //array('', 'phpMyAdmin', ''.$g4['path'].'/'.$g4['phpmyadmin_dir'].'', 'cf_phpmyadmin')
 );
 ?>
