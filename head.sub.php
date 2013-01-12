05d8e355012548609183d27649d63bded8ff0a21
gnuboard <kagla@naver.com>
2013-01-12 11:58:18 +0900

    under construction 메세지 제거

--- head.sub.php
+++ head.sub.php
@@ -1,10 +1,7 @@
 <?php
 // 이 파일은 새로운 파일 생성시 반드시 포함되어야 함
 if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
-if (!$member['mb_id']) {
-    echo "under construction";
-    exit;
-}
+
 $begin_time = get_microtime();
 
 if (!isset($g4['title']))
