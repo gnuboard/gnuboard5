64e62c60a82d183111c47b3c5f5ed5739f42c782
chicpro <chicpro@gmail.com>
2013-01-15 16:41:55 +0900

    리스트 하단 버튼 및 기능 수정

--- skin/board/neo/list.skin.php
+++ skin/board/neo/list.skin.php
@@ -11,32 +11,7 @@ if ($is_nogood) $colspan++;
 
 <? if (!$wr_id) {?><h1><?=$g4['title']?></h1><?}?>
 
-<fieldset id="bo_sch">
-    <legend>게시물 검색</legend>
-
-    <form name="fsearch" method="get">
-    <input type="hidden" name="bo_table" value="<?=$bo_table?>">
-    <input type="hidden" name="sca" value="<?=$sca?>">
-    <select name="sfl" title="검색대상">
-        <option value="wr_subject">제목</option>
-        <option value="wr_content">내용</option>
-        <option value="wr_subject||wr_content">제목+내용</option>
-        <option value="mb_id,1">회원아이디</option>
-        <option value="mb_id,0">회원아이디(코)</option>
-        <option value="wr_name,1">글쓴이</option>
-        <option value="wr_name,0">글쓴이(코)</option>
-    </select>
-    <input name="stx" class="fieldset_input required" maxlength="15" size="15" required value="<?=stripslashes($stx)?>" title="검색어">
-    <input type="hidden" name="sop" value="and">
-    <!-- <input type="radio" id="sop_and" name="sop" value="and">
-    <label for="sop_and">and</label>
-    <input type="radio" id="sop_or" name="sop" value="or">
-    <label for="sop_or">or</label> -->
-    <input type="submit" class="fieldset_submit" value="검색">
-    </form>
-</fieldset>
-
-<div class="bo_link">
+<div class="bo_fx">
     <div id="bo_cate">
         <? if ($is_category) { ?>
         <form id="fcategory" name="fcategory" method="get">
@@ -49,6 +24,27 @@ if ($is_nogood) $colspan++;
         <span>Total <?=number_format($total_count)?>건</span>
         <?=$page?> 페이지
     </div>
+
+    <fieldset id="bo_sch">
+        <legend>게시물 검색</legend>
+
+        <form name="fsearch" method="get">
+        <input type="hidden" name="bo_table" value="<?=$bo_table?>">
+        <input type="hidden" name="sca" value="<?=$sca?>">
+        <select name="sfl" title="검색대상">
+            <option value="wr_subject">제목</option>
+            <option value="wr_content">내용</option>
+            <option value="wr_subject||wr_content">제목+내용</option>
+            <option value="mb_id,1">회원아이디</option>
+            <option value="mb_id,0">회원아이디(코)</option>
+            <option value="wr_name,1">글쓴이</option>
+            <option value="wr_name,0">글쓴이(코)</option>
+        </select>
+        <input name="stx" class="fieldset_input required" maxlength="15" size="15" required value="<?=stripslashes($stx)?>" title="검색어">
+        <input type="hidden" name="sop" value="and">
+        <input type="submit" class="fieldset_submit" value="검색">
+        </form>
+    </fieldset>
 
     <? if ($rss_href || $write_href) {?>
     <ul class="btn_bo_user">
@@ -106,10 +102,9 @@ for ($i=0; $i<count($list); $i++) {
         <a href="<?=$list[$i]['ca_name_href']?>" class="bo_cate_link<? if ($wr_id == $list[$i]['wr_id']) echo " bo_current";?>"><?=$list[$i]['ca_name']?></a>
         <? } ?>
 
-        <a href="<?=$list[$i]['href']?>"<? if ($wr_id == $list[$i]['wr_id']) echo " class=\"bo_current\"";?>><?=$list[$i]['subject']?>
-
-        <? if ($list[$i]['comment_cnt']) { ?><?=$list[$i]['comment_cnt'];?><? } ?>
-
+        <a href="<?=$list[$i]['href']?>"<? if ($wr_id == $list[$i]['wr_id']) echo " class=\"bo_current\"";?>>
+            <?=$list[$i]['subject']?>
+            <? if ($list[$i]['comment_cnt']) { ?><?=$list[$i]['comment_cnt'];?><? } ?>
         </a>
 
         <?
@@ -208,21 +203,6 @@ function fboardlist_submit(f) {
     }
 
     return true;
-}
-
-// 선택한 게시물 삭제
-function select_delete() {
-    var f = document.fboardlist;
-
-    str = "삭제";
-    if (!check_confirm(str))
-        return;
-
-    if (!confirm("선택한 게시물을 정말 "+str+" 하시겠습니까?\n\n한번 "+str+"한 자료는 복구할 수 없습니다"))
-        return;
-
-    f.action = "./delete_all.php";
-    f.submit();
 }
 
 // 선택한 게시물 복사 및 이동
