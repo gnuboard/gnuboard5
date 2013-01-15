1de17f96507caf606929289ead60b55012e23fbb
whitedot <whitedot@naver.com>
2013-01-15 15:56:22 +0900

    사용자: 게시판스킨, 복사, 이동 버튼스타일

--- bbs/move.php
+++ bbs/move.php
@@ -85,7 +85,7 @@ for ($i=0; $row=sql_fetch_array($result); $i++)
 
     <div class="btn_window btn_confirm">
         <input type="submit" id="btn_submit" class="btn_submit" value="<?=$act?>">
-        <a href="javascript:window.close();">창닫기</a>
+        <a href="javascript:window.close();" class="btn_cancel">창닫기</a>
     </div>
 
 </div>
