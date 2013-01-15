238941e04175f691eefbeda7afb831d791032d3a
gnuboard <kagla@naver.com>
2013-01-15 18:58:07 +0900

    센스리더 오류? 해결을 위하여 캡챠 wav 파일에 다운로드 제공, 그러나 결과는 좋지 않음

---
+++ bbs/gcaptcha/down.php
@@ -0,0 +1,35 @@
+<?
+include_once('./_common.php');
+
+$file = addslashes($_GET['file']);
+$captcha_path = mk_subdir($g4['cache_dir'].'/'.$g4['captcha_dir']);
+$filepath = $captcha_path.'/'.$file;
+$original = "number.wav";
+
+if(preg_match("/msie/i", $_SERVER['HTTP_USER_AGENT']) && preg_match("/5\.5/", $_SERVER['HTTP_USER_AGENT'])) {
+    header("content-type: doesn/matter");
+    header("content-length: ".filesize("$filepath"));
+    header("content-disposition: attachment; filename=\"$original\"");
+    header("content-transfer-encoding: binary");
+} else {
+    header("content-type: file/unknown");
+    header("content-length: ".filesize("$filepath"));
+    header("content-disposition: attachment; filename=\"$original\"");
+    header("content-description: php generated data");
+}
+header("pragma: no-cache");
+header("expires: 0");
+flush();
+
+$fp = fopen($filepath, 'rb');
+
+$download_rate = 10;
+
+while(!feof($fp)) {
+    print fread($fp, round($download_rate * 1024));
+    flush();
+    usleep(1000);
+}
+fclose ($fp);
+flush();
+?>
