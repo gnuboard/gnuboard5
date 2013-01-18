76728acc20e39cb94ccb89953a7fd51c6b90cad1
chicpro <chicpro@gmail.com>
2013-01-18 15:45:33 +0900

    설문조사 새창 win_poll에서 winPoll로 변경

--- skin/poll/neo/poll.skin.php
+++ skin/poll/neo/poll.skin.php
@@ -13,7 +13,7 @@ if (!$po_id) {
 $po = sql_fetch(" select * from {$g4['poll_table']} where po_id = '$po_id' ");
 ?>
 
-<form name="fpoll" method="post" action="<?=$g4['bbs_url']?>/poll_update.php" onsubmit="return fpoll_submit(this);" target="win_poll">
+<form name="fpoll" method="post" action="<?=$g4['bbs_url']?>/poll_update.php" onsubmit="return fpoll_submit(this);" target="winPoll">
 <input type="hidden" name="po_id" value="<?=$po_id?>">
 <input type="hidden" name="skin_dir" value="<?=$skin_dir?>">
 <section id="poll">
