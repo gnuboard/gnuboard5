346e4acf417c5aeb6d89bf91678d1f04c8f6b13a
chicpro <chicpro@gmail.com>
2013-01-21 11:19:16 +0900

    따옴표 및 php_eol 수정

--- bbs/password_lost2.php
+++ bbs/password_lost2.php
@@ -16,12 +16,12 @@ $email = trim($_POST['mb_email']);
 if (!$email)
     alert_close('메일주소 오류입니다.');
 
-$sql = " select count(*) as cnt from {$g4['member_table']} where mb_email = '{$email}' ";
+$sql = " select count(*) as cnt from {$g4['member_table']} where mb_email = '$email' ";
 $row = sql_fetch($sql);
 if ($row['cnt'] > 1)
-    alert('동일한 메일주소가 2개 이상 존재합니다.\n\n관리자에게 문의하여 주십시오.');
+    alert('동일한 메일주소가 2개 이상 존재합니다.\\n\\n관리자에게 문의하여 주십시오.');
 
-$sql = " select mb_no, mb_id, mb_name, mb_nick, mb_email, mb_datetime from {$g4['member_table']} where mb_email = '{$email}' ";
+$sql = " select mb_no, mb_id, mb_name, mb_nick, mb_email, mb_datetime from {$g4['member_table']} where mb_email = '$email' ";
 $mb = sql_fetch($sql);
 if (!$mb['mb_id'])
     alert('존재하지 않는 회원입니다.');
@@ -35,17 +35,17 @@ $randval = rand(4, 6);
 $change_password = substr(md5(get_microtime()), 0, $randval);
 
 $mb_lost_certify = sql_password($change_password);
-$mb_datetime     = sql_password($mb[mb_datetime]);
+$mb_datetime     = sql_password($mb['mb_datetime']);
 
 // 회원테이블에 필드를 추가
-sql_query(" ALTER TABLE `{$g4[member_table]}` ADD `mb_lost_certify` VARCHAR( 255 ) NOT NULL AFTER `mb_memo` ", false);
+sql_query(" ALTER TABLE `{$g4['member_table']}` ADD `mb_lost_certify` VARCHAR( 255 ) NOT NULL AFTER `mb_memo` ", false);
 
-$sql = " update {$g4[member_table]}
-            set mb_lost_certify = '{$mb_lost_certify}'
-            where mb_id = '{$mb[mb_id]}' ";
+$sql = " update {$g4['member_table']}
+            set mb_lost_certify = '$mb_lost_certify'
+            where mb_id = '{$mb['mb_id']}' ";
 sql_query($sql);
 
-$href = $g4[url].'/'.$g4[bbs].'/password_lost_certify.php?mb_no='.$mb[mb_no].'&amp;mb_datetime='.$mb_datetime.'&amp;mb_lost_certify='.$mb_lost_certify;
+$href = $g4['url'].'/'.$g4['bbs'].'/password_lost_certify.php?mb_no='.$mb['mb_no'].'&amp;mb_datetime='.$mb_datetime.'&amp;mb_lost_certify='.$mb_lost_certify;
 
 $subject = '요청하신 회원아이디/패스워드 정보입니다.';
 
@@ -54,13 +54,13 @@ $content .= '<div style="line-height:180%;">';
 $content .= '<p>요청하신 계정정보는 다음과 같습니다.</p>';
 $content .= '<hr>';
 $content .= '<ul>';
-$content .= '<li>회원아이디 : '.$mb[mb_id].'</li>';
+$content .= '<li>회원아이디 : '.$mb['mb_id'].'</li>';
 $content .= '<li>변경 패스워드 : <span style="color:#ff3300; font:13px Verdana;"><strong>'.$change_password.'</strong></span></li>';
-$content .= '<li>이름 : '.addslashes($mb[mb_name]).'</li>';
-$content .= '<li>별명 : '.addslashes($mb[mb_nick]).'</li>';
-$content .= '<li>이메일주소 : '.addslashes($mb[mb_email]).'</li>';
-$content .= '<li>요청일시 : '.$g4[time_ymdhis].'</li>';
-$content .= '<li>홈페이지 : '.$g4[url].'</li>';
+$content .= '<li>이름 : '.addslashes($mb['mb_name']).'</li>';
+$content .= '<li>별명 : '.addslashes($mb['mb_nick']).'</li>';
+$content .= '<li>이메일주소 : '.addslashes($mb['mb_email']).'</li>';
+$content .= '<li>요청일시 : '.$g4['time_ymdhis'].'</li>';
+$content .= '<li>홈페이지 : '.$g4['url'].'</li>';
 $content .= '</ul>';
 $content .= '<hr>';
 $content .= '<p><a href="'.$href.'" target="_blank">'.$href.'</a></p>';
@@ -75,7 +75,7 @@ $content .= '<p>[끝]</p>';
 $content .= '</div>';
 
 $admin = get_admin('super');
-mailer($admin[mb_nick], $admin[mb_email], $mb[mb_email], $subject, $content, 1);
+mailer($admin['mb_nick'], $admin['mb_email'], $mb['mb_email'], $subject, $content, 1);
 
-alert_close($email.' 메일로 회원아이디와 패스워드를 인증할 수 있는 메일이 발송 되었습니다.\n\n메일을 확인하여 주십시오.');
+alert_close($email.' 메일로 회원아이디와 패스워드를 인증할 수 있는 메일이 발송 되었습니다.\\n\\n메일을 확인하여 주십시오.');
 ?>
