f718837a197358e9709e9389b71a4546e8174487
whitedot <whitedot@naver.com>
2013-01-23 18:19:09 +0900

    사용자: 현재접속자 스타일

--- skin/connect/neo/connect.skin.php
+++ skin/connect/neo/connect.skin.php
@@ -2,4 +2,4 @@
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 ?>
 
-<strong><?=$row['total_cnt']?>명 중 회원 <?=$row['mb_cnt']?>명</strong>
+<strong><?=$row['total_cnt']?></strong>명 중 회원 <?=$row['mb_cnt']?>명
