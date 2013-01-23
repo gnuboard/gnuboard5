594a1a3907606460486e51a8cc93216b214561f7
whitedot <whitedot@naver.com>
2013-01-23 18:55:59 +0900

    사용자: 아웃로그인 로그인 전 스타일

--- skin/outlogin/neo/outlogin.skin.1.php
+++ skin/outlogin/neo/outlogin.skin.1.php
@@ -13,14 +13,12 @@ if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
         <label for="ol_pw" id="ol_pwlabel">패스워드<strong class="sound_only">필수</strong></label>
         <input type="password" id="ol_pw" name="mb_password" maxlength="20" required title="패스워드">
         <input type="submit" id="ol_submit" value="로그인">
-        <div id="ol_auto">
+        <div id="ol_svc">
+            <a href="<?=$g4['bbs_url']?>/register.php"><b>회원가입</b></a>
+            <a href="<?=$g4['bbs_url']?>/password_lost.php" id="ol_password_lost">정보찾기</a>
             <input type="checkbox" id="auto_login" name="auto_login" value="1">
             <label for="auto_login" id="auto_login_label">자동로그인</label>
         </div>
-        <ul>
-            <li><a href="<?=$g4['bbs_url']?>/register.php">회원가입</a></li>
-            <li><a href="<?=$g4['bbs_url']?>/password_lost.php" id="ol_password_lost">정보찾기</a></li>
-        </ul>
     </fieldset>
     </form>
 </section>
@@ -30,7 +28,7 @@ $(function(){
     $omi = $('#ol_id');
     $omp = $('#ol_pw');
     $omp.css('display','inline-block');
-    $omp.css('width',121);
+    $omp.css('width',124);
     $omi_label = $('#ol_idlabel');
     $omp_label = $('#ol_pwlabel');
     $omi_label.addClass('ol_idlabel');
