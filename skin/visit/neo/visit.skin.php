a85fcd8890da17630eb464a60a7c6fe296f314df
whitedot <whitedot@naver.com>
2013-01-24 11:34:17 +0900

    사용자: 방문자집계 스타일

--- skin/visit/neo/visit.skin.php
+++ skin/visit/neo/visit.skin.php
@@ -5,16 +5,18 @@ global $is_admin;
 ?>
 
 <section id="visit">
-    <h2>방문자집계</h2>
-    <dl>
-        <dt>오늘</dt>
-        <dd><?=number_format($visit[1])?></dd>
-        <dt>어제</dt>
-        <dd><?=number_format($visit[2])?></dd>
-        <dt>최대</dt>
-        <dd><?=number_format($visit[3])?></dd>
-        <dt>전체</dt>
-        <dd><?=number_format($visit[4])?></dd>
-    </dl>
-    <? if ($is_admin == "super") { ?><a href="<?=$g4['admin_path']?>/visit_list.php">상세보기</a><?}?>
+    <div>
+        <h2>방문자집계</h2>
+        <dl>
+            <dt>오늘</dt>
+            <dd><?=number_format($visit[1])?></dd>
+            <dt>어제</dt>
+            <dd><?=number_format($visit[2])?></dd>
+            <dt>최대</dt>
+            <dd><?=number_format($visit[3])?></dd>
+            <dt>전체</dt>
+            <dd><?=number_format($visit[4])?></dd>
+        </dl>
+        <? if ($is_admin == "super") { ?><a href="<?=$g4['admin_path']?>/visit_list.php">상세보기</a><?}?>
+    </div>
 </section>
