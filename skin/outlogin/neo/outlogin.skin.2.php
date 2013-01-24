6ae3e25c04420e086541e03dcb2f46fa64923ab0
whitedot <whitedot@naver.com>
2013-01-24 11:21:40 +0900

    사용자: 아웃로그인 로그인 후 스타일 및 크로스브라우징

--- skin/outlogin/neo/outlogin.skin.2.php
+++ skin/outlogin/neo/outlogin.skin.2.php
@@ -3,22 +3,32 @@ if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
 ?>
 
 <!-- 로그인 후 외부로그인 시작 -->
-<section id="ol_after" class="outlogin">
+<section id="ol_after" class="ol">
     <header id="ol_after_hd">
         <h2>나의 이용정보</h2>
         <strong><?=$nick?></strong>님
         <? if ($is_admin == 'super' || $is_auth) { ?><a href="<?=$g4['admin_path']?>/" id="ol_admin">관리자</a><? } ?>
     </header>
-    <ul id="ol_after_rec">
-        <li><a href="<?=$g4['path']?>/bbs/memo.php" id="ol_after_memo" target="_blank" onclick="win_memo(); return false;">안 읽은 쪽지 <span id="ol_after_memo_img"><?=$memo_not_read?></span></a></li>
-        <li><a href="<?=$g4['path']?>/bbs/point.php" id="ol_after_pt" target="_blank" onclick="win_point(); return false;">포인트 <span id="ol_after_pt_img"><?=$point?></span></a></li>
-        <li><a href="<?=$g4['path']?>/bbs/scrap.php" id="ol_after_scrap" target="_blank" onclick="win_scrap(); return false;"><span id="ol_after_scrap_img">스크랩</span></a></li>
+    <ul id="ol_after_private">
+        <li>
+            <a href="<?=$g4['path']?>/bbs/memo.php" id="ol_after_memo" target="_blank" onclick="win_memo(); return false;">
+                <span class="sound_only">안 읽은 </span>쪽지
+                <strong><?=$memo_not_read?></strong>
+            </a>
+        </li>
+        <li>
+            <a href="<?=$g4['path']?>/bbs/point.php" id="ol_after_pt" target="_blank" onclick="win_point(); return false;">
+                포인트
+                <strong>123,456,789</strong>
+            </a>
+        </li>
+        <li>
+            <a href="<?=$g4['path']?>/bbs/scrap.php" id="ol_after_scrap" target="_blank" onclick="win_scrap(); return false;">스크랩</a>
+        </li>
     </ul>
     <footer id="ol_after_ft">
-        <ul>
-            <li><a href="<?=$g4['bbs_path']?>/member_confirm.php?url=register_form.php" id="ol_after_info">정보수정</a></li>
-            <li><a href="<?=$g4['bbs_path']?>/logout.php" id="ol_after_logout">로그아웃</a></li>
-        </ul>
+        <a href="<?=$g4['bbs_path']?>/member_confirm.php?url=register_form.php" id="ol_after_info">정보수정</a>
+        <a href="<?=$g4['bbs_path']?>/logout.php" id="ol_after_logout">로그아웃</a>
     </footer>
 </section>
 
