2cb8b47c9657a11f9a54a33631e2d07bfb92b95e
chicpro <chicpro@gmail.com>
2013-01-24 09:16:10 +0900

    이슈 173 처리

--- main.php
+++ main.php
@@ -9,7 +9,7 @@ include_once('./_head.php');
 <!-- 메인화면 최신글 시작 -->
 <?
 //  최신글
-$sql = " select bo_table from $g4[board_table] order by gr_id, bo_table ";
+$sql = " select bo_table from {$g4['board_table']} order by gr_id, bo_table ";
 $result = sql_query($sql);
 for ($i=0; $row=sql_fetch_array($result); $i++) {
     // 이 함수가 바로 최신글을 추출하는 역할을 합니다.
@@ -17,16 +17,15 @@ for ($i=0; $row=sql_fetch_array($result); $i++) {
 
     // 사용방법
     // latest(스킨, 게시판아이디, 출력라인, 글자수);
+    if(($i % 2 == 1))
+        echo "<div class=\"lt\" style=\"margin-left: 20px;\">";
+    else
+        echo "<div class=\"lt\">";
+
     echo latest("neo", $row['bo_table'], 5, 25);
+    echo "</div>";
 }
 ?>
-
-<script>
-$(function(){
-    var $lts = $('.lt:nth-child(odd)');
-    $lts.css("margin-left","20px");
-});
-</script>
 <!-- 메인화면 최신글 끝 -->
 
 <?
