a2af109da7f980605de1ad46f396054de65ad8ea
chicpro <chicpro@gmail.com>
2013-01-25 17:52:44 +0900

    기존 캡차 코드 제거

--- shop/item.php
+++ shop/item.php
@@ -1103,26 +1103,8 @@ function explan_resize_image()
 }
 </script>
 
-<script type="text/javascript" src="<?=$g4[path]?>/js/jquery.kcaptcha.js"></script>
 <script type="text/javascript">
 $(function() {
-    $("#kcaptcha_image_use, #kcaptcha_image_qa").bind("click", function() {
-        $.ajax({
-            type: 'POST',
-            url: g4_path+'/'+g4_bbs+'/kcaptcha_session.php',
-            cache: false,
-            async: false,
-            success: function(text) {
-                $("#kcaptcha_image_use, #kcaptcha_image_qa").attr('src', g4_path+'/'+g4_bbs+'/kcaptcha_image.php?t=' + (new Date).getTime());
-            }
-        });
-    })
-    .css('cursor', 'pointer')
-    .attr('title', '글자가 잘 안보이시는 경우 클릭하시면 새로운 글자가 나옵니다.')
-    .attr('width', '120')
-    .attr('height', '60')
-    .trigger('click');
-
     explan_resize_image();
 });
 </script>
