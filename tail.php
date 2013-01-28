009385e8f25de9506d3e7956adefaeaf729d0c1f
whitedot <whitedot@naver.com>
2013-01-28 14:44:10 +0900

    사용자: 하단 html5 로고 추가

--- tail.php
+++ tail.php
@@ -10,7 +10,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
     <h1><?=$config['cf_title']?> 정보</h1>
     <?=popular('neo'); // 인기검색어 ?>
     <?=visit("neo"); // 방문자수 ?>
-    <div id="ft_catch"><img src="<?=G4_URL?>/img/ft_catch.jpg" alt="Sharing All Possibilities"></div>
+    <div id="ft_catch"><img src="<?=G4_URL?>/img/ft_catch.jpg" alt="Sharing All Possibilities"><img src="<?=G4_URL?>/img/ft_html5_badge.jpg" alt="HTML5 Markup" id="ft_html5badge"></div>
     <div id="ft_copy">
         <p>Copyright &copy; <b>소유하신 도메인.</b> All rights reserved.</p>
     </div>
