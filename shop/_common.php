c1f0dfc7ff3bb5ab342e5494959478c20fe278b2
gnuboard <kagla@naver.com>
2013-01-28 14:38:48 +0900

    쇼핑몰 사용 안함 메세지 추가

--- shop/_common.php
+++ shop/_common.php
@@ -1,5 +1,5 @@
 <?
-include_once("$g4_path/common.php");
+include_once("../common.php");
 if (!defined('G4_USE_SHOP') || !G4_USE_SHOP) 
-    die("config.user.php 에 define('\$G4_USE_SHOP', true); 로 설정해 주세요.");
+    die("현재 쇼핑몰 사용 안함으로 설정되어 있습니다.<br>config.user.php 에 define('G4_USE_SHOP', true); 로 설정해 주세요.");
 ?>
