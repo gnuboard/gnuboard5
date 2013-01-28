b4fe9dc89785bbb3b01af821818686ecf329f5dd
gnuboard <kagla@naver.com>
2013-01-28 14:36:27 +0900

    G4_IS_SHOP 을 G4_USE_SHOP 으로 변경

--- shop/_common.php
+++ shop/_common.php
@@ -1,4 +1,5 @@
 <?
-$g4_path = "..";
 include_once("$g4_path/common.php");
+if (!defined('G4_USE_SHOP') || !G4_USE_SHOP) 
+    die("config.user.php 에 define('\$G4_USE_SHOP', true); 로 설정해 주세요.");
 ?>
