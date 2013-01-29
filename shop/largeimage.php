2c8eacde839568d5814c59441f48a86ba1c82727
chicpro <chicpro@gmail.com>
2013-01-29 17:09:39 +0900

    상품이미지 G4 경로 수정

--- shop/largeimage.php
+++ shop/largeimage.php
@@ -1,29 +1,33 @@
 <?
 include_once("./_common.php");
 
-$sql = " select it_name from $g4[yc4_item_table] where it_id='$it_id' ";
+$sql = " select it_name, it_img1, it_img2, it_img3, it_img4, it_img5, it_img6, it_img7, it_img8, it_img9, it_img10
+            from $g4[yc4_item_table]
+            where it_id='$it_id' ";
 $row = sql_fetch_array(sql_query($sql));
 
-$imagefile = "$g4[path]/data/item/$img";
+$imagefile = G4_DATA_PATH."/item/$img";
+$imagefile_url = G4_DATA_URL."/item/$img";
 $size = getimagesize($imagefile);
 
-$g4[title] = "$row[it_name] ($it_id)";
-include_once("$g4[path]/head.sub.php");
+$g4['title'] = "{$row['it_name']} ($it_id)";
+include_once(G4_PATH.'/head.sub.php');
 ?>
 <br>
 <div align=center>
-    <a href='#' onclick='window.close();'><img id='largeimage' src='<?=$imagefile?>' width='<?=$size[0]?>' height='<?=$size[1]?>' alt='<?=$row[it_name]?>' border=0 style='border:1 solid #E4E4E4;'></a>
+    <a href='#' onclick='window.close();'><img id='largeimage' src='<?=$imagefile_url?>' width='<?=$size[0]?>' height='<?=$size[1]?>' alt='<?=$row['it_name']?>' border=0 style='border:1 solid #E4E4E4;'></a>
 </div>
 <p>
 <table width=100% cellpadding=0 cellspacing=0>
 <tr>
-    <td width=30% align=center><a href='#' onclick='window.close();'><img src='<? echo "$g4[shop_img_path]/" ?>/btn_close.gif' border=0 alt="창닫기"></a></td>
+    <td width=30% align=center><a href='#' onclick='window.close();'><img src='<? echo G4_SHOP_IMG_URL; ?>/btn_close.gif' border=0 alt="창닫기"></a></td>
     <td width=70% align=right>
         <?
-        for ($i=1; $i<=5; $i++)
+        for ($i=1; $i<=10; $i++)
         {
-            if (file_exists("$g4[path]/data/item/{$it_id}_l{$i}"))
-                echo "<img id='large{$i}' src='$g4[path]/data/item/{$it_id}_l{$i}' border=0 width=50 height=50 style='border:1 solid #E4E4E4;'
+            $filename = $row['it_img'.$i];
+            if (file_exists(G4_DATA_PATH."/item/{$it_id}/{$filename}") && $filename != "")
+                echo "<img id='large{$i}' src='".G4_DATA_URL."/item/{$it_id}/{$filename}' border=0 width=50 height=50 style='border:1 solid #E4E4E4;'
                     onmouseover=\"document.getElementById('largeimage').src=document.getElementById('large{$i}').src;\"> &nbsp;";
         }
         ?>
@@ -31,5 +35,5 @@ include_once("$g4[path]/head.sub.php");
 </tr>
 </table>
 <?
-include_once("$g4[path]/tail.sub.php");
+include_once(G4_PATH.'/tail.sub.php');
 ?>
