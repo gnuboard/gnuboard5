1ef493c36c150d1279e8dd1e28508fe392d1627e
gnuboard <kagla@naver.com>
2013-01-30 15:44:56 +0900

    관리자 URL 수정

--- adm/admin.menu200.php
+++ adm/admin.menu200.php
@@ -1,10 +1,10 @@
 <?
 $menu['menu200'] = array (
-    array('200000', '회원관리',     $g4['admin_url'].'/member_list.php',    'member'),
-    array('200100', '회원관리',     $g4['admin_url'].'/member_list.php',    'mb_list'),
-    array('200200', '포인트관리',   $g4['admin_url'].'/point_list.php',     'mb_point'),
-    array('200300', '회원메일발송', $g4['admin_url'].'/mail_list.php',      'mb_mail'),
-    array('200800', '접속자집계',   $g4['admin_url'].'/visit_list.php',     'mb_visit'),
-    array('200900', '투표관리',     $g4['admin_url'].'/poll_list.php',      'mb_poll')
+    array('200000', '회원관리',     G4_ADMIN_URL.'/member_list.php',    'member'),
+    array('200100', '회원관리',     G4_ADMIN_URL.'/member_list.php',    'mb_list'),
+    array('200200', '포인트관리',   G4_ADMIN_URL.'/point_list.php',     'mb_point'),
+    array('200300', '회원메일발송', G4_ADMIN_URL.'/mail_list.php',      'mb_mail'),
+    array('200800', '접속자집계',   G4_ADMIN_URL.'/visit_list.php',     'mb_visit'),
+    array('200900', '투표관리',     G4_ADMIN_URL.'/poll_list.php',      'mb_poll')
 );
 ?>
