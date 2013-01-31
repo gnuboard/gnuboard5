de5fe00a270231f0ee5a66f57a4883cbcbf37c0a
gnuboard <kagla@naver.com>
2013-01-31 17:06:38 +0900

    로긴시 host 문제 해결

--- bbs/login.php
+++ bbs/login.php
@@ -5,9 +5,10 @@ $g4['title'] = '로그인';
 include_once('./_head.sub.php');
 
 $p = parse_url($url);
-if ((isset($p['scheme']) && $p['scheme']) ||
-    (isset($p['host']) && $p['host'])) {
-    alert('url에 도메인을 지정할 수 없습니다.');
+if ((isset($p['scheme']) && $p['scheme']) || (isset($p['host']) && $p['host'])) {
+    //print_r2($p);
+    if ($p['host'] != $_SERVER['HTTP_HOST'])
+        alert('url에 타 도메인을 지정할 수 없습니다.');
 }
 
 // 이미 로그인 중이라면
