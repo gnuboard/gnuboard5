0daa47ef0d6a097a34368659aedbe48ce9c7b6a7
whitedot <whitedot@naver.com>
2013-01-31 09:46:43 +0900

    사용자: 우편번호찾기 스타일

--- skin/member/neo/zip.skin.php
+++ skin/member/neo/zip.skin.php
@@ -12,10 +12,10 @@ if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
     <input type="hidden" name="frm_addr1" value="<?=$frm_addr1?>">
     <input type="hidden" name="frm_addr2" value="<?=$frm_addr2?>">
 
-    <fieldset>
+    <fieldset class="cbg">
         <label for="addr1">동/읍/면/리 검색</label>
-        <input type="text" id="addr1" name="addr1" class="fieldset_input" value="<?=$addr1?>" required minlength=2>
-        <input type="submit" class="fieldset_submit" value="검색">
+        <input type="text" id="addr1" name="addr1" class="fs_input" value="<?=$addr1?>" required minlength=2>
+        <input type="submit" class="fs_submit" value="검색">
     </fieldset>
 
     <!-- 검색결과 여기서부터 -->
@@ -26,7 +26,7 @@ if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
 
 
     <? if ($search_count > 0) { ?>
-    <dl>
+    <dl class="cbg">
         <dt>총 <?=$search_count?>건 가나다순 정렬</dt>
         <dd>
             <ul>
