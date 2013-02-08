8b9150422df7c60d9ff0ae51a21721cda7aada4c
whitedot <whitedot@naver.com>
2013-02-08 14:12:53 +0900

    사용자: 게시판스킨 읽기 작업 중

--- skin/board/neo/view.skin.php
+++ skin/board/neo/view.skin.php
@@ -13,7 +13,10 @@ if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
 
     <section id="bo_v_info">
         <h2>게시물 정보</h2>
-        작성자 <strong><?=$view['name']?><? if ($is_ip_view) { echo "&nbsp;($ip)"; } ?></strong><span class="sound_only">작성일</span><strong><?=date("y-m-d H:i", strtotime($view['wr_datetime']))?></strong>조회<strong><?=number_format($view['wr_hit'])?>회</strong>댓글<strong><?=number_format($view['wr_comment'])?>건</strong>
+        작성자 <strong><?=$view['name']?><? if ($is_ip_view) { echo "&nbsp;($ip)"; } ?></strong>
+        <span class="sound_only">작성일</span><strong><?=date("y-m-d H:i", strtotime($view['wr_datetime']))?></strong>
+        조회<strong><?=number_format($view['wr_hit'])?>회</strong>
+        댓글<strong><?=number_format($view['wr_comment'])?>건</strong>
     </section>
 
     <?
