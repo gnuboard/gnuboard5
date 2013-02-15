576a2f5581b3bbbdffcf577bcae1f97132d1b001
chicpro <chicpro@gmail.com>
2013-02-15 13:01:44 +0900

    #268 경로수정

--- adm/mail_test.php
+++ adm/mail_test.php
@@ -5,7 +5,7 @@ include_once('./_common.php');
 if (!$config['cf_email_use'])
     alert('환경설정에서 \'메일발송 사용\'에 체크하셔야 메일을 발송할 수 있습니다.');
 
-include_once($g4['path'].'/lib/mailer.lib.php');
+include_once(G4_LIB_PATH.'/mailer.lib.php');
 
 auth_check($auth[$sub_menu], 'w');
 
