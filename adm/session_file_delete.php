8a1e2b48ac2f5185ed023fd621bd8b5f752f654c
whitedot <whitedot@naver.com>
2013-02-18 14:31:32 +0900

    관리자: #279 처리

--- adm/session_file_delete.php
+++ adm/session_file_delete.php
@@ -12,43 +12,49 @@ include_once("./admin.head.php");
 <div id="session_del">
     <p>
         완료 메세지가 나오기 전에 프로그램의 실행을 중지하지 마십시오.
-    <p>
-    <span id="ct">
-<?
-flush();
+    </p>
 
-if (!$dir=@opendir(G4_DATA_PATH.'/session')) {
-  echo "세션 디렉토리를 열지못했습니다.";
-}
+    <?
+    flush();
 
-$cnt=0;
-while($file=readdir($dir)) {
-
-    if (!strstr($file,'sess_')) continue;
-    if (strpos($file,'sess_')!=0) continue;
-
-    $session_file = G4_DATA_PATH.'/session/'.$file;
-
-    if (!$atime=@fileatime($session_file)) {
-        continue;
+    $list_tag_st = "";
+    $list_tag_end = "";
+    if (!$dir=@opendir(G4_DATA_PATH.'/session')) {
+      echo "<p>세션 디렉토리를 열지못했습니다.</p>";
+    } else {
+        $list_tag_st = "<ul>\n<li>완료됨</li>\n";
+        $list_tag_end = "</ul>\n";
     }
-    if (time() > $atime + (3600 * 6)) {  // 지난시간을 초로 계산해서 적어주시면 됩니다. default : 6시간전
-        $cnt++;
-        $return = unlink($session_file);
-        //echo "<script>document.getElementById('ct').innerHTML += '{$session_file}<br/>';</script>\n";
-        echo "{$session_file}<br/>\n";
 
-        flush();
+    $cnt=0;
+    echo $list_tag_st;
+    while($file=readdir($dir)) {
 
-        if ($cnt%10==0)
-            //echo "<script>document.getElementById('ct').innerHTML = '';</script>\n";
-            echo "\n";
+        if (!strstr($file,'sess_')) continue;
+        if (strpos($file,'sess_')!=0) continue;
+
+        $session_file = G4_DATA_PATH.'/session/'.$file;
+
+        if (!$atime=@fileatime($session_file)) {
+            continue;
+        }
+        if (time() > $atime + (3600 * 6)) {  // 지난시간을 초로 계산해서 적어주시면 됩니다. default : 6시간전
+            $cnt++;
+            $return = unlink($session_file);
+            //echo "<script>document.getElementById('ct').innerHTML += '{$session_file}<br/>';</script>\n";
+            echo "<li>{$session_file}</li>\n";
+
+            flush();
+
+            if ($cnt%10==0)
+                //echo "<script>document.getElementById('ct').innerHTML = '';</script>\n";
+                echo "\n";
+        }
     }
-}
-//echo "<script>document.getElementById('ct').innerHTML += '세션데이터 {$cnt}건 삭제 완료.<br><br>프로그램의 실행을 끝마치셔도 좋습니다.';</script>\n";
-echo "세션데이터 {$cnt}건 삭제 완료.<br><br>프로그램의 실행을 끝마치셔도 좋습니다.\n";
+    echo $list_tag_end;
+    //echo "<script>document.getElementById('ct').innerHTML += '세션데이터 {$cnt}건 삭제 완료.<br><br>프로그램의 실행을 끝마치셔도 좋습니다.';</script>\n";
+    echo "<p><span>세션데이터 {$cnt}건 삭제 완료.</span><br>프로그램의 실행을 끝마치셔도 좋습니다.</p>\n";
 ?>
-    </span>
 </div>
 
 <?php
