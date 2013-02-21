dd6444b4ac14829912e6dc00e470860d18fdbcf1
whitedot <whitedot@naver.com>
2013-02-21 10:09:06 +0900

    adm.css 를 admin.css 로 변경, 기본테이블 thead th 색상 변경, 패스워드 확인 페이지 안내문구 변경

--- skin/member/basic/password.skin.php
+++ skin/member/basic/password.skin.php
@@ -1,12 +1,25 @@
 <?
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+$delete_str = "";
+if ($w == 'x') $delete_str = "댓";
+if ($w == 'u') $g4['title'] = $delete_str."글 수정";
+else if ($w == 'd' || $w == 'x') $g4['title'] = $delete_str."글 삭제";
+else $g4['title'] = $g4['title'];
 ?>
 
 <div id="pw_confirm">
     <h1><?=$g4['title']?></h1>
     <p>
+        <? if ($w == 'u') {?>
+        <strong>작성자만 글을 수정할 수 있습니다.</strong>
+        작성자 본인이라면, 글 작성시 입력한 패스워드를 입력하여 글을 수정할 수 있습니다.
+        <? } else if ($w == 'd' || $w == 'x') { ?>
+        <strong>작성자만 글을 삭제할 수 있습니다.</strong>
+        작성자 본인이라면, 글 작성시 입력한 패스워드를 입력하여 글을 삭제할 수 있습니다.
+        <? } else { ?>
         <strong>비밀글 기능으로 보호된 글입니다.</strong>
         작성자와 관리자만 열람하실 수 있습니다. 본인이라면 패스워드를 입력하세요.
+        <? } ?>
     </p>
 
     <form name="fboardpassword" method="post" action="<? echo $action; ?>">
