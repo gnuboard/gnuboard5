56bb5f132562c01c1b1ac0e7d8fddec245751de3
whitedot <whitedot@naver.com>
2013-02-27 13:46:40 +0900

    모바일: 작업 중2

--- tail.php
+++ tail.php
@@ -1,12 +1,9 @@
 <?
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
-// 하단 파일 경로 지정 : 이 코드는 가능한 삭제하지 마십시오.
-if ($config['cf_include_tail']) {
-    if (!@include_once($config['cf_include_tail'])) {
-        die('기본환경 설정에서 하단 파일 경로가 잘못 설정되어 있습니다.');
-    }
-    return; // 이 코드의 아래는 실행을 하지 않습니다.
+if (G4_IS_MOBILE) {
+    include_once(G4_MOBILE_PATH.'/tail.php');
+    return;
 }
 ?>
     </div>
