cc342f4f9d34f6ef6a3e9103c5ceab9ab200c256
whitedot <whitedot@naver.com>
2013-02-27 14:43:09 +0900

    모바일: 작업 중3

--- head.php
+++ head.php
@@ -13,6 +13,14 @@ if (G4_IS_MOBILE) {
     include_once(G4_MOBILE_PATH.'/head.php');
     return;
 }
+
+// 상단 파일 경로 지정 : 이 코드는 가능한 삭제하지 마십시오.
+if ($config['cf_include_head']) {
+    if (!@include_once($config['cf_include_head'])) {
+        die('기본환경 설정에서 상단 파일 경로가 잘못 설정되어 있습니다.');
+    }
+    return; // 이 코드의 아래는 실행을 하지 않습니다.
+}
 ?>
 
 <header id="hd">
--- tail.php
+++ tail.php
@@ -5,6 +5,14 @@ if (G4_IS_MOBILE) {
     include_once(G4_MOBILE_PATH.'/tail.php');
     return;
 }
+
+// 하단 파일 경로 지정 : 이 코드는 가능한 삭제하지 마십시오.
+if ($config['cf_include_tail']) {
+    if (!@include_once($config['cf_include_tail'])) {
+        die('기본환경 설정에서 하단 파일 경로가 잘못 설정되어 있습니다.');
+    }
+    return; // 이 코드의 아래는 실행을 하지 않습니다.
+}
 ?>
     </div>
 </div>
