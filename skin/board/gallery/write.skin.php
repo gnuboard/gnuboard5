27761718b13f9a66543e71abe9cf7d567233219c
chicpro <chicpro@gmail.com>
2013-03-04 14:37:10 +0900

    ckeditor 노출되지 않는 오류 수정

--- skin/board/gallery/write.skin.php
+++ skin/board/gallery/write.skin.php
@@ -104,7 +104,7 @@ echo $option_hidden;
 
 <tr>
     <th scope="row"><label for="wr_content">내용<strong class="sound_only">필수</strong></label></th>
-    <td class="wr_content"><?=editor_html("wr_content", $content, $is_dhtml_editor);?></td>
+    <td class="wr_content"><? echo $editor_html; // 에디터 사용시는 에디터로, 아니면 textarea 로 노출 ?></td>
 </tr>
 
 <? for ($i=1; $is_link && $i<=G4_LINK_COUNT; $i++) { ?>
