8603ce52af4e8491b77b3a20140569de5068a298
rollydream <xgirl_87x@naver.com>
2013-03-06 15:04:04 +0900

    기본 멤버 속성 수정

--- skin/member/basic/member_confirm.skin.php
+++ skin/member/basic/member_confirm.skin.php
@@ -10,17 +10,17 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
         회원님의 정보를 안전하게 보호하기 위해 패스워드를 한번 더 확인합니다.
     </p>
 
-    <form name="fmemberconfirm" method="post" onsubmit="return fmemberconfirm_submit(this);">
-    <input type=hidden name="mb_id" value="<?=$member[mb_id]?>">
-    <input type=hidden name="w" value="u">
+    <form name="fmemberconfirm" onsubmit="return fmemberconfirm_submit(this);" method="post">
+    <input type="hidden" name="mb_id" value="<?=$member[mb_id]?>">
+    <input type="hidden" name="w" value="u">
 
     <fieldset>
         회원아이디
         <span id="mb_confirm_id"><?=$member[mb_id]?></span>
 
         <label for="confirm_mb_password">패스워드<strong class="sound_only">필수</strong></label>
-        <input type="password" id="confirm_mb_password" name="mb_password" class="fs_input" maxLength="20" size="15" required>
-        <input type="submit" id="btn_submit" class="fs_submit" value="확인">
+        <input type="password" name="mb_password" id="confirm_mb_password" required class="fs_input" size="15" maxLength="20">
+        <input type="submit" value="확인" id="btn_submit" class="fs_submit">
     </fieldset>
 
     </form>
