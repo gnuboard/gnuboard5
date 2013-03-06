9fabacfb7dfb8ec4eee2e50e51817add9bed1fc1
chicpro <chicpro@gmail.com>
2013-03-06 17:55:39 +0900

    텍스트 사이즈 조절 기능에 원래 크기로 돌리는 기능 추가

--- head.php
+++ head.php
@@ -163,4 +163,5 @@ if ($config['cf_include_head']) {
         <div id="text_size">
             <button class="no_text_resize" onclick="font_resize('container', 'increase');">크게</button>
             <button class="no_text_resize" onclick="font_resize('container', 'decrease');">작게</button>
+            <button class="no_text_resize" onclick="font_default('container');">기본</button>
         </div>
