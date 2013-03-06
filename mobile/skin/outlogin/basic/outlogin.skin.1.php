1a19d33f3fe69164360dbbd76274fa417deca9ca
whitedot <whitedot@naver.com>
2013-03-06 11:58:51 +0900

    문서 최상단 로그인 중 메세지 삽입, #307 의견5 에 따른 처리

--- mobile/skin/outlogin/basic/outlogin.skin.1.php
+++ mobile/skin/outlogin/basic/outlogin.skin.1.php
@@ -12,10 +12,10 @@ if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
         <input type="password" id="ol_pw" name="mb_password" class="required" maxlength="20" required placeholder="패스워드(필수)" title="패스워드(필수)">
         <input type="submit" id="ol_submit" value="로그인">
         <div id="ol_svc">
-            <a href="<?=G4_BBS_URL?>/register.php"><b>회원가입</b></a>
-            <a href="<?=G4_BBS_URL?>/password_lost.php" id="ol_password_lost">정보찾기</a>
             <input type="checkbox" id="auto_login" name="auto_login" value="1">
             <label for="auto_login" id="auto_login_label">자동로그인</label>
+            <a href="<?=G4_BBS_URL?>/register.php"><b>회원가입</b></a>
+            <a href="<?=G4_BBS_URL?>/password_lost.php" id="ol_password_lost">정보찾기</a>
         </div>
     </fieldset>
     </form>
