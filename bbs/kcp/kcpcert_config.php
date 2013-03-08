e652434f1f5236d5354ba39d76aa360d047dcda3
chicpro <chicpro@gmail.com>
2013-03-08 17:48:26 +0900

    kcp 인증 사이트코드를 $config 변수로 수정

--- bbs/kcp/kcpcert_config.php
+++ bbs/kcp/kcpcert_config.php
@@ -8,18 +8,16 @@ $home_dir = G4_BBS_PATH.'/kcp'; // ct_cli 절대경로 ( bin 전까지 )
 //web_siteid 값이 없으면 KCP 에서 지정한 값으로 설정됨
 $web_siteid = '';
 
-$kcpcert_test = true;
-
-if($kcpcert_test) { // 테스트
+if($config['cf_kcpcert_site_cd'] && $config['cf_kcpcert_site_cd'] != 'S6186') { // 실인증
+    $site_cd = $config['cf_kcpcert_site_cd'];
+    $cert_url = 'https://cert.kcp.co.kr/kcp_cert/cert_view.jsp';
+} else { // 테스트인증
     $site_cd = 'S6186';
     $cert_url = 'https://testcert.kcp.co.kr/kcp_cert/cert_view.jsp';
-} else { // 실인증
-    $site_cd = '';
-    $cert_url = 'https://cert.kcp.co.kr/kcp_cert/cert_view.jsp';
 }
 
 if(!$site_cd)
-    alert('KCP 휴대폰인증 사이트코드가 없습니다.', G4_URL);
+    alert('KCP 휴대폰인증 사이트코드가 없습니다.\\관리자 > 기본환경설정에 사이트코드를 입력해 주십시오.', G4_URL);
 
 // KCP 인증 라이브러리
 require G4_BBS_PATH.'/kcp/lib/ct_cli_lib.php';
