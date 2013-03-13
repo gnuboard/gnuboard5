176cfad80707714b868058b611840f35b8048a1b
chicpro <chicpro@gmail.com>
2013-03-13 09:57:37 +0900

    warning 관련 header 정의 위치 변경

--- install/install_db.php
+++ install/install_db.php
@@ -1,15 +1,14 @@
 <?
 set_time_limit(0);
-
-include_once ('../config.php');
-include_once ('./install.inc.php');
-
 $gmnow = gmdate('D, d M Y H:i:s') . ' GMT';
 header('Expires: 0'); // rfc2616 - Section 14.21
 header('Last-Modified: ' . $gmnow);
 header('Cache-Control: no-store, no-cache, must-revalidate'); // HTTP/1.1
 header('Cache-Control: pre-check=0, post-check=0, max-age=0'); // HTTP/1.1
 header('Pragma: no-cache'); // HTTP/1.0
+
+include_once ('../config.php');
+include_once ('./install.inc.php');
 
 //print_r($_POST); exit;
 
