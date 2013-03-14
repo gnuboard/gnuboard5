f8326df1df82aa11a3747a9668d4704ee169a688
whitedot <whitedot@naver.com>
2013-03-14 16:48:04 +0900

    사용자: fs_input fs_submit frm_input btn_submit 으로 수정

--- skin/board/gallery/list.skin.php
+++ skin/board/gallery/list.skin.php
@@ -168,8 +168,8 @@ include_once(G4_LIB_PATH.'/thumbnail.lib.php');
         <option value="wr_name,1"<?=get_selected($sfl, "wr_name,1");?>>글쓴이</option>
         <option value="wr_name,0"<?=get_selected($sfl, "wr_name,0");?>>글쓴이(코)</option>
     </select>
-    <input type="text" name="stx" value="<?=stripslashes($stx)?>" title="검색어(필수)" required class="fs_input required" size="15" maxlength="15">
-    <input type="submit" class="fs_submit" value="검색">
+    <input type="text" name="stx" value="<?=stripslashes($stx)?>" title="검색어(필수)" required class="frm_input required" size="15" maxlength="15">
+    <input type="submit" value="검색" class="btn_submit">
     </form>
 </fieldset>
 
