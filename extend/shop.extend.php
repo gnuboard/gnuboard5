be753cb6866c4b579de6cc5ca38a7213562e185e
chicpro <chicpro@gmail.com>
2013-03-21 15:04:04 +0900

    g4s 2013-03-21일자 코드 반영

--- extend/shop.extend.php
+++ extend/shop.extend.php
@@ -1,9 +1,6 @@
 <?
 if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
 
-if(!G4_USE_SHOP)
-    return;
-
 include_once(G4_LIB_PATH.'/shop.lib.php');
 
 //------------------------------------------------------------------------------
