6e7b9469f4dc5a887aac3d46c2d81e47f08ebe6c
chicpro <chicpro@gmail.com>
2013-03-26 16:30:23 +0900

    bo_image_head 필드 관련 코드 삭제

--- bbs/board_head.php
+++ bbs/board_head.php
@@ -1,18 +1,6 @@
 <?
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
-/*
-// 게시판 관리의 상단 파일 경로
-if ($board['bo_include_head']) {
-    @include ($board['bo_include_head']);
-}
-
-// 게시판 관리의 상단 이미지 경로
-if ($board['bo_image_head']) {
-    echo '<img src="'.G4_DATA_PATH.'/file/'.$bo_table.'/'.$board['bo_image_head'].'">';
-}
-*/
-
 // 게시판 관리의 상단 내용
 if (G4_IS_MOBILE) {
     // 모바일의 경우 설정을 따르지 않는다.
--- bbs/board_tail.php
+++ bbs/board_tail.php
@@ -1,18 +1,6 @@
 <?
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
-/*
-// 게시판 관리의 하단 내용
-if ($board['bo_content_tail']) {
-    echo stripslashes($board['bo_content_tail']);
-}
-
-// 게시판 관리의 하단 이미지 경로
-if ($board['bo_image_tail']) {
-    echo '<img src="'.G4_DATA_PATH.'/file/'.$bo_table.'/'.$board['bo_image_tail'].'">';
-}
-*/
-
 // 게시판 관리의 하단 파일 경로
 if (G4_IS_MOBILE) {
     echo stripslashes($board['bo_mobile_content_tail']);
