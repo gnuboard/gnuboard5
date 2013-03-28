203792bd6d427e6ddcebff12d381124e19319e72
whitedot <whitedot@naver.com>
2013-03-28 09:44:51 +0900

    모바일: 음성캡챠 audio #412 에 따른 처리

--- bbs/gcaptcha/gcaptcha.lib.php
+++ bbs/gcaptcha/gcaptcha.lib.php
@@ -183,12 +183,9 @@ function captcha_html($class='captcha')
     $html .= "\n".'<script src="'.G4_GCAPTCHA_URL.'/gcaptcha.js"></script>';
     $html .= '<fieldset id="captcha" class="'.$class.'">';
     $html .= '<legend class="sound_only">자동등록방지</legend>';
+    if (G4_IS_MOBILE) $html .= '<audio src="'.$mp3_file_url.'?_='.$rand.'" controls></audio>';
     $html .= '<img src="'.$jpg_file_url.'?_='.$rand.'" alt="자동등록방지 숫자">';
-    if (G4_IS_MOBILE) {
-        $html .= '<audio src="'.$mp3_file_url.'?_='.$rand.'" controls></audio>';
-    } else {
-        $html .= '<a href="'.$mp3_file_url.'?_='.$rand.'" id="captcha_mp3" target="_blank"><img src="'.G4_GCAPTCHA_URL.'/img/sound.gif" alt="숫자를 음성으로 듣기"></a>';
-    }
+    if (!G4_IS_MOBILE) $html .= '<a href="'.$mp3_file_url.'?_='.$rand.'" id="captcha_mp3" target="_blank"><img src="'.G4_GCAPTCHA_URL.'/img/sound.gif" alt="숫자를 음성으로 듣기"></a>';
     $html .= '<input type="text" id="captcha_key" name="captcha_key" class="captcha_box frm_input" size="6" maxlength="6" required title="자동등록방지 숫자 입력">';
     $html .= '<p class="sound_only">자동등록방지 숫자를 순서대로 입력하세요.</p>';
     $html .= '</fieldset>';
