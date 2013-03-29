31f5c9b7edf7dc514136a6feb852c7abf07773d6
rollydream <xgirl_87x@naver.com>
2013-03-29 14:39:38 +0900

    쇼핑몰관리자 : #10 이벤트관리 이벤트입력 표준화 및 스타일 세부 수정

--- adm/shop_admin/itemeventform.php
+++ adm/shop_admin/itemeventform.php
@@ -58,14 +58,15 @@ include_once (G4_ADMIN_PATH.'/admin.head.php');
             echo "&nbsp;&nbsp;&nbsp;";
             echo icon("보기", G4_SHOP_URL."/event.php?ev_id={$ev['ev_id']}");
             ?>
+        </td>
     </tr>
     <? } ?>
     <tr>
-        <th scope="col">출력스킨</th>
+        <th scope="row"><label for="ev_skin">출력스킨</label></th>
         <td colspan="3">
             <?=help("기본으로 제공하는 스킨은 $cart_dir/list.skin.*.php 입니다.\n\n$cart_dir/list.php&skin=userskin.php 처럼 직접 만든 스킨을 사용할 수도 있습니다.");?>
-            <select name=ev_skin>
-            <?  echo get_list_skin_options("^list\.skin\.(.*)\.php", G4_SHOP_PATH); ?>
+            <select name="ev_skin" id="ev_skin">
+                <?  echo get_list_skin_options("^list\.skin\.(.*)\.php", G4_SHOP_PATH); ?>
             </select>
             <? if ($w == 'u') { ?>
             <script>document.all.ev_skin.value='<?=$ev['ev_skin']?>';</script>
@@ -73,29 +74,29 @@ include_once (G4_ADMIN_PATH.'/admin.head.php');
         </td>
     </tr>
     <tr>
-        <th scope="col"><label for="ev_img_width">출력이미지 폭</label></th>
+        <th scope="row"><label for="ev_img_width">출력이미지 폭</label></th>
         <td>
               <input type="text" name="ev_img_width" value="<? echo $ev['ev_img_width'] ?>" id="ev_img_width" class="frm_input" size="5"> 픽셀
         </td>
-        <th scope="col"><label for="ev_img_height">출력이미지 높이</label></th>
+        <th scope="row"><label for="ev_img_height">출력이미지 높이</label></th>
         <td>
           <input type="text" name="ev_img_height" value="<? echo $ev['ev_img_height'] ?>" id="ev_img_height" class="frm_input" size="5"> 픽셀
         </td>
     </tr>
     <tr>
-        <th scope="col"><label for="ev_list_mod">1라인 이미지수</label></th>
+        <th scope="row"><label for="ev_list_mod">1라인 이미지수</label></th>
         <td>
             <?=help("1라인에 설정한 값만큼의 상품을 출력하지만 스킨에 따라 1라인에 하나의 상품만 출력할 수도 있습니다.", 50);?>
             <input type="text" name="ev_list_mod" value="<? echo $ev['ev_list_mod'] ?>" id="ev_list_mod" class="frm_input" size="3"> 개
         </td>
-        <th scope="col"><label for="ev_list_row">총라인수</label></th>
+        <th scope="row"><label for="ev_list_row">총라인수</label></th>
         <td>
             <?=help("한페이지에 몇라인을 출력할것인지를 설정합니다.\n\n한페이지에서 표시하는 상품수는 (1라인 이미지수 x 총라인수) 입니다.");?>
             <input type="text" name="ev_list_row" value="<? echo $ev['ev_list_row'] ?>" id="ev_list_row" class="frm_input" size="3"> 라인
         </td>
     </tr>
     <tr>
-        <th scope="col"><label for="ev_use">사용</label></th>
+        <th scope="row"><label for="ev_use">사용</label></th>
         <td colspan="3">
             <?=help("사용하지 않으면 왼쪽의 이벤트 메뉴와 이벤트리스트 페이지에 접근할 수 없습니다.");?>
             <select name="ev_use" id="ev_use">
@@ -106,13 +107,13 @@ include_once (G4_ADMIN_PATH.'/admin.head.php');
         </td>
     </tr>
     <tr>
-        <th scope="col"><label for="ev_subject">이벤트제목</label></th>
+        <th scope="row"><label for="ev_subject">이벤트제목</label></th>
         <td colspan="3">
             <input type="text" name="ev_subject" value="<? echo htmlspecialchars2($ev['ev_subject']) ?>" id="ev_subject" required class="required frm_input"  size="60">
         </td>
     </tr>
     <tr>
-        <th scope="col">메뉴이미지</th>
+        <th scope="row">메뉴이미지</th>
         <td colspan="3">
             <?=help("쇼핑몰 왼쪽 메뉴에 텍스트 메뉴 대신 이미지로 넣을 경우 사용합니다.");?>
             <input type="file" name="ev_mimg">
@@ -128,7 +129,7 @@ include_once (G4_ADMIN_PATH.'/admin.head.php');
     </tr>
     <? if ($mimg_str) { echo "<tr><td></td><td colspan=\"3\">$mimg_str</td></tr>"; } ?>
     <tr>
-        <th scope="col">상단이미지</th>
+        <th scope="row">상단이미지</th>
         <td colspan="3">
             <?=help("이벤트 페이지 상단에 업로드 한 이미지를 출력합니다.");?>
             <input type="file" name="ev_himg">
@@ -144,7 +145,7 @@ include_once (G4_ADMIN_PATH.'/admin.head.php');
     </tr>
     <? if ($himg_str) { echo "<tr><td colspan=\"4\">$himg_str</td></tr>"; } ?>
     <tr>
-        <th scope="col">하단이미지</th>
+        <th scope="row">하단이미지</th>
         <td colspan="3">
             <?=help("이벤트 페이지 하단에 업로드 한 이미지를 출력합니다.");?>
             <input type="file" name="ev_timg">
@@ -160,13 +161,13 @@ include_once (G4_ADMIN_PATH.'/admin.head.php');
     </tr>
     <? if ($timg_str) { echo "<tr><td colspan=\"4\">$timg_str</td></tr>"; } ?>
     <tr>
-        <th scope="col">상단내용</th>
+        <th scope="row">상단내용</th>
         <td colspan="3">
             <?=editor_html('ev_head_html', $ev['ev_head_html']);?>
         </td>
     </tr>
     <tr>
-        <th scope="col">하단내용</th>
+        <th scope="row">하단내용</th>
         <td colspan="3">
             <?=editor_html('ev_tail_html', $ev['ev_tail_html']);?>
         </td>
