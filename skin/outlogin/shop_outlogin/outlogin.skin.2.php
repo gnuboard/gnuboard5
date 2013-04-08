c6b0b3c5d2cf854a501b423e96b4b1e7fd0003be
whitedot <whitedot@naver.com>
2013-04-08 16:52:22 +0900

    사용하지 않는 img 파일들 정리 및 아웃로그인 설문조사 스킨 동기화

--- skin/outlogin/shop_outlogin/outlogin.skin.2.php
+++ skin/outlogin/shop_outlogin/outlogin.skin.2.php
@@ -1,25 +1,45 @@
 <?
-if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가 
+if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
 ?>
 
-<table width=187 cellpadding=1 cellspacing=0 bgcolor=#cccccc>
-<tr>
-    <td>
-        <table width=100% bgcolor=#FFFFFF cellpadding=0 cellspacing=0 height=97>
-        <tr><td height=6></td></tr>
-        <tr><td align=center height=20><FONT COLOR="#0083B9"><?=$nick?></FONT>님 
-            <? if ($is_admin == "super" || $is_auth) { ?><a href="<?=$g4[admin_path]?>/"><img src="<?=$outlogin_skin_path?>/img/admin.gif" width="33" height="15" border="0" align="absmiddle"></a><? } ?></td></tr>
-        <tr><td align=center height=20><a href="javascript:win_point();"><font color="#737373">포인트 : <b><?=$point?></b> 점</font></a></td></tr>
-        <tr><td align=center height=25>
-            <a href="<?=$g4[bbs_path]?>/logout.php"><img src='<?=$outlogin_skin_path?>/img/btn_logout.gif' border=0></a>
-            <a href="<?=$g4[bbs_path]?>/member_confirm.php?url=register_form.php"><img src='<?=$outlogin_skin_path?>/img/btn_modify.gif' border=0></a>
-            </td></tr>
-        <tr><td align=center>&nbsp;
-                <a href="javascript:win_memo();"><FONT COLOR="#BB3D3E">쪽지 (<?=$memo_not_read?>)</FONT></a>&nbsp;&nbsp;
-                <a href="javascript:win_scrap();"><img src='<?=$outlogin_skin_path?>/img/btn_scrap.gif' border=0 align=absmiddle></a>
-            </td></tr>
-        <tr><td height=7 colspan=3></td></tr>
-        </table>
-    </td>
-</tr>
-</table>
+<link rel="stylesheet" href="<?=$outlogin_skin_url?>/style.css">
+
+<!-- 로그인 후 외부로그인 시작 -->
+<section id="ol_after" class="ol">
+    <header id="ol_after_hd">
+        <h2>나의 회원정보</h2>
+        <strong><?=$nick?>님</strong>
+        <? if ($is_admin == 'super' || $is_auth) { ?><a href="<?=G4_ADMIN_URL?>" class="btn_admin">관리자 모드</a><? } ?>
+    </header>
+    <ul id="ol_after_private">
+        <li>
+            <a href="<?=G4_BBS_URL?>/memo.php" target="_blank" id="ol_after_memo" class="win_memo">
+                <span class="sound_only">안 읽은 </span>쪽지
+                <strong><?=$memo_not_read?></strong>
+            </a>
+        </li>
+        <li>
+            <a href="<?=G4_BBS_URL?>/point.php" target="_blank" id="ol_after_pt" class="win_point">
+                포인트
+                <strong><?=$point?></strong>
+            </a>
+        </li>
+        <li>
+            <a href="<?=G4_BBS_URL?>/scrap.php" target="_blank" id="ol_after_scrap" class="win_scrap">스크랩</a>
+        </li>
+    </ul>
+    <footer id="ol_after_ft">
+        <a href="<?=G4_BBS_URL?>/member_confirm.php?url=register_form.php" id="ol_after_info">정보수정</a>
+        <a href="<?=G4_BBS_URL?>/logout.php" id="ol_after_logout">로그아웃</a>
+    </footer>
+</section>
+
+<script>
+// 탈퇴의 경우 아래 코드를 연동하시면 됩니다.
+function member_leave()
+{
+    if (confirm("정말 회원에서 탈퇴 하시겠습니까?"))
+        location.href = "<?=G4_BBS_URL?>/member_confirm.php?url=member_leave.php";
+}
+</script>
+<!-- 로그인 후 외부로그인 끝 -->
--- skin/poll/shop_poll/poll.skin.php
+++ skin/poll/shop_poll/poll.skin.php
@@ -1,45 +1,36 @@
 <?
-if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가 
-
-global $is_admin;
-
-// 투표번호가 넘어오지 않았다면 가장 큰(최근에 등록한) 투표번호를 얻는다
-if (!$po_id) {
-    $po_id = $config[cf_max_po_id];
-
-    if (!$po_id) return;
-}
-
-$po = sql_fetch(" select * from $g4[poll_table] where po_id = '$po_id' ");
+if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
 ?>
 
-<table background='<?=$poll_skin_path?>/img/bg_poll.gif' width=100%>
-<form name="fpoll" method="post" action="<?=$g4[bbs_path]?>/poll_update.php" onsubmit="return fpoll_submit(this);" target="winPoll">
+<form name="fpoll" action="<?=G4_BBS_URL?>/poll_update.php" onsubmit="return fpoll_submit(this);" target="win_poll" method="post">
 <input type="hidden" name="po_id" value="<?=$po_id?>">
 <input type="hidden" name="skin_dir" value="<?=$skin_dir?>">
-<tr><td style='padding:4px' align=center><img src='<?=$poll_skin_path?>/img/bar_poll.gif'></td></tr>
-<tr><td align=center style='padding-bottom:4px'>
-    <table bgcolor=#FFFFFF width=165 cellpadding=0 cellspacing=0>
-    <tr><td height=5></td></tr>
-    <tr><td height=25 style='padding:2px;'>&nbsp;<img src='<?=$poll_skin_path?>/img/icon_poll_q.gif' align=bottom> <?=$po[po_subject]?> <? if ($is_admin == "super") { ?><a href="<?=$g4[admin_path]?>/poll_form.php?w=u&po_id=<?=$po_id?>"><img src="<?=$poll_skin_path?>/img/admin.gif" width="33" height="15" border=0 align=absmiddle></a></center><? } ?></td></tr>
-    <tr><td height=5></td></tr>
-
-    <? for ($i=1; $i<=9 && $po["po_poll{$i}"]; $i++) { ?>
-    <tr><td><input type="radio" name="gb_poll" value="<?=$i?>"> <?=$po['po_poll'.$i]?></td></tr>
-    <? } ?>
-
-    <tr><td align=center height=40>
-        <input type="image" src="<?=$poll_skin_path?>/img/poll_button.gif" border="0">
-        <a href="javascript:;" onclick="poll_result('<?=$po_id?>');"><img src="<?=$poll_skin_path?>/img/poll_view.gif" border="0"></td></tr>
-    </table>
-    </td>
-</tr>
+<section id="poll">
+    <header>
+        <h2>설문조사</h2>
+        <? if ($is_admin == "super") { ?><a href="<?=G4_ADMIN_URL?>/poll_form.php?w=u&amp;po_id=<?=$po_id?>" class="btn_admin">설문조사 관리</a><? } ?>
+        <p><?=$po['po_subject']?></p>
+    </header>
+    <ul>
+        <? for ($i=1; $i<=9 && $po["po_poll{$i}"]; $i++) { ?>
+        <li><input type="radio" name="gb_poll" value="<?=$i?>" id="gb_poll_<?=$i?>"> <label for="gb_poll_<?=$i?>"><?=$po['po_poll'.$i]?></label></li>
+        <? } ?>
+    </ul>
+    <footer>
+        <input type="submit" value="투표하기">
+        <a href="<?=G4_BBS_URL."/poll_result.php?po_id=$po_id&amp;skin_dir=$skin_dir"?>" target="_blank" onclick="poll_result(this.href); return false;">결과보기</a>
+    </footer>
+</section>
 </form>
-</table>
 
-<script language='JavaScript'>
+<script>
 function fpoll_submit(f)
 {
+    <?
+    if ($member['mb_level'] < $po['po_level'])
+        echo " alert('권한 {$po['po_level']} 이상의 회원만 투표에 참여하실 수 있습니다.'); return false; ";
+    ?>
+
     var chk = false;
     for (i=0; i<f.gb_poll.length;i ++) {
         if (f.gb_poll[i].checked == true) {
@@ -48,27 +39,22 @@ function fpoll_submit(f)
         }
     }
 
-    <?
-    if ($member[mb_level] < $po[po_level])
-        echo " alert('권한 $po[po_level] 이상의 회원만 투표에 참여하실 수 있습니다.'); return false; ";
-    ?>
-
     if (!chk) {
-        alert("항목을 선택하세요");
+        alert("투표하실 설문항목을 선택하세요");
         return false;
     }
 
-    win_poll();
+    win_poll(f.action);
     return true;
 }
 
-function poll_result(po_id)
+function poll_result(url)
 {
     <?
-    if ($member[mb_level] < $po[po_level])
-        echo " alert('권한 $po[po_level] 이상의 회원만 결과를 보실 수 있습니다.'); return false; ";
+    if ($member['mb_level'] < $po['po_level'])
+        echo " alert('권한 {$po['po_level']} 이상의 회원만 결과를 보실 수 있습니다.'); return false; ";
     ?>
 
-    win_poll("<?=$g4[bbs_path]?>/poll_result.php?po_id="+po_id+"&skin_dir="+document.fpoll.skin_dir.value);
+    win_poll(url);
 }
 </script>
