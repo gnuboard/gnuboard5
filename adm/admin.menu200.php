25f3c48419a6f642a0c0782013d1df90164525e1
whitedot <whitedot@naver.com>
2013-04-09 10:22:32 +0900

    관리자: 메뉴 순서 #48 및 스타일 변경

--- adm/admin.menu200.php
+++ adm/admin.menu200.php
@@ -1,10 +1,10 @@
 <?
 $menu['menu200'] = array (
-    array('200000', '회원관리',     G4_ADMIN_URL.'/member_list.php',    'member'),
-    array('200100', '회원관리',     G4_ADMIN_URL.'/member_list.php',    'mb_list'),
-    array('200200', '포인트관리',   G4_ADMIN_URL.'/point_list.php',     'mb_point'),
-    array('200300', '회원메일발송', G4_ADMIN_URL.'/mail_list.php',      'mb_mail'),
-    array('200800', '접속자집계',   G4_ADMIN_URL.'/visit_list.php',     'mb_visit'),
-    array('200900', '투표관리',     G4_ADMIN_URL.'/poll_list.php',      'mb_poll')
+    array('200000', '회원관리', G4_ADMIN_URL.'/member_list.php', 'member'),
+    array('200100', '회원관리', G4_ADMIN_URL.'/member_list.php', 'mb_list'),
+    array('200300', '회원메일발송', G4_ADMIN_URL.'/mail_list.php', 'mb_mail'),
+    array('200800', '접속자집계', G4_ADMIN_URL.'/visit_list.php', 'mb_visit', 1),
+    array('200200', '포인트관리', G4_ADMIN_URL.'/point_list.php', 'mb_point'),
+    array('200900', '투표관리', G4_ADMIN_URL.'/poll_list.php', 'mb_poll')
 );
 ?>
