ea017fe74031d4c83eb166a4bfe854d1564f57b3
whitedot <whitedot@naver.com>
2013-04-11 10:28:08 +0900

    쇼핑몰관리자: 쇼핑몰관리 인덱스 표준화 및 스타일 작업 중

--- adm/shop_admin/index.php
+++ adm/shop_admin/index.php
@@ -8,243 +8,220 @@ $g4['title'] = ' 쇼핑몰관리';
 include_once (G4_ADMIN_PATH.'/admin.head.php');
 ?>
 
-<table width=100%>
-<tr>
-	<td width=50% valign=top>
-        <table width=100% cellpadding=0 cellspacing=0>
-        <tr>
-            <td width=70%><?//=subtitle("입금완료 미배송내역")?></td>
-            <td width=30% align=right><a href='./deliverylist.php?sort1=od_invoice&sort2=asc&chk_misu=1'><img src='<?=G4_ADMIN_URL?>/img/icon_more.gif' border=0></a>&nbsp;</td>
-        </tr>
-        </table>
+<section class="cbox">
+    <h2>입금완료 미배송내역</h2>
 
-        <table width=100% cellpadding=0 cellspacing=0>
-        <tr><td colspan=5 height=2 bgcolor=#0E87F9></td></tr>
-        <tr align=center class=ht>
-            <td width=80>주문번호</td>
-            <td>주문자</td>
-            <td width=90>입금액</td>
-            <td width=90>결제방법</td>
-            <td width=40>수정</td>
-        </tr>
-        <tr><td colspan=5 height=1 bgcolor=#CCCCCC></td></tr>
-        <?
-        // 미수금이 없고 운송장번호가 없는 자료를 구함
-        $sql = " select a.od_id,
-                        a.*, "._MISU_QUERY_."
-                   from {$g4['shop_order_table']} a
-                   left join {$g4['shop_cart_table']} b on (b.uq_id=a.uq_id)
-                  group by a.od_id
-                  /*having misu <= 0 and a.od_invoice = '' and ordercancel = 0*/
-                  /*having orderamount - receiptamount = 0 and a.od_invoice = ''*/
-                  having misu <= 0 and a.od_invoice = ''
-                  order by a.od_id desc
-                  limit $max_limit ";
-        $result = sql_query($sql);
-        for ($i=0; $row=sql_fetch_array($result); $i++)
+    <table>
+    <thead>
+    <tr>
+        <th scope="col">주문번호</th>
+        <th scope="col">주문자</th>
+        <th scope="col">입금액</th>
+        <th scope="col">결제방법</th>
+        <th scope="col">수정</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?
+    // 미수금이 없고 운송장번호가 없는 자료를 구함
+    $sql = " select a.od_id,
+                    a.*, "._MISU_QUERY_."
+               from {$g4['shop_order_table']} a
+               left join {$g4['shop_cart_table']} b on (b.uq_id=a.uq_id)
+              group by a.od_id
+              /*having misu <= 0 and a.od_invoice = '' and ordercancel = 0*/
+              /*having orderamount - receiptamount = 0 and a.od_invoice = ''*/
+              having misu <= 0 and a.od_invoice = ''
+              order by a.od_id desc
+              limit $max_limit ";
+    $result = sql_query($sql);
+    for ($i=0; $row=sql_fetch_array($result); $i++)
+    {
+        $sql1 = " select * from {$g4['member_table']} where mb_id = '{$row['mb_id']}' ";
+        $row1 = sql_fetch($sql1);
+
+        $name = get_sideview($row['mb_id'], get_text($row['od_name']), $row1['mb_email'], $row1['mb_homepage']);
+
+        $settle_method = "";
+        if ($row['od_settle_case'])
         {
-            $sql1 = " select * from {$g4['member_table']} where mb_id = '{$row['mb_id']}' ";
-            $row1 = sql_fetch($sql1);
-
-            $name = get_sideview($row['mb_id'], get_text($row['od_name']), $row1['mb_email'], $row1['mb_homepage']);
-
-            $settle_method = "";
-            if ($row['od_settle_case'])
-            {
-                $settle_method = $row['od_settle_case'];
-            }
-            else
-            {
-                if ($row['od_receipt_bank'])  $settle_method .= "무통장";
-                if ($row['od_receipt_card'])  $settle_method .= "카드";
-                if ($row['od_receipt_point']) $settle_method .= "포인트";
-            }
-
-            $list = $i%2;
-            echo "
-            <tr align=center class='list$list ht'>
-                <td>{$row['od_id']}</td>
-                <td>$name</td>
-                <td align=right>".display_amount($row['receiptamount'])."&nbsp;</td>
-                <td>$settle_method</td>
-                <td>".icon("수정", "./orderform.php?od_id={$row['od_id']}")."</td>
-            </tr>
-            ";
+            $settle_method = $row['od_settle_case'];
         }
-
-        if ($i == 0) {
-            echo "<tr><td colspan=5 align=center class=ht>자료가 없습니다.</td></tr>";
-        }
-        ?>
-            <tr><td colspan=5 height=1 bgcolor=#CCCCCC></td></tr>
-        </table>
-    </td>
-	<td width=1%></td>
-	<td width=49% valign=top>
-        <table width=100% cellpadding=0 cellspacing=0>
-        <tr>
-            <td width=70%><?//=subtitle("미입금 주문내역")?></td>
-            <td width=30% align=right><a href='./orderlist.php?sort1=receiptamount&sort2=asc'><img src='<?=G4_ADMIN_URL?>/img/icon_more.gif' border=0></a>&nbsp;</td>
-        </tr>
-        </table>
-
-        <table width=100%  cellpadding=0 cellspacing=0>
-        <tr><td colspan=5 height=2 bgcolor=#0E87F9></td></tr>
-        <tr align=center class=ht>
-            <td width=80>주문번호</td>
-            <td>주문자</td>
-            <td width=90>주문액</td>
-            <td width=90>결제방법</td>
-            <td width=40>수정</td>
-        </tr>
-        <tr><td colspan=5 height=1 bgcolor=#CCCCCC></td></tr>
-        <?
-        // 미수금이 있고 송장번호가 없는 자료를 구함
-        $sql = " select a.od_id,
-                        a.*, "._MISU_QUERY_."
-                   from {$g4['shop_order_table']} a
-                   left join {$g4['shop_cart_table']} b on (b.uq_id=a.uq_id)
-                  group by a.od_id
-                  /* having receiptamount <= 0 */
-                  having misu > 0
-                  order by a.od_id desc
-                  limit $max_limit ";
-        $result = sql_query($sql);
-        for ($i=0; $row=sql_fetch_array($result); $i++)
+        else
         {
-            $sql1 = " select * from {$g4['member_table']} where mb_id = '{$row['mb_id']}' ";
-            $row1 = sql_fetch($sql1);
-
-            $name = get_sideview($row['mb_id'], get_text($row['od_name']), $row1['mb_email'], $row1['mb_homepage']);
-
-            $settle_method = "";
-            if ($row['od_settle_case'])
-            {
-                $settle_method = $row['od_settle_case'];
-            }
-            else
-            {
-                if ($row['od_temp_bank'])   $settle_method  .= "무통장";
-                if ($row['od_temp_card'])   $settle_method .= "카드";
-                if ($row['od_temp_milage']) $settle_method .= "포인트";
-            }
-
-            $list = $i%2;
-            echo "
-            <tr align=center class='list$list ht'>
-                <td><a href='./orderstatuslist.php?sort1=od_id&sel_field=od_id&search={$row['od_id']}'>{$row['od_id']}</a></td>
-                <td>$name</td>
-                <td align=right>".display_amount($row['orderamount'])."&nbsp;</td>
-                <td>$settle_method</td>
-                <td>".icon("수정", "./orderform.php?od_id={$row['od_id']}")."</td>
-            </tr>";
+            if ($row['od_receipt_bank'])  $settle_method .= "무통장";
+            if ($row['od_receipt_card'])  $settle_method .= "카드";
+            if ($row['od_receipt_point']) $settle_method .= "포인트";
         }
+    ?>
+    <tr>
+        <td><?=$row['od_id']?></td>
+        <td><?=$name?></td>
+        <td><?=display_amount($row['receiptamount'])?></td>
+        <td><?=$settle_method?></td>
+        <td><a href="./orderform.php?od_id=<?=$row['od_id']?>">수정</a></td>
+    </tr>
+    <?
+    }
+    if ($i == 0) echo '<tr><td colspan="5" class="empty_table">자료가 없습니다.</td></tr>';
+    ?>
+    </tbody>
+    </table>
 
-        if ($i == 0)
-            echo "<tr><td colspan=5 align=center class=ht>자료가 없습니다.</td></tr>";
-        ?>
-        <tr><td colspan=5 height=1 bgcolor=#CCCCCC></td></tr>
-        </table>
+    <div class="btn_ft">
+        <a href="./deliverylist.php?sort1=od_invoice&amp;sort2=asc&amp;chk_misu=1">입금완료 미배송내역 더보기</a>
+    </div>
+</section>
 
+<section class="cbox">
+    <h2>미입금 주문내역</h2>
 
-    </td>
-</tr>
-</table><br>
+    <table>
+    <thead>
+    <tr>
+        <th scope="col">주문번호</th>
+        <th scope="col">주문자</th>
+        <th scope="col">주문액</th>
+        <th scope="col">결제방법</th>
+        <th scope="col">수정</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?
+    // 미수금이 있고 송장번호가 없는 자료를 구함
+    $sql = " select a.od_id,
+                    a.*, "._MISU_QUERY_."
+               from {$g4['shop_order_table']} a
+               left join {$g4['shop_cart_table']} b on (b.uq_id=a.uq_id)
+              group by a.od_id
+              /* having receiptamount <= 0 */
+              having misu > 0
+              order by a.od_id desc
+              limit $max_limit ";
+    $result = sql_query($sql);
+    for ($i=0; $row=sql_fetch_array($result); $i++)
+    {
+        $sql1 = " select * from {$g4['member_table']} where mb_id = '{$row['mb_id']}' ";
+        $row1 = sql_fetch($sql1);
 
+        $name = get_sideview($row['mb_id'], get_text($row['od_name']), $row1['mb_email'], $row1['mb_homepage']);
 
-<table width=100%>
-<tr>
-	<td width=50% valign=top>
-        <table width=100% cellpadding=0 cellspacing=0>
-        <tr>
-            <td width=70%><?//=subtitle("사용후기")?></td>
-            <td width=30% align=right><a href='./itempslist.php?sort1=is_confirm&sort2=asc'><img src='<?=G4_ADMIN_URL?>/img/icon_more.gif' border=0></a>&nbsp;</td>
-        </tr>
-        </table>
-
-        <table width=100% cellpadding=0 cellspacing=0>
-        <tr><td colspan=3 height=2 bgcolor=#0E87F9></td></tr>
-        <tr align=center class=ht>
-        	<td width=100>회원명</td>
-        	<td>제목</td>
-        	<td width=40>수정</td>
-        </tr>
-        <tr><td colspan=3 height=1 bgcolor=#CCCCCC></td></tr>
-        <?
-        $sql = " select * from {$g4[shop_item_ps_table]}
-                  where is_confirm = 0
-                  order by is_id desc
-                  limit $max_limit ";
-        $result = sql_query($sql);
-        for ($i=0; $row=sql_fetch_array($result); $i++)
+        $settle_method = "";
+        if ($row['od_settle_case'])
         {
-            $sql1 = " select * from {$g4['member_table']} where mb_id = '{$row['mb_id']}' ";
-            $row1 = sql_fetch($sql1);
-
-            $name = get_sideview($row['mb_id'], get_text($row['is_name']), $row1['mb_email'], $row1['mb_homepage']);
-
-            $list = $i%2;
-            echo "
-            <tr align=center class='list$list ht'>
-                <td align=center>$name</td>
-                <td>".cut_str($row['is_subject'],40)."</td>
-                <td align=center>".icon("수정", "./itempsform.php?w=u&is_id={$row['is_id']}")."</td>
-            </tr>";
+            $settle_method = $row['od_settle_case'];
         }
-
-        if ($i == 0)
-            echo "<tr><td colspan=3 align=center class=ht>자료가 없습니다.</td></tr>";
-        ?>
-        <tr><td colspan=3 height=1 bgcolor=#CCCCCC></td></tr>
-        </table>
-    <td>
-    <td width=1%></td>
-	<td width=50% valign=top>
-        <table width=100% cellpadding=0 cellspacing=0>
-        <tr>
-            <td width=70%><?//=subtitle("상품문의")?></td>
-            <td width=30% align=right><a href='./itemqalist.php?sort1=iq_answer&sort2=asc'><img src='<?=G4_ADMIN_URL?>/img/icon_more.gif' border=0></a>&nbsp;</td>
-        </tr>
-        </table>
-
-        <table width=100% cellpadding=0 cellspacing=0>
-        <tr><td colspan=3 height=2 bgcolor=#0E87F9></td></tr>
-        <tr align=center class=ht>
-        	<td width=100>회원명</td>
-        	<td>제목</td>
-        	<td width=40>수정</td>
-        </tr>
-        <tr><td colspan=3 height=1 bgcolor=#CCCCCC></td></tr>
-        <?
-        $sql = " select * from {$g4['shop_item_qa_table']}
-                  where iq_answer = ''
-                  order by iq_id desc
-                  limit $max_limit ";
-        $result = sql_query($sql);
-        for ($i=0; $row=sql_fetch_array($result); $i++)
+        else
         {
-            $sql1 = " select * from {$g4['member_table']} where mb_id = '{$row['mb_id']}' ";
-            $row1 = sql_fetch($sql1);
-
-            $name = get_sideview($row['mb_id'], get_text($row['iq_name']), $row1['mb_email'], $row1['mb_homepage']);
-
-            $list = $i%2;
-            echo "
-            <tr align=center class='list$list ht'>
-                <td align=center>$name</td>
-                <td>".cut_str($row['iq_subject'],40)."</td>
-                <td align=center>".icon("수정", "./itemqaform.php?w=u&iq_id={$row['iq_id']}")."</td>
-            </tr>";
+            if ($row['od_temp_bank'])   $settle_method  .= "무통장";
+            if ($row['od_temp_card'])   $settle_method .= "카드";
+            if ($row['od_temp_milage']) $settle_method .= "포인트";
         }
+    ?>
+    <tr>
+        <td><a href="./orderstatuslist.php?sort1=od_id&amp;sel_field=od_id&amp;search=<?=$row['od_id']?>"><?=$row['od_id']?></a></td>
+        <td><?=$name?></td>
+        <td><?=display_amount($row['orderamount'])?></td>
+        <td><?=$settle_method?></td>
+        <td><a href="./orderform.php?od_id=<?=$row['od_id']?>">수정</a></td>
+    </tr>
+    <?
+    }
+    if ($i == 0) echo '<tr><td colspan="5">자료가 없습니다.</td></tr>';
+    ?>
+    </tbody>
+    </table>
 
-        if ($i == 0)
-            echo "<tr><td colspan=3 align=center class=ht>자료가 없습니다.</td></tr>";
-        ?>
-        <tr><td colspan=3 height=1 bgcolor=#CCCCCC></td></tr>
-        </table>
-    </td>
-</tr>
-</table><br>
+    <div class="btn_ft">
+        <a href="./orderlist.php?sort1=receiptamount&amp;sort2=asc">미입금 주문내역 더보기</a>
+    </div>
+</section>
+
+<section class="cbox">
+    <h2>사용후기</h2>
+
+    <table>
+    <thead>
+    <tr>
+        <th scope="col">회원명</th>
+        <th scope="col">제목</th>
+        <th scope="col">수정</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?
+    $sql = " select * from {$g4[shop_item_ps_table]}
+              where is_confirm = 0
+              order by is_id desc
+              limit $max_limit ";
+    $result = sql_query($sql);
+    for ($i=0; $row=sql_fetch_array($result); $i++)
+    {
+        $sql1 = " select * from {$g4['member_table']} where mb_id = '{$row['mb_id']}' ";
+        $row1 = sql_fetch($sql1);
+
+        $name = get_sideview($row['mb_id'], get_text($row['is_name']), $row1['mb_email'], $row1['mb_homepage']);
+    ?>
+    <tr>
+        <td><?=$name?></td>
+        <td><?=cut_str($row['is_subject'],40)?></td>
+        <td><a href="./itempsform.php?w=u&amp;is_id=<?=$row['is_id']?>">수정</a></td>
+    </tr>
+    <?
+    }
+    if ($i == 0) echo '<tr><td colspan="3" class="empty_table">자료가 없습니다.</td></tr>';
+    ?>
+    </tbody>
+    </table>
+
+    <div class="btn_ft">
+        <a href="./itempslist.php?sort1=is_confirm&amp;sort2=asc">사용후기 더보기</a>
+    </div>
+</section>
+
+<section class="cbox">
+    <h2>상품문의</h2>
+
+    <table>
+    <thead>
+    <tr>
+        <th scope="col">회원명</th>
+        <th scope="col">제목</th>
+        <th scope="col">수정</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?
+    $sql = " select * from {$g4['shop_item_qa_table']}
+              where iq_answer = ''
+              order by iq_id desc
+              limit $max_limit ";
+    $result = sql_query($sql);
+    for ($i=0; $row=sql_fetch_array($result); $i++)
+    {
+        $sql1 = " select * from {$g4['member_table']} where mb_id = '{$row['mb_id']}' ";
+        $row1 = sql_fetch($sql1);
+
+        $name = get_sideview($row['mb_id'], get_text($row['iq_name']), $row1['mb_email'], $row1['mb_homepage']);
+    ?>
+    <tr>
+        <td><?=$name?></td>
+        <td><?=cut_str($row['iq_subject'],40)?></td>
+        <td><a href="./itemqaform.php?w=u&amp;iq_id=<?=$row['iq_id']?>">수정</a></td>
+    </tr>";
+    <?
+    }
+
+    if ($i == 0)
+        echo '<tr><td colspan="3" class="empty_table">자료가 없습니다.</td></tr>';
+    ?>
+    </tbody>
+    </table>
+
+    <div class="btn_ft">
+        <a href="./itemqalist.php?sort1=iq_answer&amp;sort2=asc">상품문의 더보기</a>
+    </div>
+</section>
 
 <?
 include_once (G4_ADMIN_PATH.'/admin.tail.php');
