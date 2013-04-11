8c2ce6e19036c5efe60c4b42a1431a4e07fcb6f6
rollydream <xgirl_87x@naver.com>
2013-04-11 14:21:05 +0900

    쇼핑몰관리자 : #58 상품판매순위 표준화 및 스타일 1차 작업 완료

--- adm/shop_admin/itemsellrank.php
+++ adm/shop_admin/itemsellrank.php
@@ -54,95 +54,105 @@ $result = sql_query($sql);
 $qstr1 = "$qstr&sort1=$sort1&sort2=$sort2&fr_date=$fr_date&to_date=$to_date&sel_ca_id=$sel_ca_id";
 ?>
 
-<table width=100% cellpadding=4 cellspacing=0>
-<form name=flist>
-<input type=hidden name=doc   value="<? echo $doc ?>">
-<input type=hidden name=sort1 value="<? echo $sort1 ?>">
-<input type=hidden name=sort2 value="<? echo $sort2 ?>">
-<input type=hidden name=page  value="<? echo $page ?>">
-<tr>
-    <td width=10%><a href='<?=$_SERVER['PHP_SELF']?>'>처음</a></td>
-    <td width=80% align=center>
-        <select name="sel_ca_id">
-            <option value=''>전체분류
-            <?
-            $sql1 = " select ca_id, ca_name from {$g4['shop_category_table']} order by ca_id ";
-            $result1 = sql_query($sql1);
-            for ($i=0; $row1=mysql_fetch_array($result1); $i++) {
-                $len = strlen($row1['ca_id']) / 2 - 1;
-                $nbsp = "";
-                for ($i=0; $i<$len; $i++) $nbsp .= "&nbsp;&nbsp;&nbsp;";
-                echo "<option value='{$row1['ca_id']}'>$nbsp{$row1['ca_name']}\n";
-            }
-            ?>
-        </select>
-        <script> document.flist.sel_ca_id.value = '<?=$sel_ca_id?>';</script>
+<style type="text/css">
+    .itemsellrank{text-align:center}
+</style>
 
-        기간 : <input type=text name=fr_date size=8 maxlength=8 itemname='기간' value='<?=$fr_date?>'> ~ <input type=text name=to_date size=8 maxlength=8 itemname='기간' value='<?=$to_date?>'>
-        <input type=image src='<?=G4_ADMIN_URL?>/img/btn_search.gif' align=absmiddle>
-    </td>
-    <td width=10% align=right>건수 : <? echo $total_count ?>&nbsp;</td>
-</tr>
-</table>
+<form name="flist">
+<input type="hidden" name="doc" value="<? echo $doc ?>">
+<input type="hidden" name="sort1" value="<? echo $sort1 ?>">
+<input type="hidden" name="sort2" value="<? echo $sort2 ?>">
+<input type="hidden" name="page" value="<? echo $page ?>">
+<p><a href='<?=$_SERVER['PHP_SELF']?>'>처음</a></p>
+<fieldset>
+<legend>상품판매순위 검색</legend>
+<select name="sel_ca_id" title="검색분류">
+    <option value=''>전체분류</option>
+    <?
+    $sql1 = " select ca_id, ca_name from {$g4['shop_category_table']} order by ca_id ";
+    $result1 = sql_query($sql1);
+    for ($i=0; $row1=mysql_fetch_array($result1); $i++) {
+        $len = strlen($row1['ca_id']) / 2 - 1;
+        $nbsp = "";
+        for ($i=0; $i<$len; $i++) $nbsp .= "&nbsp;&nbsp;&nbsp;";
+        echo "<option value='{$row1['ca_id']}'>$nbsp{$row1['ca_name']}\n";
+    }
+    ?>
+</select>
+<script> document.flist.sel_ca_id.value = '<?=$sel_ca_id?>';</script>
 
-<table cellpadding=0 cellspacing=0 width=100%>
-<tr><td colspan=20 height=3 bgcolor=#0E87F9></td></tr>
-<tr align=center class=ht>
-    <td width=50>순위</td>
-    <td width=80></td>
-    <td width=''>상품명</td>
-    <td width=50><a href='<?=title_sort("ct_status_1",1)."&$qstr1"?>'>쇼핑</a></td>
-    <td width=50><a href='<?=title_sort("ct_status_2",1)."&$qstr1"?>'>주문</a></td>
-    <td width=50><a href='<?=title_sort("ct_status_3",1)."&$qstr1"?>'>준비</a></td>
-    <td width=50><a href='<?=title_sort("ct_status_4",1)."&$qstr1"?>'>배송</a></td>
-    <td width=50><a href='<?=title_sort("ct_status_5",1)."&$qstr1"?>'>완료</a></td>
-    <td width=50><a href='<?=title_sort("ct_status_6",1)."&$qstr1"?>'>취소</a></td>
-    <td width=50><a href='<?=title_sort("ct_status_7",1)."&$qstr1"?>'>반품</a></td>
-    <td width=50><a href='<?=title_sort("ct_status_8",1)."&$qstr1"?>'>품절</a></td>
-    <td width=50><a href='<?=title_sort("ct_status_sum",1)."&$qstr1"?>'>합계</a></td>
-</tr>
-<tr><td colspan=20 height=1 bgcolor=#CCCCCC></td></tr>
-<?
-for ($i=0; $row=mysql_fetch_array($result); $i++)
-{
-    $href = G4_SHOP_URL."/item.php?it_id={$row['it_id']}";
+기간 : <input type="text" name="fr_date" value="<?=$fr_date?>" class="frm_input" size="8" maxlength="8"> ~ <input type="text" name="to_date" value="<?=$to_date?>" class="frm_input" size="8" maxlength="8">
+<input type="submit" value="검색" class="btn_submit">
+</fieldset>
+</form>
+<p>건수 : <? echo $total_count ?></p>
+<section class="cbox">
+    <h2>상품판매순위</h2>
+    <p>*수량을 합산하여 순위를 출력합니다.</p>
+    <table class="frm_basic">
+    <colgroup>
+        <col class="grid_1">
+        <col class="grid_8">
+        <col class="grid_1">
+        <col class="grid_1">
+        <col class="grid_1">
+        <col class="grid_1">
+        <col class="grid_1">
+        <col class="grid_1">
+        <col class="grid_1">
+        <col class="grid_1">
+        <col class="grid_1">
+    </colgroup>
+    <thead>
+    <tr>
+        <th scope="col">순위</th>
+        <th scope="col">상품평</th>
+        <th scope="col"><a href="<?=title_sort("ct_status_1",1)."&$qstr1"?>">쇼핑</a></th>
+        <th scope="col"><a href="<?=title_sort("ct_status_2",1)."&$qstr1"?>">주문</a></th>
+        <th scope="col"><a href="<?=title_sort("ct_status_3",1)."&$qstr1"?>">준비</a></th>
+        <th scope="col"><a href="<?=title_sort("ct_status_4",1)."&$qstr1"?>">배송</a></th>
+        <th scope="col"><a href="<?=title_sort("ct_status_5",1)."&$qstr1"?>">완료</a></th>
+        <th scope="col"><a href="<?=title_sort("ct_status_6",1)."&$qstr1"?>">취소</a></th>
+        <th scope="col"><a href="<?=title_sort("ct_status_7",1)."&$qstr1"?>">반품</a></th>
+        <th scope="col"><a href="<?=title_sort("ct_status_8",1)."&$qstr1"?>">품절</a></th>
+        <th scope="col"><a href="<?=title_sort("ct_status_sum",1)."&$qstr1"?>">합계</a></th>
+    </tr>
+    </thead>
+    <tbody>
+    <?
+    for ($i=0; $row=mysql_fetch_array($result); $i++)
+    {
+        $href = G4_SHOP_URL."/item.php?it_id={$row['it_id']}";
 
-    $num = $rank + $i + 1;
+        $num = $rank + $i + 1;
 
-    $list = $i%2;
-    echo "
-    <tr class='list$list center'>
-        <td>$num</td>
-        <td style='padding-top:5px; padding-bottom:5px;'><a href='$href'>".get_it_image($row['it_id'].'_s', 50, 50)."</a></td>
-        <td align=left><a href='$href'>".cut_str($row['it_name'],30)."</a></td>
-        <td>".$row['ct_status_1']."</td>
-        <td>".$row['ct_status_2']."</td>
-        <td>".$row['ct_status_3']."</td>
-        <td>".$row['ct_status_4']."</td>
-        <td>".$row['ct_status_5']."</td>
-        <td>".$row['ct_status_6']."</td>
-        <td>".$row['ct_status_7']."</td>
-        <td>".$row['ct_status_8']."</td>
-        <td>".$row['ct_status_sum']."</td>
-    </tr><tr><td colspan=20 height=1 bgcolor=F5F5F5></td></tr>";
-}
+        $list = $i%2;
+        ?>
+        <tr>
+            <td class="itemsellrank"><?=$num?></td>
+            <td><a href="<?=$href?>"><?=get_it_image($row['it_id'].'_s', 50, 50)?><?=cut_str($row['it_name'],30)?></a></td>
+            <td class="itemsellrank"><?=$row['ct_status_1']?></td>
+            <td class="itemsellrank"><?=$row['ct_status_2']?></td>
+            <td class="itemsellrank"><?=$row['ct_status_3']?></td>
+            <td class="itemsellrank"><?=$row['ct_status_4']?></td>
+            <td class="itemsellrank"><?=$row['ct_status_5']?></td>
+            <td class="itemsellrank"><?=$row['ct_status_6']?></td>
+            <td class="itemsellrank"><?=$row['ct_status_7']?></td>
+            <td class="itemsellrank"><?=$row['ct_status_8']?></td>
+            <td class="itemsellrank"><?=$row['ct_status_sum']?></td>
+        </tr>
+        <?
+    }
 
-if ($i == 0) {
-    echo "<tr><td colspan=20 align=center height=100 bgcolor=#ffffff><span class=point>자료가 한건도 없습니다.</span></td></tr>\n";
-}
-?>
-<tr><td colspan=20 height=1 bgcolor=CCCCCC></td></tr>
-</table>
+    if ($i == 0) {
+        echo '<tr><td colspan="20" class="empty_table"><span>자료가 한건도 없습니다.</span></td></tr>';
+    }
+    ?>
+    </tbody>
+    </table>
+    <?=get_paging($config['cf_write_pages'], $page, $total_page, "{$_SERVER['PHP_SELF']}?$qstr1&page=");?>
+</section>
 
-
-<table width=100%>
-<tr>
-    <td width=50%>&nbsp;</td>
-    <td width=50% align=right><?=get_paging($config['cf_write_pages'], $page, $total_page, "{$_SERVER['PHP_SELF']}?$qstr1&page=");?></td>
-</tr>
-</table>
-
-* 수량을 합산하여 순위를 출력합니다.
 
 <?
 include_once (G4_ADMIN_PATH.'/admin.tail.php');
