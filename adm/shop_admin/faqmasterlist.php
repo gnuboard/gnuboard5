a4beac97549a7ce399d31e425f290faca99c13aa
whitedot <whitedot@naver.com>
2013-04-15 18:23:14 +0900

    쇼핑몰관리자: #28 쇼핑몰관리 FAQ관리 마스터 표준화 및 스타일 완료

--- adm/shop_admin/faqmasterlist.php
+++ adm/shop_admin/faqmasterlist.php
@@ -23,29 +23,25 @@ $sql = "select * $sql_common order by fm_id desc limit $from_record, {$config['c
 $result = sql_query($sql);
 ?>
 
-<style type="text/css">
-#faq_box span{position:absolute;top:-12;right:30px}
-#faq_register h2{color:#18abff}
-#faq_register ul{list-style:none;padding-left:0}
-#faq_register ul li{height:25px;line-height:25px}
-.faq_center{text-align:center}
-</style>
-
 <section class="cbox">
     <h2>FAQ관리</h2>
-    <?=$pg_anchor?>
-    <p id="faq_box"><a href="<?=$_SERVER['PHP_SELF']?>">처음</a><span>건수  <? echo $total_count ?></span></p>
+
+    <p>
+        <? if ($page > 1) {?><a href="<?=$_SERVER['PHP_SELF']?>">처음으로</a><? } ?>
+        <span>전체 FAQ <?=$total_count?>건</span>
+    </p>
+
+    <ol>
+        <li>FAQ는 무제한으로 등록할 수 있습니다</li>
+        <li><strong>FAQ추가</strong>를 눌러 FAQ Master를 생성합니다. (하나의 FAQ 타이틀 생성 : 자주하시는 질문, 이용안내..등 )</li>
+        <li>생성한 FAQ Master 의 <strong>상세보기</strong>를 눌러 세부 내용을 관리할 수 있습니다.</li>
+    </ol>
+
     <div id="btn_add">
         <a href="./faqmasterform.php">FAQ추가</a>
     </div>
+
     <table>
-    <colgroup>
-        <col class="grid_1">
-        <col class="grid_11">
-        <col class="grid_2">
-        <col class="gird_1">
-        <col class="grid_3">
-    </colgroup>
     <thead>
     <tr>
         <th scope="col">ID</th>
@@ -56,50 +52,34 @@ $result = sql_query($sql);
     </tr>
     </thead>
     <tbody>
-    <?
-    for ($i=0; $row=mysql_fetch_array($result); $i++)
-    {
+    <? for ($i=0; $row=mysql_fetch_array($result); $i++) {
         $sql1 = " select COUNT(*) as cnt from {$g4['shop_faq_table']} where fm_id = '{$row['fm_id']}' ";
         $row1 = sql_fetch($sql1);
         $cnt = $row1['cnt'];
-
-        $s_detail_vie = icon("보기", "./faqlist.php?fm_id={$row['fm_id']}");
-
-        $s_mod = icon("수정", "./faqmasterform.php?w=u&fm_id={$row['fm_id']}");
-        $s_del = icon("삭제", "javascript:del('./faqmasterformupdate.php?w=d&fm_id={$row['fm_id']}');");
-        $s_vie = icon("보기", G4_SHOP_URL."/faq.php?fm_id={$row['fm_id']}");
-        $list = $i%2;
-        ?>
-        <tr>
-            <td class="faq_center"><?=$row['fm_id']?></td>
-            <td><?=stripslashes($row['fm_subject']) ?></td>
-            <td class="faq_center"><?=$cnt?></td>
-            <td class="faq_center"><a href="./faqlist.php?fm_id=<?=$row['fm_id']?>">상세보기</a></td>
-            <td class="faq_center"><a href="">수정</a> <a href="./faqmasterformupdate.php?w=d&fm_id=<?=$row['fm_id']?>">삭제</a> <a href="<?=G4_SHOP_URL?>/faq.php?fm_id=<?=$row['fm_id']?>">보기</a></td>
-        </tr>
-        <?
+    ?>
+    <tr>
+        <td class="td_num"><?=$row['fm_id']?></td>
+        <td><?=stripslashes($row['fm_subject']) ?></td>
+        <td class="td_num"><?=$cnt?></td>
+        <td class="td_smallmng"><a href="./faqlist.php?fm_id=<?=$row['fm_id']?>">상세보기</a></td>
+        <td class="td_mng">
+            <a href="<?=G4_SHOP_URL?>/faq.php?fm_id=<?=$row['fm_id']?>">보기</a>
+            <a href="./faqmasterform.php?w=u&amp;fm_id=<?=$row['fm_id']?>">수정</a>
+            <a href="javascript:del('./faqmasterformupdate.php?w=d&amp;fm_id=<?=$row['fm_id']?>');">삭제</a>
+        </td>
+    </tr>
+    <?
     }
 
     if ($i == 0){
-        echo '<tr><td colspan="5" class="faq_center"><span>자료가 한건도 없습니다.</span></td></tr>';
+        echo '<tr><td colspan="5" class="empty_table"><span>자료가 한건도 없습니다.</span></td></tr>';
     }
     ?>
     </tbody>
     </table>
 </section>
 
-<p><?=get_paging($config['cf_write_pages'], $page, $total_page, "{$_SERVER['PHP_SELF']}?$qstr&page=");?></p>
-
-<section id="faq_register" class="cbox">
-    <h2>FAQ 등록하기</h2>
-    <?=$pg_anchor?>
-    <ul>
-        <li>: FAQ는 무제한으로 등록할 수 있습니다</li>
-        <li>1. 먼저 <img src="<?=G4_ADMIN_URL?>/img/icon_insert.gif" alt="등록">를 눌러 FAQ Master를 생성합니다. (하나의 FAQ 타이틀 생성 : 자주하시는 질문, 이용안내..등 )</li>
-        <li> 2. 상세보기에 있는 <img src="<?=G4_ADMIN_URL?>/img/icon_viewer.gif" alt="보기">을 눌러 세부 내용으로 들어갑니다.</li>
-    </ul>
-</section>
-
+<?=get_paging($config['cf_write_pages'], $page, $total_page, "{$_SERVER['PHP_SELF']}?$qstr&amp;page=");?>
 
 <?
 include_once (G4_ADMIN_PATH.'/admin.tail.php');
