47ace25af5060fdf22b3b9f77a83ba5ffd80ada3
whitedot <whitedot@naver.com>
2013-04-18 10:55:29 +0900

    쇼핑몰관리자: #39 쇼핑몰현황 주문내역출력 표준화 및 스타일 완료

--- adm/shop_admin/orderprint.php
+++ adm/shop_admin/orderprint.php
@@ -10,73 +10,73 @@ include_once (G4_ADMIN_PATH.'/admin.head.php');
 
 <section class="cbox">
     <h2>주문내역출력</h2>
+    <p>기간별 혹은 주문번호구간별 주문내역을 새창으로 출력할 수 있습니다.</p>
+
     <table class="frm_tbl">
     <colgroup>
         <col class="grid_3">
-        <col class="gird_15">
+        <col>
     </colgroup>
     <tbody>
     <tr>
-        <th scope="row">출력기간</th>
+        <th scope="row">기간별 출력</th>
         <td>
             <form name="forderprint" action="./orderprintresult.php" onsubmit="return forderprintcheck(this);" autocomplete="off">
             <input type="hidden" name="case" value="1">
 
             <input type="radio" name="csv" value="xls" id="xls1"><label for="xls1">MS엑셀 XLS 데이터</label>
             <input type="radio" name="csv" value="csv" id="csv1"><label for="csv1">MS엑셀 CSV 데이터</label>
-                        <input type="text" name="fr_date" value="<?=date("Ymd");?>" id="fr_date" class="frm_input" size="10" maxlength="8">
-            <a href="javascript:win_calendar('fr_date', document.getElementById('fr_date').value, '');">
-            <img src="<?=G4_URL?>/img/calendar.gif" alt="달력 - 날짜를 선택하세요">
-            </a>
-            ∼
-            <input type="text" name="to_date" value="<?=date("Ymd");?>" id="to_date" class="frm_input" size="10" maxlength="8">
-            <a href="javascript:win_calendar('to_date', document.getElementById('to_date').value, '');">
-            <img src="<?=G4_URL?>/img/calendar.gif" alt="달력 - 날짜를 선택하세요">
-            </a>
-            &nbsp;
-            <select name="ct_status">
-                <option value="주문">주문</option>
-                <option value="준비">상품준비중</option>
-                <option value="배송">배송</option>
-                <option value="완료">완료</option>
-                <option value="취소">취소</option>
-                <option value="반품">반품</option>
-                <option value="품절">품절</option>
-                <option value="전체">전체</option>
-            </select>
-            <input type="submit" value="확  인">
+            <div>
+                <label for="ct_status_p" class="sound_only">출력대상</label>
+                <select name="ct_status" id="ct_status_p">
+                    <option value="주문">주문</option>
+                    <option value="준비">상품준비중</option>
+                    <option value="배송">배송</option>
+                    <option value="완료">완료</option>
+                    <option value="취소">취소</option>
+                    <option value="반품">반품</option>
+                    <option value="품절">품절</option>
+                    <option value="전체">전체</option>
+                </select>
+                <input type="text" name="fr_date" value="<?=date("Ymd");?>" id="fr_date" class="frm_input" size="10" maxlength="8"> 부터
+                <input type="text" name="to_date" value="<?=date("Ymd");?>" id="to_date" class="frm_input" size="10" maxlength="8"> 까지
+                <button type="submit" class="btn_frmline">출력 (새창)</button>
+            </div>
+
             </form>
         </td>
     </tr>
     <tr>
-        <th scope="row">주문번호구간</th>
+        <th scope="row">주문번호구간별 출력</th>
         <td>
-            <form name="forderprint" action="./orderprintresult.php" onsubmit="return forderprintcheck(this);" autocomplete="off" ><!--form시작-->
+            <form name="forderprint" action="./orderprintresult.php" onsubmit="return forderprintcheck(this);" autocomplete="off" >
             <input type="hidden" name="case" value="2">
+
             <input type="radio" name="csv" value="xls" id="xls2"><label for="xls2">MS엑셀 XLS 데이터</label>
             <input type="radio" name="csv" value="csv" id="csv2"><label for="csv2">MS엑셀 CSV 데이터</label>
-            <input type="text" name="fr_od_id" class="frm_input" size="10" maxlength="10"> 부터
-            <input type="text" name="to_od_id" class="frm_input" size="10" maxlength="10"> 까지
-            <select name="ct_status">
-                <option value="주문">주문</option>
-                <option value="준비">상품준비중</option>
-                <option value="배송">배송</option>
-                <option value="완료">완료</option>
-                <option value="취소">취소</option>
-                <option value="반품">반품</option>
-                <option value="품절">품절</option>
-                <option value="전체">전체</option>
-            </select>
-            <input type="submit" value="확  인">
+            <div>
+                <label for="ct_status_n" class="sound_only">출력대상</label>
+                <select name="ct_status" id="ct_status_n">
+                    <option value="주문">주문</option>
+                    <option value="준비">상품준비중</option>
+                    <option value="배송">배송</option>
+                    <option value="완료">완료</option>
+                    <option value="취소">취소</option>
+                    <option value="반품">반품</option>
+                    <option value="품절">품절</option>
+                    <option value="전체">전체</option>
+                </select>
+                <input type="text" name="fr_od_id" class="frm_input" size="10" maxlength="10"> 부터
+                <input type="text" name="to_od_id" class="frm_input" size="10" maxlength="10"> 까지
+                <button type="submit" class="btn_frmline">출력 (새창)</button>
+            </div>
+
             </form>
         </td>
     </tr>
     </tbody>
     </table>
 </section>
-
-
-
 
 <script>
 function forderprintcheck(f)
