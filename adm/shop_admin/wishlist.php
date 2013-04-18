e5d70b10102eeafcda42ae7866a9bf19f5a4dc91
whitedot <whitedot@naver.com>
2013-04-18 13:13:37 +0900

    쇼핑몰관리자: #59 쇼핑몰현황 보관함내역 표준화 및 스타일

--- adm/shop_admin/wishlist.php
+++ adm/shop_admin/wishlist.php
@@ -42,55 +42,59 @@ $rank = ($page - 1) * $rows;
 $sql = $sql . " limit $from_record, $rows ";
 $result = sql_query($sql);
 
-$qstr = "page=$page&sort1=$sort1&sort2=$sort2";
-$qstr1 = "fr_date=$fr_date&to_date=$to_date&sel_ca_id=$sel_ca_id";
-?>
-<style type="text/css">
-    .wishlist{text-align:center}
-</style>
+$qstr = 'page='.$page.'&amp;sort1='.$sort1.'&amp;sort2='.$sort2;
+$qstr1 = 'fr_date='.$fr_date.'&amp;to_date='.$to_date.'&amp;sel_ca_id='.$sel_ca_id;
 
+$listall = '';
+if ($search) // 검색렬일 때만 처음 버튼을 보여줌
+    $listall = '<a href="'.$_SERVER['PHP_SELF'].'">전체목록</a>';
+?>
 
 <form name="flist">
-<input type="hidden" name="doc" value="<? echo $doc ?>">
-<input type="hidden" name="sort1" value="<? echo $sort1 ?>">
-<input type="hidden" name="sort2" value="<? echo $sort2 ?>">
-<input type="hidden" name="page" value="<? echo $page ?>">
+<input type="hidden" name="doc" value="<?=$doc ?>">
+<input type="hidden" name="sort1" value="<?=$sort1 ?>">
+<input type="hidden" name="sort2" value="<?=$sort2 ?>">
+<input type="hidden" name="page" value="<?=$page ?>">
 
-<p><a href="<?=$_SERVER['PHP_SELF']?>">처음</a></p>
 <fieldset>
-<legend>보관함현황 검색</legend>
-<select name="sel_ca_id" title="검색분류">
-    <option value=''>전체분류</option>
-    <?
-    $sql1 = " select ca_id, ca_name from {$g4['shop_category_table']} order by ca_id ";
-    $result1 = sql_query($sql1);
-    for ($i=0; $row1=mysql_fetch_array($result1); $i++) {
-        $len = strlen($row1['ca_id']) / 2 - 1;
-        $nbsp = "";
-        for ($i=0; $i<$len; $i++) $nbsp .= "&nbsp;&nbsp;&nbsp;";
-        echo "<option value='{$row1['ca_id']}'>$nbsp{$row1['ca_name']}\n";
-    }
-    ?>
-</select>
-<script> document.flist.sel_ca_id.value = '<?=$sel_ca_id?>';</script>
-기간 : 
-<input type="text" name="fr_date" value="<?=$fr_date?>" class="frm_input" size="8" maxlength="8">
-~ 
-<input type="text" name="to_date" value="<?=$to_date?>" class="frm_input" size="8" maxlength="8">
-<input type="submit" value="검색" class="btn_submit">
+    <legend>보관함현황 검색</legend>
+
+    <span>
+        <?=$listall?>
+        전체 보관함 내역 <?=$total_count ?>건
+    </span>
+
+    <label for="sel_ca_id" class="sound_only">검색대상</label>
+    <? // ##### // 웹 접근성 취약 지점 시작 - 지운아빠 2013-04-18 ?>
+    <select name="sel_ca_id" id="sel_ca_id">
+        <option value=''>전체분류</option>
+        <?
+        $sql1 = " select ca_id, ca_name from {$g4['shop_category_table']} order by ca_id ";
+        $result1 = sql_query($sql1);
+        for ($i=0; $row1=mysql_fetch_array($result1); $i++) {
+            $len = strlen($row1['ca_id']) / 2 - 1;
+            $nbsp = "";
+            for ($i=0; $i<$len; $i++) $nbsp .= "&nbsp;&nbsp;&nbsp;";
+            echo "<option value='{$row1['ca_id']}'>$nbsp{$row1['ca_name']}\n";
+        }
+        ?>
+    </select>
+    <? // ##### // 웹 접근성 취약 지점 끝 ?>
+    기간설정
+    <label for="fr_date" class="sound_only">기간 시작일</label>
+    <input type="text" name="fr_date" value="<?=$fr_date?>" id="fr_date" class="frm_input" size="8" maxlength="8"> 부터
+    <label for="to_date" class="sound_only">기간 종료일</label>
+    <input type="text" name="to_date" value="<?=$to_date?>" id="to_date" class="frm_input" size="8" maxlength="8"> 까지
+    <input type="submit" value="검색" class="btn_submit">
 </fieldset>
-<p>건수 : <? echo $total_count ?></p>
+
 </form>
 
 <section class="cbox">
-    <h2>보관함현황</h2>
-    <p> *수량을 합산하여 순위를 출력합니다.</p>
-    <table class="frm_basic">
-    <colgroup>
-        <col class="grid_2">
-        <col class="gird_14">
-        <col class="grid_2">
-    </colgroup>
+    <h2>보관함 현황</h2>
+    <p>고객님들이 보관함에 가장 많이 넣은 순으로 순위를 출력합니다.</p>
+
+    <table>
     <thead>
     <tr>
         <th scope="col">순위</th>
@@ -102,21 +106,18 @@ $qstr1 = "fr_date=$fr_date&to_date=$to_date&sel_ca_id=$sel_ca_id";
     <?
     for ($i=0; $row=mysql_fetch_array($result); $i++)
     {
-        $s_mod = icon("수정", "./itemqaform.php?w=u&iq_id={$row['iq_id']}&$qstr");
-        $s_del = icon("삭제", "javascript:del('./itemqaupdate.php?w=d&iq_id={$row['iq_id']}&$qstr');");
+        // $s_mod = icon("수정", "./itemqaform.php?w=u&iq_id={$row['iq_id']}&$qstr");
+        // $s_del = icon("삭제", "javascript:del('./itemqaupdate.php?w=d&iq_id={$row['iq_id']}&$qstr');");
 
-        $href = G4_SHOP_URL."/item.php?it_id={$row['it_id']}";
-
+        $href = G4_SHOP_URL.'/item.php?it_id='.$row['it_id'];
         $num = $rank + $i + 1;
-
-        $list = $i%2;
-        ?>
-        <tr>
-            <td class="wishlist"><?=$num?></td>
-            <td><a href="<?=$href?>"><?=get_it_image($row['it_id'].'_s', 50, 50)?><?=cut_str($row['it_name'],30)?></a></td>
-            <td class="wishlist"><?=$row[it_id_cnt]?></td>
-        </tr>
-        <?
+    ?>
+    <tr>
+        <td class="td_num"><?=$num?></td>
+        <td><a href="<?=$href?>"><?=get_it_image($row['it_id'].'_s', 50, 50)?><?=cut_str($row['it_name'],30)?></a></td>
+        <td class="td_num"><?=$row['it_id_cnt']?></td>
+    </tr>
+    <?
     }
 
     if ($i == 0) {
