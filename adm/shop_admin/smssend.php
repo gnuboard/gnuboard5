404fa350b69cd72e3aa748c1e72e9a2982cb39f5
whitedot <whitedot@naver.com>
2013-04-18 15:48:11 +0900

    쇼핑몰관리자: #78 쇼핑몰현황 SMS 발송 표준화 및 스타일 개선안에 따라 완료

--- adm/shop_admin/smssend.php
+++ adm/shop_admin/smssend.php
@@ -11,9 +11,14 @@ include_once (G4_ADMIN_PATH.'/admin.head.php');
 $send_number = preg_replace("/[^0-9]/", "", $default['de_admin_company_tel']);
 ?>
 
-<?//=subtitle($g4['title'])?>
+<?
+//if ($default['de_sms_use'] == "icode") { // 아이코드 사용
+if ($is_admin) {
+?>
+<form action="./smssendicode.php" name="smsform" method="post" autocomplete="off">
+<input type="hidden" name="receive_number" value="">
 
-<script language="JavaScript">
+<script>
 function byte_check(cont, bytes)
 {
     var i = 0;
@@ -63,7 +68,7 @@ function byte_check(cont, bytes)
 </script>
 
 
-<script language="JavaScript" type="text/JavaScript">
+<script>
 var StrComma = "";
 
 function tel_enter()
@@ -83,146 +88,214 @@ function tel_enter()
 
 function receive_add()
 {
-	var intCount = 0;
-	var strMobile = document.smsform.receive_input.value;
-	//strMobile = strMobile.replace("-", "", strMobile);
+    var intCount = 0;
+    var strMobile = document.smsform.receive_input.value;
+    //strMobile = strMobile.replace("-", "", strMobile);
     strMobile = strMobile.replace("-", "");
 
-	for (i = 0; i < document.smsform.receive_buffer.length; i++)
-	{
-		if (strMobile == document.smsform.receive_buffer.options[i].value)
-		{
-			return alert("같은 번호는 재입력 하실수 없습니다");
-			document.smsform.receive_buffer.options.remove(i);
-			intCount = intCount - 1;
-			document.smsform.count.value = intCount ;
-			document.smsform.receive_input.focus();
-		}
-	}
+    for (i = 0; i < document.smsform.receive_buffer.length; i++)
+    {
+        if (strMobile == document.smsform.receive_buffer.options[i].value)
+        {
+            return alert("같은 번호는 재입력 하실수 없습니다");
+            document.smsform.receive_buffer.options.remove(i);
+            intCount = intCount - 1;
+            document.smsform.count.value = intCount ;
+            document.smsform.receive_input.focus();
+        }
+    }
 
 
 
-	strDigit= "0123456789-";
-	intIdLength = strMobile.length;
-	var blnChkFlag;
+    strDigit= "0123456789-";
+    intIdLength = strMobile.length;
+    var blnChkFlag;
 
-	for (i = 0; i < intIdLength; i++)
-	{
-		strNumberChar = strMobile.charAt(i);
-		blnChkFlag = false;
+    for (i = 0; i < intIdLength; i++)
+    {
+        strNumberChar = strMobile.charAt(i);
+        blnChkFlag = false;
 
-		for (j = 0; j < strDigit.length ; j++)
-		{
-			strCompChar = strDigit.charAt(j);
+        for (j = 0; j < strDigit.length ; j++)
+        {
+            strCompChar = strDigit.charAt(j);
 
-			if (strNumberChar == strCompChar)
-			{
-				blnChkFlag = true;
-			}
-		}
+            if (strNumberChar == strCompChar)
+            {
+                blnChkFlag = true;
+            }
+        }
 
-		if (blnChkFlag == false)
-		{
-			break;
-		}
-	}
+        if (blnChkFlag == false)
+        {
+            break;
+        }
+    }
 
-	if (strMobile == "" )
-	{
-		alert ("추가할 수신번호를 입력해 주세요");
-	}
-	else if (strMobile.length < 10 || strMobile.length > 13 )
-	{
-		alert ("수신번호는 최대 13자, 최소 10자이내로 입력해 주세요.\n\n 예) 01X-123-4567 또는 01X1234567  ");
-		document.smsform.receive_input.value="";
-		document.smsform.receive_input.focus();
-	}
-	else if ( !blnChkFlag )
-	{
-		alert("수신번호는 숫자만 가능합니다.");
-		document.smsform.receive_input.value="";
-		document.smsform.receive_input.focus();
-	}
-	else
-	{
+    if (strMobile == "" )
+    {
+        alert ("추가할 수신번호를 입력해 주세요");
+    }
+    else if (strMobile.length < 10 || strMobile.length > 13 )
+    {
+        alert ("수신번호는 최대 13자, 최소 10자이내로 입력해 주세요.\n\n 예) 01X-123-4567 또는 01X1234567  ");
+        document.smsform.receive_input.value="";
+        document.smsform.receive_input.focus();
+    }
+    else if ( !blnChkFlag )
+    {
+        alert("수신번호는 숫자만 가능합니다.");
+        document.smsform.receive_input.value="";
+        document.smsform.receive_input.focus();
+    }
+    else
+    {
 
 
-		document.smsform.receive_number.value = document.smsform.receive_number.value + document.smsform.receive_input.value + "," ;
+        document.smsform.receive_number.value = document.smsform.receive_number.value + document.smsform.receive_input.value + "," ;
         StrComma = ",";
-		add() ;
+        add() ;
 
-	}
+    }
 
 }
 
 function add()
 {
-		var intCount = document.smsform.count.value ;
-		var newOpt = document.createElement('OPTION');
-		newOpt.text =  document.smsform.receive_input.value;
-		newOpt.value = document.smsform.receive_input.value;
-		document.smsform.receive_buffer.options.add(newOpt);
+        var intCount = document.smsform.count.value ;
+        var newOpt = document.createElement('OPTION');
+        newOpt.text =  document.smsform.receive_input.value;
+        newOpt.value = document.smsform.receive_input.value;
+        document.smsform.receive_buffer.options.add(newOpt);
 
-		document.smsform.receive_input.value = "" ;
-		intCount = intCount - 1 + 2;
-		document.smsform.count.value = intCount ;
-		document.smsform.receive_input.focus();
+        document.smsform.receive_input.value = "" ;
+        intCount = intCount - 1 + 2;
+        document.smsform.count.value = intCount ;
+        document.smsform.receive_input.focus();
 
 }
 
 function receive_del()
 {
-	if (document.smsform.receive_buffer.selectedIndex < 0)
-	{
-		alert ("삭제할 번호를 선택해 주세요");
-	}
-	else
-	{
-		var aaa;
-		aaa = document.smsform.receive_number.value ;
-		aaa = aaa.replace(document.smsform.receive_buffer.value + ",","");
-		document.smsform.receive_number.value = aaa ;
+    if (document.smsform.receive_buffer.selectedIndex < 0)
+    {
+        alert ("삭제할 번호를 선택해 주세요");
+    }
+    else
+    {
+        var aaa;
+        aaa = document.smsform.receive_number.value ;
+        aaa = aaa.replace(document.smsform.receive_buffer.value + ",","");
+        document.smsform.receive_number.value = aaa ;
 
-		var num ;
-		var intCount = document.smsform.count.value ;
-		num = document.smsform.receive_buffer.selectedIndex ;
-		document.smsform.receive_buffer.options.remove(num);
-		intCount = intCount - 1;
-		document.smsform.count.value = intCount ;
-	}
+        var num ;
+        var intCount = document.smsform.count.value ;
+        num = document.smsform.receive_buffer.selectedIndex ;
+        document.smsform.receive_buffer.options.remove(num);
+        intCount = intCount - 1;
+        document.smsform.count.value = intCount ;
+    }
 }
 
 function receive_alldel()
 {
 
-		document.smsform.receive_number.value = "0" ;
-		var intCount = document.smsform.count.value ;
-		for (i = 0; i < intCount; i++)
-		{
-			document.smsform.receive_buffer.options.remove(0);
-		}
-		document.smsform.count.value = "0" ;
+        document.smsform.receive_number.value = "0" ;
+        var intCount = document.smsform.count.value ;
+        for (i = 0; i < intCount; i++)
+        {
+            document.smsform.receive_buffer.options.remove(0);
+        }
+        document.smsform.count.value = "0" ;
 }
 </script>
 
-<? if ($default['de_sms_use'] == "icode") { // 아이코드 사용 ?>
-<form action="./smssendicode.php" name="smsform" method=post autocomplete=off>
-<input type="hidden" name="receive_number" value="">
-<? } else { ?>
-<form action="javascript:alert('SMS 사용을 하고 있지 않아 문자를 전송할 수 없습니다.');" name="smsform" method=post autocomplete=off>
-<input type="hidden" name="receive_number" value="">
-<? } ?>
+<section id="sms_send" class="cbox">
+    <h2>SMS 문자전송 내용 입력</h2>
+    <p>예약발송 기능을 이용하시면, 예약된 시간에 맞춰 SMS 문자를 일괄발송할 수 있습니다.</p>
+
+    <div id="sms_frm">
+        <table class="frm_tbl">
+        <colgroup>
+            <col class="grid_3">
+            <col>
+        </colgroup>
+        <tbody>
+        <tr>
+            <th>발신번호</th>
+            <td>
+                <?=help('SMS 발신자 번호를 입력하세요.')?>
+                <input name="send_number" type="text" value="<?=$send_number?>" id="send_number" class="frm_input">
+            </td>
+        </tr>
+        <tr>
+            <th>수신번호</th>
+            <td>
+                <?=help('여러명에게 보내실 때는 전화번호를 엔터로 구분하세요.')?>
+                <textarea></textarea>
+                <div><span>총 수신인 <strong></strong>명</span></div>
+            </td>
+        </tr>
+        <tr>
+            <th>문자내용</th>
+            <td>
+                <?=help("주의! 80 bytes 까지만 전송됩니다.\n영문 한글자 : 1byte , 한글 한글자 : 2bytes , 특수문자의 경우 1 또는 2 bytes 입니다.")?>
+                <textarea name="sms_contents" ONKEYUP="byte_check(document.smsform.sms_contents, bytes);"></textarea>
+                <div id="bytes">0 / 80 바이트</div>
+            </td>
+        </tr>
+        <tr>
+            <th><label for="reserved_flag">예약발송</label></th>
+            <td>
+                <input type="checkbox" name="reserved_flag" value="true" id="reserved_flag">
+                예약발송 사용
+                <label for="reserved_year" class="sound_only">연도 설정</label>
+                <select name="reserved_year" id="reserved_year">
+                    <? for ($i=date("Y"); $i<=date("Y")+1; $i++) { echo "<option value='$i'>".substr($i,-2)."</option>"; } ?>
+                </select>년
+                <label for="reserved_month" class="sound_only">월 설정</label>
+                <select name="reserved_month" id="reserved_month">
+                    <? for ($i=1; $i<=12; $i++) { echo "<option value='$i'>$i</option>"; } ?>
+                </select>월
+                <label for="reserved_day" class="sound_only">일 설정</label>
+                <select name="reserved_day" id="reserved_day">
+                    <? for ($i=1; $i<=31; $i++) { echo "<option value='$i'>$i</option>"; } ?>
+                </select>일
+                <label for="reserved_hour" class="sound_only">시 설정</label>
+                <select name="reserved_hour" id="reserved_hour">
+                    <? for ($i=1; $i<=24; $i++) { echo "<option value='$i'>$i</option>"; } ?>
+                </select>시
+                <label for="reserved_minute" class="sound_only">분 설정</label>
+                <select name="reserved_minute" id="reserved_minute">
+                    <? for ($i=1; $i<=60; $i++) { echo "<option value='$i'>$i</option>"; } ?>
+                </select>분
+            </td>
+        </tr>
+        </tbody>
+        </table>
+
+        <div class="btn_confirm">
+            <input type="submit" value="전송" class="btn_submit">
+        </div>
+    </div>
+
+    <div id="sms_sm">
+        <span id="sms_sm_text">여기에 문자내용 입력한 것이 실시간으로...</span>
+        <p>이 이미지는 이해를 돕기 위한 이미지로써, 실제 발송 시 화면에서 보이는 것과 차이가 있을 수 있습니다.</p>
+    </div>
+</section>
+
 
 <table border="0" cellpadding="0" cellspacing="0">
 <tr>
     <td>
-    <table align=center><tr><td><div id=bytes align=center>0 / 80 바이트</div></table>
+    <table align=center><tr><td></table>
     <table border="0" cellpadding="0" cellspacing="0">
     <tr>
         <td align="center" valign="middle"><table width="182" border="0" cellspacing="0" cellpadding="0">
             <tr>
               <td height="184" colspan="3" background="./img/skinL1_top.gif" align=center><bR><br><br>
-			  <textarea  style='OVERFLOW: hidden; border:solid 0; width:100px; height:80px; background-color:#FFE07E; FONT-SIZE: 9pt; font-family:굴림체;' name=sms_contents cols="16"  wrap=virtual ONKEYUP="byte_check(document.smsform.sms_contents, bytes);"></textarea></td>
+              </td>
             </tr>
             <tr>
               <td width="39"><img src="./img/skinL1_img1.gif" width="39" height="20"></td>
@@ -250,7 +323,7 @@ function receive_alldel()
                         <tr>
                           <td width="10" align="right"><img src="./img/skinL1_icon.gif" width="8" height="8"></td>
                           <td width="52" align="center" valign="middle">발신번호</td>
-                          <td width="102" height="22"> <input name="send_number" type="text" class=ed size="10" value="<?=$send_number?>"></td>
+                          <td width="102" height="22"> </td>
                         </tr>
                       </table></td>
                   </tr>
@@ -294,32 +367,18 @@ function receive_alldel()
                   </tr>
                   <tr>
                     <td width="12">&nbsp;</td>
-                    <td width="164" height="25" class=small><input type="checkbox" name="reserved_flag" value="true">예약&nbsp; <select name="reserved_month" style="font-size:8pt">
-                    <? for ($i=1; $i<=12; $i++) { echo "<option value='$i'>$i</option>"; } ?>
-                      </select>월<select name="reserved_day" style="font-size:8pt">
-                    <? for ($i=1; $i<=31; $i++) { echo "<option value='$i'>$i</option>"; } ?>
-                      </select>일</td>
+                    <td width="164" height="25" class=small></td>
                   </tr>
                   <tr>
                     <td>&nbsp;</td>
-                    <td height="23" class=small><select name="reserved_year" style="font-size:8pt">
-                    <? for ($i=date("Y"); $i<=date("Y")+1; $i++) { echo "<option value='$i'>".substr($i,-2)."</option>"; } ?>
-                      </select>년<select name="reserved_hour" style="font-size:8pt">
-                    <? for ($i=1; $i<=24; $i++) { echo "<option value='$i'>$i</option>"; } ?>
-                      </select>시<select name="reserved_minute" style="font-size:8pt">
-                    <? for ($i=1; $i<=60; $i++) { echo "<option value='$i'>$i</option>"; } ?>
-                      </select>분</td>
+                    <td height="23" class=small></td>
                   </tr>
                 </table></td>
             </tr>
           </table></td>
       </tr>
     </table>
-
-  </td>
-  </tr>
- </table>
- </form>
+</form>
 
 <script language="JavaScript">
 document.smsform.reserved_year.value = '<?=date("Y")?>';
@@ -356,6 +415,18 @@ function smsform_check(f)
     f.submit();
 }
 </script>
+
+<? } else { ?>
+
+<section class="cbox">
+    <h2>SMS 문자전송 서비스를 사용할 수 없습니다.</h2>
+    <p>
+        SMS 를 사용하지 않고 있기 때문에, 문자 전송을 할 수 없습니다.<br>
+        SMS 사용 설정은 <a href="./configform.php#frm_sms" class="btn_frmline">쇼핑몰관리 &gt; 쇼핑몰설정 &gt; SMS설정</a> 에서 하실 수 있습니다.
+    </p>
+</section>
+
+<? } ?>
 
 
 <?
