3efa406f80e273b78afcc5ef85b4408b4d5acff1
gnuboard <kagla@naver.com>
2013-04-23 18:31:14 +0900

    이름에 한글명 체크를 하지 않는다. 휴대폰 본인확인의 경우 영문도 가능하기 때문

--- bbs/register_form_update.php
+++ bbs/register_form_update.php
@@ -66,7 +66,8 @@ if ($w == '' || $w == 'u') {
     if ($msg = empty_mb_email($mb_email))   alert($msg, "", true, true);
     if ($msg = reserve_mb_id($mb_id))       alert($msg, "", true, true);
     if ($msg = reserve_mb_nick($mb_nick))   alert($msg, "", true, true);
-    if ($msg = valid_mb_name($mb_name))     alert($msg, "", true, true);
+    // 이름에 한글명 체크를 하지 않는다.
+    //if ($msg = valid_mb_name($mb_name))     alert($msg, "", true, true);
     if ($msg = valid_mb_nick($mb_nick))     alert($msg, "", true, true);
     if ($msg = valid_mb_email($mb_email))   alert($msg, "", true, true);
     if ($msg = prohibit_mb_email($mb_email))alert($msg, "", true, true);
