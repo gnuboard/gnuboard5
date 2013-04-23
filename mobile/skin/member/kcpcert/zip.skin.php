da4ed419a6a3c47039e97101165cca2227b6046f
whitedot <whitedot@naver.com>
2013-04-23 15:08:34 +0900

    모바일: 회원통합스킨 우편번호 홑따옴표 처리

--- mobile/skin/member/kcpcert/zip.skin.php
+++ mobile/skin/member/kcpcert/zip.skin.php
@@ -32,7 +32,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
         <dd>
             <ul>
                 <? for ($i=0; $i<count($list); $i++) { ?>
-                <li><a href='javascript:;' onclick="find_zip('<?=$list[$i][zip1]?>', '<?=$list[$i][zip2]?>', '<?=$list[$i][addr]?>');"><span class="post_code"><?=$list[$i][zip1]?>-<?=$list[$i][zip2]?></span> <?=$list[$i][addr]?> <?=$list[$i][bunji]?></a></li>
+                <li><a href='javascript:;' onclick="find_zip('<?=$list[$i]['zip1']?>', '<?=$list[$i]['zip2']?>', '<?=$list[$i]['addr']?>');"><span class="post_code"><?=$list[$i]['zip1']?>-<?=$list[$i]['zip2']?></span> <?=$list[$i]['addr']?> <?=$list[$i]['bunji']?></a></li>
                 <? } ?>
             </ul>
         </dd>
