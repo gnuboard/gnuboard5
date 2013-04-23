c94f0516a7018b6d60361f48ec158ebd57499037
chicpro <chicpro@gmail.com>
2013-04-23 13:24:16 +0900

    배너 이미지 출력되지 않는 오류 수정

--- shop/boxbanner.inc.php
+++ shop/boxbanner.inc.php
@@ -14,20 +14,20 @@ for ($i=0; $row=sql_fetch_array($result); $i++)
     // 테두리 있는지
     $bn_border  = $row['bn_border'];
     // 새창 띄우기인지
-    $bn_new_win = ($row['bn_new_win']) ? "target=".$row['bn_new_win'] : "";
+    $bn_new_win = ($row['bn_new_win']) ? ' target="'.$row['bn_new_win'].'"' : '';
 
-    $bimg = G4_DATA_PATH."/banner/{$row['bn_id']}";
+    $bimg = G4_DATA_PATH.'/banner/'.$row['bn_id'];
     if (file_exists($bimg))
     {
         $size = getimagesize($bimg);
-        echo "<tr><td>";
-        if ($row['bn_url'][0] == "#")
-            echo "<a href='{$row['bn_url']}'>";
-        else if ($row['bn_url'] && $row['bn_url'] != "http://") {
-            echo "<a href=\"".G4_SHOP_URL."/bannerhit.php?bn_id={$row['bn_id']}&url=".urlencode($row['bn_url'])."\" $bn_new_win>";
+        echo '<tr><td>';
+        if ($row['bn_url'][0] == '#')
+            echo '<a href="'.$row['bn_url'].'">';
+        else if ($row['bn_url'] && $row['bn_url'] != 'http://') {
+            echo '<a href="'.G4_SHOP_URL.'/bannerhit.php?bn_id='.$row['bn_id'].'&amp;url='.urlencode($row['bn_url']).'"'.$bn_new_win.'>';
         }
-        echo "<img src=\"$bimg\" border=\"{$bn_border}\" alt=\"{$row[bn_alt]}\" width=\"{$size[0]}\" height=\"{$size[1]}\"></a>";
-        echo "</td></tr>\n";
+        echo '<img src="'.G4_DATA_URL.'/banner/'.$row['bn_id'].'" border="'.$bn_border.'" alt="'.$row['bn_alt'].'" width="'.$size[0].'" height="'.$size[1].'"></a>';
+        echo '</td></tr>'.PHP_EOL;
     }
 }
 ?>
