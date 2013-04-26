1a046f80b0383fadc2cbfd091b8cf7c6636a6ab7
chicpro <chicpro@gmail.com>
2013-04-26 22:37:50 +0900

    g4s 디렉토리 삭제

--- g4s/adm/_common.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('G4_IS_ADMIN', true);
-include_once ('../common.php');
-include_once(G4_ADMIN_PATH.'/admin.lib.php');
-?>
--- g4s/plugin/gcaptcha/run.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once("./_common.php");
-include_once("./captcha.lib.php");
-$captcha = new captcha();
-$captcha->run();
-?>
