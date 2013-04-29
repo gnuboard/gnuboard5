779e77c034d1108b463b2a1a77146436404aec31
whitedot <whitedot@naver.com>
2013-04-29 16:14:52 +0900

    쇼핑몰: #97 상품분류 표준화 및 스타일 완료

--- shop/boxcategory.inc.php
+++ shop/boxcategory.inc.php
@@ -26,22 +26,24 @@ $sub_menu_left = 100; // 2단계 메뉴 왼쪽 좌표 (1단계 좌표에서 부�
         ?>
         <li class="gnb_1dli" style="z-index:<?php echo $gnb_zindex; ?>">
             <a href="<?php echo G4_SHOP_URL.'/list.php?ca_id='.$row['ca_id']; ?>" class="gnb_1da"><?php echo $row['ca_name']; ?></a>
-            <ul class="gnb_2dul">
-                <?
-                // 2단계 분류 판매 가능한 것만
-                 $sql2 = " select ca_id, ca_name from {$g4['shop_category_table']}
-                   where LENGTH(ca_id) = '4'
-                     and SUBSTRING(ca_id,1,2) = '{$row['ca_id']}'
-                     and ca_use = '1'
-                   order by ca_id ";
-                $result2 = sql_query($sql2);
-                $hnum2 = @mysql_num_rows($result2);
-                for ($j=0; $row2=sql_fetch_array($result2); $j++)
-                {
-                ?>
+            <?
+            // 2단계 분류 판매 가능한 것만
+             $sql2 = " select ca_id, ca_name from {$g4['shop_category_table']}
+               where LENGTH(ca_id) = '4'
+                 and SUBSTRING(ca_id,1,2) = '{$row['ca_id']}'
+                 and ca_use = '1'
+               order by ca_id ";
+            $result2 = sql_query($sql2);
+            $hnum2 = @mysql_num_rows($result2);
+            for ($j=0; $row2=sql_fetch_array($result2); $j++)
+            {
+            if ($j==0) echo '<ul class="gnb_2dul">';
+            ?>
                 <li class="gnb_2dli"><a href="<?php echo G4_SHOP_URL; ?>/list.php?ca_id=<?php echo $row2['ca_id']; ?>" class="gnb_2da"><?php echo $row2['ca_name']; ?></a></li>
-                <? } ?>
-            </ul>
+            <?
+            if ($j==0) echo '</ul>';
+            }
+            ?>
         </li>
         <? } ?>
     </ul>
