3e05e7c2d4161cb8749fc5437924939b66c7c8d6
whitedot <whitedot@naver.com>
2013-04-29 11:30:36 +0900

    쇼핑몰: #100 쇼핑몰 하단 출력 표준화 및 스타일

--- shop/shop.tail.php
+++ shop/shop.tail.php
@@ -9,50 +9,38 @@ $admin = get_admin("super");
 
 </div>
 
+<footer id="ft">
+    <div>
+        <a href="<?php echo G4_SHOP_URL; ?>/" id="ft_logo"><img src="<?php echo G4_DATA_URL; ?>/common/logo_img" alt="처음으로"></a>
+        <ul>
+            <li><a href="<?=G4_SHOP_URL?>/content.php?co_id=company">회사소개</a></li>
+            <li><a href="<?=G4_SHOP_URL?>/content.php?co_id=provision">서비스이용약관</a></li>
+            <li><a href="<?=G4_SHOP_URL?>/content.php?co_id=privacy">개인정보 취급방침</a></li>
+        </ul>
+        <p>
+        <?=$default['de_admin_company_addr']?> /
+        전화 : <?=$default['de_admin_company_tel']?> /
+        팩스 : <?=$default['de_admin_company_fax']?> /
+        운영자 : <?=$admin['mb_name']?><br>
+        사업자 등록번호 : <?=$default['de_admin_company_saupja_no']?> /
+        대표 : <?=$default['de_admin_company_owner']?> /
+        개인정보관리책임자 : <?=$default['de_admin_info_name']?><br>
+        통신판매업신고번호 : <?=$default['de_admin_tongsin_no']?>
+        <? if ($default['de_admin_buga_no']) echo " / 부가통신사업신고번호 : {$default['de_admin_buga_no']}"; ?><br>
+        Copyright &copy; 2001-2013 <?=$default['de_admin_company_name']?>. All Rights Reserved.
+        </p>
+        <a href="#" id="ft_totop">상단으로</a>
+    </div>
+</footer>
 
-<!-- 하단 -->
-<table align=center width='<?=$table_width?>' cellpadding=0 cellspacing=0>
-<tr>
-    <td width=180 bgcolor=#EBEBEB><a href='<?=G4_SHOP_URL?>/'><img src='<?=G4_DATA_URL?>/common/logo_img' border=0 style="filter:gray();"></a></td>
-    <td><img src='<?=G4_SHOP_URL?>/img/tail_img01.gif'></td>
-    <td width=10></td>
-    <td><img src='<?=G4_SHOP_URL?>/img/tail_img02.gif'></td>
-    <td width=770 bgcolor=#EBEBEB style='padding-left:10px;'>
-        <table width=98% cellpadding=0 cellspacing=0 border=0>
-        <tr><td height=30>
-            <a href="<?=G4_SHOP_URL?>/content.php?co_id=company">회사소개</a> |
-            <a href="<?=G4_SHOP_URL?>/content.php?co_id=provision">서비스이용약관</a> |
-            <a href="<?=G4_SHOP_URL?>/content.php?co_id=privacy">개인정보 취급방침</a>
-            </td></tr>
-        <tr><td height=1 bgcolor=#CBCBCB></td></tr>
-        <tr><td height=60 style='line-height:150%'>
-            <FONT COLOR="#46808F">
-                <?=$default['de_admin_company_addr']?> /
-                전화 : <?=$default['de_admin_company_tel']?> /
-                팩스 : <?=$default['de_admin_company_fax']?> /
-                운영자 : <?=$admin['mb_name']?> <BR>
-                사업자 등록번호 : <?=$default['de_admin_company_saupja_no']?> /
-                대표 : <?=$default['de_admin_company_owner']?> /
-                개인정보관리책임자 : <?=$default['de_admin_info_name']?> <br>
-                통신판매업신고번호 : <?=$default['de_admin_tongsin_no']?>
-                <? if ($default['de_admin_buga_no']) echo " / 부가통신사업신고번호 : {$default['de_admin_buga_no']}"; ?>
-                <br>Copyright &copy; 2001-2013 <?=$default['de_admin_company_name']?>. All Rights Reserved. </FONT></td></tr></table>
-    </td>
-</tr>
-</table>
-<!-- 하단끝 -->
-
+<?php if(!G4_IS_MOBILE){ ?>
+<a href="<?php echo $_SERVER['PHP_SELF'].($_SERVER['QUERY_STRING']?'?'.$_SERVER['QUERY_STRING'].'&amp;':'?').'device=mobile'; ?>" id="device_change">모바일 버전으로 보기</a>
+<?php } ?>
 
 <?
 $sec = get_microtime() - $begin_time;
 $file = $_SERVER['PHP_SELF'];
 ?>
-
-</td>
-</tr>
-</table><br/>
-<!-- 전체끝 -->
-
 
 <?
 include_once(G4_PATH.'/tail.sub.php');
