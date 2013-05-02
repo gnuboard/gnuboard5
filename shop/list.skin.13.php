308afa6950d3a14c113ec511b9da88b38214d500
chicpro <chicpro@gmail.com>
2013-05-02 18:02:41 +0900

    리스트에서 상품이미지를 m 사이즈로 변경

--- shop/list.skin.13.php
+++ shop/list.skin.13.php
@@ -21,7 +21,7 @@ for ($i=1; $row=sql_fetch_array($result); $i++)
 ?>
     <li class="sct_li <?php echo $sct_last; ?>">
         <a href="<?php echo $href; ?>" class="sct_a">
-            <span class="sct_img"><?php echo get_it_image($row['it_id'].'_s', $img_width, $img_height, '', $type); ?></span>
+            <span class="sct_img"><?php echo get_it_image($row['it_id'].'_m', $img_width, $img_height, '', $type); ?></span>
             <b><?php echo stripslashes($row['it_name']); ?></b>
             <?php if ($row['it_cust_amount'] && !$row['it_gallery']) { ?>
             <s><?php echo display_amount($row['it_cust_amount']); ?></s>
