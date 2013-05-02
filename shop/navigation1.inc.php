b8366e77252f1edcfe0a574a459e32265107deb2
whitedot <whitedot@naver.com>
2013-05-02 18:23:29 +0900

    쇼핑몰: #118 navigation2.inc.php 내용 이동

--- shop/navigation1.inc.php
+++ shop/navigation1.inc.php
@@ -24,7 +24,12 @@ else
     $str = $g4['title'];
 
 //if ($it_id) $str .= " > $it[it_name]";
-
-include('./navigation2.inc.php');
 ?>
 
+<!-- 네비게이션 -->
+<table width='100%' cellpadding=0 cellspacing=0 align=center>
+<tr><td height=2></td>
+<tr><td height=20 valign=top style='padding-left:2px;'><img src='<?=$g4[shop_img_path]?>/navi_icon.gif' align=absmiddle> 
+    현재위치 : <a href='<?=$g4[path]?>/'>Home</a> > <?=$str?></td></tr>
+<tr><td height=1 bgcolor=#EFEFEF></td></tr>
+</table>
