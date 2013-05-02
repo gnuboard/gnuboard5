7b5d8f370026ba16df9ef409be3fec37a2948c44
gnuboard <kagla@naver.com>
2013-05-02 15:38:46 +0900

    timezone 선언후 mysql 에서도 설정되게 하기

--- config.php
+++ config.php
@@ -9,7 +9,6 @@ define('_GNUBOARD_', true);
 define('G4_TIMEZONE', 'Asia/Seoul');
 
 if (PHP_VERSION >= '5.3.0') {
-    //if (function_exists("date_default_timezone_set")) date_default_timezone_set("Asia/Seoul");
     date_default_timezone_set(G4_TIMEZONE);
 }
 
