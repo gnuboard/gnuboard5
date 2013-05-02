64130a556104ff6a84a7f093dfc2b81e7764ec6c
whitedot <whitedot@naver.com>
2013-05-02 21:39:14 +0900

    쇼핑몰: #120 관련

--- shop/list.sort.php
+++ shop/list.sort.php
@@ -11,13 +11,13 @@ $sct_sort_href = $_SERVER['PHP_SELF'].'?ca_id='.$ca_id.'&amp;skin='.$skin.'&amp;
     </div>
 
     <ul>
-        <li><a href="<?php echo $sct_sort_href; ?>it_amount desc" class="btn01">낮은가격순</a></li>
-        <li><a href="<?php echo $sct_sort_href; ?>it_amount asc" class="btn01">높은가격순</a></li>
-        <li><a href="<?php echo $sct_sort_href; ?>it_name asc" class="btn01">상품명순</a></li>
-        <li><a href="<?php echo $sct_sort_href; ?>it_type1 desc" class="btn01">히트상품</a></li>
-        <li><a href="<?php echo $sct_sort_href; ?>it_type2 desc" class="btn01">추천상품</a></li>
-        <li><a href="<?php echo $sct_sort_href; ?>it_type3 desc" class="btn01">최신상품</a></li>
-        <li><a href="<?php echo $sct_sort_href; ?>it_type4 desc" class="btn01">인기상품</a></li>
-        <li><a href="<?php echo $sct_sort_href; ?>it_type5 desc" class="btn01">할인상품</a></li>
+        <li><a href="<?php echo $sct_sort_href; ?>it_amount%20desc" class="btn01">낮은가격순</a></li>
+        <li><a href="<?php echo $sct_sort_href; ?>it_amount%20asc" class="btn01">높은가격순</a></li>
+        <li><a href="<?php echo $sct_sort_href; ?>it_name%20asc" class="btn01">상품명순</a></li>
+        <li><a href="<?php echo $sct_sort_href; ?>it_type1%20desc" class="btn01">히트상품</a></li>
+        <li><a href="<?php echo $sct_sort_href; ?>it_type2%20desc" class="btn01">추천상품</a></li>
+        <li><a href="<?php echo $sct_sort_href; ?>it_type3%20desc" class="btn01">최신상품</a></li>
+        <li><a href="<?php echo $sct_sort_href; ?>it_type4%20desc" class="btn01">인기상품</a></li>
+        <li><a href="<?php echo $sct_sort_href; ?>it_type5%20desc" class="btn01">할인상품</a></li>
     </ul>
 </section>
