8e07e95ebede02f0eeceb866af4dc0fd25befff6
whitedot <whitedot@naver.com>
2013-05-06 21:20:47 +0900

    쇼핑몰: #141 상품주문 위시리스트 표준화 및 스타일 완료

--- shop/wishlist.php
+++ shop/wishlist.php
@@ -8,85 +8,89 @@ $g4['title'] = "보관함";
 include_once('./_head.php');
 ?>
 
-<img src="<?php echoG4_SHOP_URL; ?>/img/top_wishlist.gif" border="0"><p>
+<div id="sod_ws">
 
-<form name=fwishlist method=post action="" style="padding:0px;">
-<input type=hidden name=act       value="multi">
-<input type=hidden name=sw_direct value=''>
-<input type=hidden name=prog      value='wish'>
-<table width=100% align=center cellpadding=0 cellspacing=0>
-<tr><td colspan=6 height=2 class=c1></td></tr>
-<tr align=center height=28 class=c2>
-    <td colspan=2>상품명</td>
-    <td width=120>보관일시</td>
-    <td width=100>상품체크</td>
-    <td width=50>삭제</td>
-</tr>
-<tr><td colspan=6 height=1 class=c1></td></tr>
-<?php
-$sql = " select *
-           from {$g4['shop_wish_table']} a,
-                {$g4['shop_item_table']} b
-          where a.mb_id = '{$member['mb_id']}'
-            and a.it_id  = b.it_id
-          order by a.wi_id desc ";
-$result = sql_query($sql);
-for ($i=0; $row = mysql_fetch_array($result); $i++) {
+    <form name="fwishlist" method="post" action="">
+    <input type="hidden" name="act"       value="multi">
+    <input type="hidden" name="sw_direct" value="">
+    <input type="hidden" name="prog"      value="wish">
 
-    $out_cd = "";
-    for($k=1; $k<=6; $k++){
-        $opt = trim($row["it_opt{$k}"]);
-        if(preg_match("/\n/", $opt)||preg_match("/;/" , $opt)) {
-            $out_cd = "no";
-            break;
+    <table class="basic_tbl">
+    <thead>
+    <tr>
+        <th scope="col">선택</th>
+        <th scope="col">이미지</th>
+        <th scope="col">상품명</th>
+        <th scope="col">보관일시</th>
+        <th scope="col">삭제</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    $sql = " select *
+               from {$g4['shop_wish_table']} a,
+                    {$g4['shop_item_table']} b
+              where a.mb_id = '{$member['mb_id']}'
+                and a.it_id  = b.it_id
+              order by a.wi_id desc ";
+    $result = sql_query($sql);
+    for ($i=0; $row = mysql_fetch_array($result); $i++) {
+
+        $out_cd = '';
+        for($k=1; $k<=6; $k++){
+            $opt = trim($row['it_opt'.$k]);
+            if(preg_match("/\n/", $opt)||preg_match("/;/" , $opt)) {
+                $out_cd = 'no';
+                break;
+            }
         }
+
+        $it_amount = get_amount($row);
+
+        if ($row['it_tel_inq']) $out_cd = 'tel_inq';
+
+        $image = get_it_image($row['it_id'].'_s', 70, 70, '');
+
+        $s_del = '';
+    ?>
+
+    <tr>
+        <td class="td_chk">
+            <?php
+            // 품절검사
+            $it_stock_qty = get_it_stock_qty($row['it_id']);
+            if($it_stock_qty <= 0)
+            {
+            ?>
+            품절
+            <input type="hidden" name="it_id[$i]">
+            <?php } else { //품절이 아니면 체크할수 있도록한다 ?>
+            <input type="checkbox" name="it_id[<?php echo $i; ?>]"     value="<?php echo $row['it_id']; ?>" onclick="out_cd_check(this, '<?php echo $out_cd; ?>');">
+            <?php } ?>
+            <input type="hidden"   name="it_name[<?php echo $i; ?>]"   value="<?php echo $row['it_name']; ?>">
+            <input type="hidden"   name="it_amount[<?php echo $i; ?>]" value="<?php echo $it_amount; ?>">
+            <input type="hidden"   name="it_point[<?php echo $i; ?>]"  value="<?php echo $row['it_point']; ?>">
+            <input type="hidden"   name="ct_qty[<?php echo $i; ?>]"    value="1">
+        </td>
+        <td class="sod_ws_img"><?php echo $image; ?></td>
+        <td><a href="./item.php?it_id=<?php echo $row['it_id']; ?>"><?php echo stripslashes($row['it_name']); ?></a></td>
+        <td class="td_datetime"><?php echo $row['wi_time']; ?></td>
+        <td class="td_smallmng"><a href="./wishupdate.php?w=d&amp;wi_id=<?php echo $row['wi_id']; ?>">삭제</a></td>
+    </tr>
+    <?php
     }
 
-    $it_amount = get_amount($row);
+    if ($i == 0)
+        echo '<tr><td colspan="5" class="empty_table">보관함이 비었습니다.</td></tr>';
+    ?>
+    </tr>
+    </table>
+    </form>
 
-    if ($row['it_tel_inq']) $out_cd = "tel_inq";
-
-    if ($i > 0)
-        echo "<tr><td colspan=\"20\" height=\"1\" background=\"".G4_SHOP_URL."/dot_line.gif\"></td></tr>\n";
-
-    $image = get_it_image($row['it_id']."_s", 50, 50, $row['it_id']);
-
-    $s_del = "<a href=\"./wishupdate.php?w=d&wi_id={$row['wi_id']}\"><img src=\"".G4_SHOP_URL."/img/btn_del.gif\" border=\"0\" align=\"absmiddle\" alt=\"삭제\"></a>";
-
-    echo "<tr>\n";
-    echo "<td align=center style='padding-top:5px; padding-bottom:5px;'>$image</td>\n";
-    echo "<td><a href=\"./item.php?it_id={$row['it_id']}\">".stripslashes($row['it_name'])."</a></td>\n";
-    echo "<td align=center>{$row['wi_time']}</td>\n";
-    echo "<td align=center>";
-    // 품절검사
-    $it_stock_qty = get_it_stock_qty($row['it_id']);
-    if($it_stock_qty <= 0)
-    {
-        echo "<img src=\"".G4_SHOP_URL."/img/icon_pumjul.gif\" border=\"0\" align=\"absmiddle\">";
-        echo "<input type=hidden name=it_id[$i] >";
-    } else { //품절이 아니면 체크할수 있도록한다
-        echo "<input type=checkbox name=it_id[$i]     value='$row[it_id]' onclick=\"out_cd_check(this, '$out_cd');\">";
-    }
-    echo "<input type=hidden   name=it_name[$i]   value='{$row['it_name']}'>";
-    echo "<input type=hidden   name=it_amount[$i] value='$it_amount'>";
-    echo "<input type=hidden   name=it_point[$i]  value='{$row['it_point']}'>";
-    echo "<input type=hidden   name=ct_qty[$i]    value='1'>";
-    echo "</td>\n";
-    echo "<td align=center>$s_del</td>\n";
-    echo "</tr>\n";
-}
-
-if ($i == 0)
-    echo "<tr><td colspan=20 align=center height=100><span class=point>보관함이 비었습니다.</span></td></tr>\n";
-?>
-</tr>
-<tr><td colspan=6 height=1 class=c1></td></tr>
-</table><br>
-</form>
-
-<div align=right>
-    <a href="javascript:fwishlist_check(document.fwishlist,'');"><img src='<?php echo G4_SHOP_URL; ?>/img/btn_cart_in.gif' border=0></a>
-    <a href="javascript:fwishlist_check(document.fwishlist,'direct_buy');"><img src='<?php echo G4_SHOP_URL; ?>/img/btn_buy.gif' border=0></a>&nbsp;
+    <div id="sod_ws_act">
+        <a href="javascript:fwishlist_check(document.fwishlist,'');" class="btn01">장바구니 담기</a>
+        <a href="javascript:fwishlist_check(document.fwishlist,'direct_buy');" class="btn02">주문하기</a>
+    </div>
 </div>
 
 <script language="JavaScript">
