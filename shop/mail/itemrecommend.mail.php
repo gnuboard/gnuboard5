12cbcbd4eb54a777bb25783e51a5ca354897ad27
whitedot <whitedot@naver.com>
2013-05-08 20:14:31 +0900

    쇼핑몰: #143 상품 관련 메일 발송 추천상품 표준화 및 스타일 완료

--- shop/mail/itemrecommend.mail.php
+++ shop/mail/itemrecommend.mail.php
@@ -1,47 +1,40 @@
-<html>
+<?php //추천상품 ?>
+<!doctype html>
+<html lang="ko">
 <head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8">
-<title>추천 상품</title>
+<meta charset="utf-8">
+<title><?php echo $config['cf_title']; ?> - <?php echo $from_name; ?> 의 상품 추천 메일</title>
 </head>
 
-<style>
-body, th, td, form, input, select, text, textarea, caption { font-size: 12px; font-family:굴림;}
-.line {border: 1px solid #868F98;}
-</style>
+<?php
+$cont_st = 'margin:0 auto 20px;width:94%;border:0;border-collapse:collapse';
+$caption_st = 'padding:0 0 5px;font-weight:bold';
+$th_st = 'padding:5px;border-top:1px solid #e9e9e9;border-bottom:1px solid #e9e9e9;background:#f5f6fa;text-align:left';
+$td_st = 'padding:5px;border-top:1px solid #e9e9e9;border-bottom:1px solid #e9e9e9';
+$empty_st = 'padding:30px;border-top:1px solid #e9e9e9;border-bottom:1px solid #e9e9e9;text-align:center';
+$ft_a_st = 'display:block;padding:30px 0;background:#484848;color:#fff;text-align:center;text-decoration:none';
+?>
 
-<body leftmargin="0" topmargin="20" marginwidth="0" marginheight="20">
-<table width=600 cellpadding=0 cellspacing=0>
-    <tr>
-        <td rowspan=2 align=right valign=top><img src="<?php echo G4_SHOP_URL; ?>/mail/img/mail_left.gif" width="25" height="281"></td>
-        <td height=1 colspan=3 bgcolor=#E3E3E3></td>
-        <td rowspan=2 valign=top><img src="<?php echo G4_SHOP_URL; ?>/mail/img/mail_right.gif" width="25" height="281"></td>
-    </tr>
-    <tr>
-        <td valign=top align=center style='padding-top:15px'>
-            <table width=95% cellpadding=1 cellspacing=0 bgcolor=#C5C5C5>
-                <tr>
-                    <td>
-                        <table width=100% cellpadding=10 cellspacing=0>
-                            <tr><td bgcolor=#2396C5 height=50 align=right><font color="#FFFFFF"><B>추천상품메일</B></font></td></tr>
-                            <tr><td bgcolor=#FFFFFF style="padding-left:15px; padding-bottom:15px; text-align:left;" height=30><font color=#5AB0D4><B><?php echo $from_name; ?> </B></font>님께서 추천하신 상품입니다.
-                                    <table width=98% cellpadding=1 cellspacing=0 bgcolor=#DFDFDF>
-                                        <tr><td>
-                                                <table width=100% cellpadding=20 cellspacing=0 bgcolor=#F8F8F8>
-                                                    <tr><td style="text-align:left;" height=30><b><a href="<?php echo G4_SHOP_URL.'/item.php?it_id='.$it_id; ?>" target=_top><?php echo $it_name; ?></a></b></td></tr>
-                                                    <tr>
-                                                        <td valign=top style="padding:10px;line-height:150%; text-align:left;"><a href="<?php echo G4_SHOP_URL.'/item.php?it_id='.$it_id; ?>" target="_top"><img src="<?php echo G4_DATA_URL.'/item/'.$it_mimg; ?>" border="0" align="left" hspace="10"></a><?php echo $content; ?></td>
-                                                    </tr>
-                                                    <tr><td style="text-align:left;">※ 이 메일은 광고 메일이 아닙니다.</td></tr>
-                                                </table>
-                                            </td></tr>
-                                    </table>
-                                </td></tr>
-                        </table>
-                    </td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-</table>
+<body>
+
+<div style="margin:30px auto;width:600px;border:10px solid #f7f7f7">
+    <div style="border:1px solid #dedede">
+        <h1 style="margin:0 0 20px;padding:30px 30px 20px;background:#f7f7f7;color:#555;font-size:1.4em">
+            <?php echo $config['cf_title'];?> - 상품 추천 메일
+        </h1>
+
+        <p style="<?php echo $cont_st; ?>"><b><?php echo $from_name; ?></b> 님께서 추천하신 상품입니다.</p>
+
+        <div style="text-align:center"><a href="<?php echo G4_SHOP_URL.'/item.php?it_id='.$it_id; ?>" target="_blank"><img src="<?php echo G4_DATA_URL.'/item/'.$it_mimg; ?>" alt=""></a></div>
+
+        <p style="<?php echo $cont_st; ?>"><strong><?php echo $subject; ?></strong></p>
+
+        <p style="<?php echo $cont_st; ?>"><?php echo $content; ?></p>
+
+        <a href="<?php echo G4_SHOP_URL.'/item.php?it_id='.$it_id; ?>" target="_blank" style="<?php echo $ft_a_st; ?>">상품 자세히 보기</a>
+
+    </div>
+</div>
+
 </body>
 </html>
