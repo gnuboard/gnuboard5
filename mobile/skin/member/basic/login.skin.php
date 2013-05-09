159251e169253efcf49dd4596b9b2ccda20f9fd6
whitedot <whitedot@naver.com>
2013-05-09 21:09:34 +0900

    M쇼핑몰,쇼핑몰: #166 로그인 비회원구매 및 주문조회 스타일 완료

--- mobile/skin/member/basic/login.skin.php
+++ mobile/skin/member/basic/login.skin.php
@@ -10,17 +10,18 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
     <form name="flogin" action="<?php echo $login_action_url ?>" onsubmit="return flogin_submit(this);" method="post">
     <input type="hidden" name="url" value='<?php echo $login_url ?>'>
 
-    <fieldset>
-        <label for="login_id" class="sound_only">아이디<strong class="sound_only"> 필수</strong></label>
-        <input type="text" name="mb_id" id="login_id" placeholder="아이디(필수)" required class="frm_input required" maxLength="20">
-        <label for="login_pw" class="sound_only">패스워드<strong class="sound_only"> 필수</strong></label>
-        <input type="password" name="mb_password" id="login_pw" placeholder="패스워드(필수)" required class="frm_input required" maxLength="20">
+    <fieldset id="mb_login_fs">
+        <legend>회원로그인</legend>
+        <label for="login_id" class="login_id sound_only">회원아이디<strong class="sound_only"> 필수</strong></label>
+        <input type="text" name="mb_id" id="login_id" placeholder="회원아이디" required class="frm_input required" size="20" maxLength="20">
+        <label for="login_pw" class="login_pw sound_only">패스워드<strong class="sound_only"> 필수</strong></label>
+        <input type="password" name="mb_password" id="login_pw" placeholder="패스워드" required class="frm_input required" size="20" maxLength="20">
         <input type="submit" value="로그인" class="btn_submit">
         <input type="checkbox" name="auto_login" id="login_auto_login">
         <label for="login_auto_login">자동로그인</label>
     </fieldset>
 
-    <section>
+    <section id="mb_login_info">
         <h2>회원로그인 안내</h2>
         <p>
             회원아이디 및 패스워드가 기억 안나실 때는 아이디/패스워드 찾기를 이용하십시오.<br>
