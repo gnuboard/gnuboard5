aa9494e1d05f90b8fa504b34c56d9145f3eb6c5c
whitedot <whitedot@naver.com>
2013-05-09 10:37:40 +0900

    쇼핑몰: 상단 검색창 required 처리

--- shop/shop.head.php
+++ shop/shop.head.php
@@ -51,7 +51,7 @@ include_once(G4_LIB_PATH.'/popular.lib.php');
                 </span>
 
                 <label for="sch_all_str" class="sound_only">검색어<strong class="sound_only"> 필수</strong></label>
-                <input type="text" name="search_str" value="<?php echo stripslashes(get_text($search_str)); ?>" id="sch_all_str">
+                <input type="text" name="search_str" value="<?php echo stripslashes(get_text($search_str)); ?>" id="sch_all_str" required>
                 <input type="submit" value="검색" id="sch_all_submit">
 
                 </form>
