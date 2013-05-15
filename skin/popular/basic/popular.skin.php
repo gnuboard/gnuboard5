7ea9f1c1fc64a8899b672ee165e31f0e899e42a5
whitedot <whitedot@naver.com>
2013-05-15 18:04:54 +0900

    사용자: #459 인기검색어 스킨 마크업 수정2

--- skin/popular/basic/popular.skin.php
+++ skin/popular/basic/popular.skin.php
@@ -7,10 +7,13 @@ if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
 <aside id="popular">
     <div>
         <h2>인기검색어</h2>
-        <ul>
-        <?php for ($i=0; $i<count($list); $i++) {  ?>
+        <?php
+        for ($i=0; $i<count($list); $i++) {
+            if ($i == 0) echo '<ul>'.PHP_EOL;
+        ?>
             <li><a href="<?php echo G4_BBS_URL ?>/search.php?sfl=wr_subject&amp;sop=and&amp;stx=<?php echo urlencode($list[$i]['pp_word']) ?>"><?php echo $list[$i]['pp_word'] ?></a></li>
-        <?php }  ?>
-        </ul>
+        <?php }
+        if ($i > 0) echo '</ul>'.PHP_EOL;
+        ?>
     </div>
 </aside>
