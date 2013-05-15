3566f99357f9b9d491dd66e0f1779266ca6278a9
gnuboard <kagla@naver.com>
2013-05-15 09:30:55 +0900

    G4_KCP 를 G4_KCPCERT 로 상수명 변경 및 kcp 폴더를 kcpcert 로 변경

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
-<?<?php
+<?php
 
 /********************
     상수 선언
@@ -48,7 +48,7 @@ define('G4_SKIN_DIR',       'skin');
 define('G4_GCAPTCHA_DIR',   'gcaptcha');
 define('G4_CKEDITOR_DIR',   'ckeditor');
 define('G4_MOBILE_DIR',     'mobile');
-define('G4_KCP_DIR',        'kcp');
+define('G4_KCPCERT_DIR',    'kcpcert');
 define('G4_SNS_DIR',        'sns');
 define('G4_SYNDI_DIR',      'syndi');
 
@@ -78,7 +78,7 @@ define('G4_SKIN_URL',       G4_URL.'/'.G4_SKIN_DIR);
 define('G4_PLUGIN_URL',     G4_URL.'/'.G4_PLUGIN_DIR);
 define('G4_GCAPTCHA_URL',   G4_PLUGIN_URL.'/'.G4_GCAPTCHA_DIR);
 define('G4_CKEDITOR_URL',   G4_PLUGIN_URL.'/'.G4_CKEDITOR_DIR); // CKEDITOR 의 라이브러리 경로
-define('G4_KCP_URL',        G4_PLUGIN_URL.'/'.G4_KCP_DIR);
+define('G4_KCPCERT_URL',    G4_PLUGIN_URL.'/'.G4_KCPCERT_DIR);
 define('G4_SNS_URL',        G4_PLUGIN_URL.'/'.G4_SNS_DIR);
 define('G4_SYNDI_URL',      G4_PLUGIN_URL.'/'.G4_SYNDI_DIR);
 define('G4_MOBILE_URL',     G4_URL.'/'.G4_MOBILE_DIR);
@@ -93,7 +93,7 @@ define('G4_PLUGIN_PATH',    G4_PATH.'/'.G4_PLUGIN_DIR);
 define('G4_SKIN_PATH',      G4_PATH.'/'.G4_SKIN_DIR);
 define('G4_GCAPTCHA_PATH',  G4_PLUGIN_PATH.'/'.G4_GCAPTCHA_DIR);
 define('G4_CKEDITOR_PATH',  G4_PLUGIN_PATH.'/'.G4_CKEDITOR_DIR);
-define('G4_KCP_PATH',       G4_PLUGIN_PATH.'/'.G4_KCP_DIR);
+define('G4_KCPCERT_PATH',   G4_PLUGIN_PATH.'/'.G4_KCPCERT_DIR);
 define('G4_SNS_PATH',       G4_PLUGIN_PATH.'/'.G4_SNS_DIR);
 define('G4_SYNDI_PATH',     G4_PLUGIN_PATH.'/'.G4_SYNDI_DIR);
 define('G4_MOBILE_PATH',    G4_PATH.'/'.G4_MOBILE_DIR);
