79cdc228e4208ea1ad7957895cb4bf2ac53f17d2
gnuboard <kagla@naver.com>
2013-05-15 09:30:55 +0900

    G4_KCP 를 G4_KCPCERT 로 상수명 변경 및 kcp 폴더를 kcpcert 로 변경

--- skin/member/basic/register_form.skin.php
+++ skin/member/basic/register_form.skin.php
@@ -234,7 +234,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 <?php 
 if ($config['cf_kcpcert_use']) {
     // 휴대폰인증 form
-    include_once(G4_KCP_PATH.'/kcpcert_form.php');
+    include_once(G4_KCPCERT_PATH.'/kcpcert_form.php');
 ?>
 <script>
 $(function() {
