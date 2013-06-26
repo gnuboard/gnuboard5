f1b187fc1aac2796d98419bba064f71683fde7a5
chicpro <chicpro@gmail.com>
2013-06-26 18:40:32 +0900

    2단 분류 항목이 노출되지 않는 오류 해결

--- head.sub.php
+++ head.sub.php
@@ -71,7 +71,11 @@ if ($is_admin) {
 ?>
 </script>
 <script src="<?php echo G4_JS_URL ?>/jquery-1.8.3.min.js"></script>
+<?php if (defined('_SHOP_')) { ?>
+<script src="<?php echo G4_JS_URL ?>/jquery.shop.menu.js"></script>
+<?php } else { ?>
 <script src="<?php echo G4_JS_URL ?>/jquery.menu.js"></script>
+<?php } ?>
 <script src="<?php echo G4_JS_URL ?>/common.js"></script>
 <script src="<?php echo G4_JS_URL ?>/wrest.js"></script>
 <?php
---
+++ js/jquery.shop.menu.js
@@ -0,0 +1,86 @@
+$(function(){
+    var hide_menu = false;
+    var mouse_event = false;
+    var oldX = oldY = 0;
+
+    $(document).mousemove(function(e) {
+        if(oldX == 0) {
+            oldX = e.pageX;
+            oldY = e.pageY;
+        }
+
+        if(oldX != e.pageX || oldY != e.pageY) {
+            mouse_event = true;
+        }
+    });
+
+    // 주메뉴
+    var $gnb = $(".gnb_1dli > a");
+    $gnb.mouseover(function() {
+        if(mouse_event) {
+            $(".gnb_1dli").removeClass("gnb_1dli_over gnb_1dli_over2 gnb_1dli_on");
+            $(this).parent().addClass("gnb_1dli_over gnb_1dli_on");
+            hide_menu = false;
+        }
+    });
+
+    $gnb.mouseout(function() {
+        hide_menu = true;
+    });
+
+    $(".gnb_2dli").mouseover(function() {
+        hide_menu = false;
+    });
+
+    $(".gnb_2dli").mouseout(function() {
+        hide_menu = true;
+    });
+
+    $gnb.focusin(function() {
+        $(".gnb_1dli").removeClass("gnb_1dli_over gnb_1dli_over2 gnb_1dli_on");
+        $(this).parent().addClass("gnb_1dli_over gnb_1dli_on");
+        hide_menu = false;
+    });
+
+    $gnb.focusout(function() {
+        hide_menu = true;
+    });
+
+    $(".gnb_2da").focusin(function() {
+        $(".gnb_1dli").removeClass("gnb_1dli_over gnb_1dli_over2 gnb_1dli_on");
+        var $gnb_li = $(this).closest(".gnb_1dli").addClass("gnb_1dli_over gnb_1dli_on");
+        menu_rearrange($(this).closest(".gnb_1dli"));
+        hide_menu = false;
+    });
+
+    $(".gnb_2da").focusout(function() {
+        hide_menu = true;
+    });
+
+    $('#gnb_1dul>li').bind('mouseleave',function(){
+        submenu_hide();
+    });
+
+    $(document).bind('click focusin',function(){
+        if(hide_menu) {
+            submenu_hide();
+        }
+    });
+
+    // 텍스트 리사이즈 카운트 쿠키있으면 실행
+    var resize_act;
+    var text_resize_count = parseInt(get_cookie("ck_font_resize_count"));
+    if(!isNaN(text_resize_count)) {
+        if(text_resize_count > 0)
+            resize_act = "increase";
+        else if(text_resize_count < 0)
+            resize_act = "decrease";
+
+        if(Math.abs(text_resize_count) > 0)
+            font_resize2("container", resize_act, Math.abs(text_resize_count));
+    }
+});
+
+function submenu_hide() {
+    $(".gnb_1dli").removeClass("gnb_1dli_over gnb_1dli_over2 gnb_1dli_on");
+}
