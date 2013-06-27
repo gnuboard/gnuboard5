8dd78d5b8ee8f273f6606972febd270cf82e5da0
chicpro <chicpro@gmail.com>
2013-06-27 17:10:50 +0900

    동일ID로인한 결제모듈 설치안내 메세지 계속 출력되는 오류 수정

--- shop/orderform.php
+++ shop/orderform.php
@@ -140,10 +140,12 @@ function init_pay_button()
     */
     // 체크 방법이 변경
     if( GetPluginObject() == null ){
+        document.getElementById("display_setup_message_top").style.display = "block" ;
         document.getElementById("display_setup_message").style.display = "block" ;
         document.getElementById("display_pay_button").style.display = "none" ;
     }
     else{
+        document.getElementById("display_setup_message_top").style.display = "none" ;
         document.getElementById("display_setup_message").style.display = "none" ;
         document.getElementById("display_pay_button").style.display = "block" ;
     }
@@ -158,7 +160,7 @@ function get_intall_file()
 </script>
 
     <!-- Payplus Plug-in 설치 안내 -->
-    <p id="display_setup_message" style="display:block">
+    <p id="display_setup_message_top" style="display:block">
         <strong>결제안내</strong>
         <span class="red">결제를 하시려면 상단의 노란색 표시줄을 클릭</span>하시거나, <a href="https://pay.kcp.co.kr/plugin_new/file/KCPPluginSetup.exe" onclick="return get_intall_file();"><span class="bold">[수동설치]</span></a>를 눌러 Payplus Plug-in을 설치하시기 바랍니다.<br>
         [수동설치]를 눌러 설치하신 경우 <span class="red bold">새로고침(F5)키</span>를 눌러 진행하시기 바랍니다.
@@ -945,7 +947,6 @@ function get_intall_file()
         ?>
     </section>
 
-<<<<<<< HEAD
     <!-- Payplus Plug-in 설치 안내 -->
     <p id="display_setup_message" style="display:block">
         <span class="red">결제를 계속 하시려면 상단의 노란색 표시줄을 클릭</span>하시거나 <a href="https://pay.kcp.co.kr/plugin_new/file/KCPPluginSetup.exe" onclick="return get_intall_file();"><b><u>[수동설치]</u></b></a>를 눌러 다운로드 된 Payplus Plug-in을 설치하시기 바랍니다.<br>
@@ -953,9 +954,6 @@ function get_intall_file()
         새로고침(F5) 한후에도 계속 설치파일이 다운로드 되거나 결제가 되지 않으면 브라우저를 새로 열어서 주문해 주시기 바랍니다.<br>
         브라우저가 익스플로러가 아닌 경우 Payplus Plug-in 설치에 문제가 있을수 있음을 알려 드립니다.
     </p>
-
-=======
->>>>>>> 225fa4a75dd51016367658455d6c416890c90b64
     <div id="display_pay_button" class="btn_confirm" style="display:none">
         <input type="submit" value="주문하기" class="btn_submit">
         <a href="javascript:history.go(-1);" class="btn01">취소</a>
