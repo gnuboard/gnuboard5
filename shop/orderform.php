ca742d0668d5f5c76dfbb7da77c1d576cc2b6e68
gnuboard <kagla@naver.com>
2013-06-28 09:24:00 +0900

    머지 충돌 해결

--- shop/orderform.php
+++ shop/orderform.php
@@ -48,12 +48,7 @@ if (file_exists('./settle_'.$default['de_card_pg'].'.inc.php')) {
 <script>
 function CheckPayplusInstall()
 {
-    if (ChkBrowser())
-    {
-        document.writeln(KCPUX_OBJ); //-- inner 형식 변경
-        //setTimeout("plugin_Set_Time_Out('ie');", 100);
-    }
-    else
+    if(!ChkBrowser())
     {
         var inst = 0;
         for (var i = 0; i < navigator.plugins.length; i++)
@@ -88,19 +83,10 @@ function CheckPayplusInstall()
             document.location.href = GetInstallFile();
             return false;
         }
-        else
-        {
-            //var plugin_info = "<embed id='plugin' " + "type=" + KCP_GetPluginVersion() + " pluginspage='' width='0' height='0'></embed>";
-            //document.write(plugin_info);
-
-            setTimeout("plugin_Set_Time_Out('mozilra');", 100);
-        }
     }
 
     StartSmartUpdate();
     setTimeout("init_pay_button();",300);
-
-    return true;
 }
 
 /* Payplus Plug-in 실행 */
@@ -140,10 +126,12 @@ function init_pay_button()
     */
     // 체크 방법이 변경
     if( GetPluginObject() == null ){
+        document.getElementById("display_setup_message_top").style.display = "block" ;
         document.getElementById("display_setup_message").style.display = "block" ;
         document.getElementById("display_pay_button").style.display = "none" ;
     }
     else{
+        document.getElementById("display_setup_message_top").style.display = "none" ;
         document.getElementById("display_setup_message").style.display = "none" ;
         document.getElementById("display_pay_button").style.display = "block" ;
     }
@@ -158,7 +146,7 @@ function get_intall_file()
 </script>
 
     <!-- Payplus Plug-in 설치 안내 -->
-    <p id="display_setup_message" style="display:block">
+    <p id="display_setup_message_top" style="display:block">
         <strong>결제안내</strong>
         <span class="red">결제를 하시려면 상단의 노란색 표시줄을 클릭</span>하시거나, <a href="https://pay.kcp.co.kr/plugin_new/file/KCPPluginSetup.exe" onclick="return get_intall_file();"><span class="bold">[수동설치]</span></a>를 눌러 Payplus Plug-in을 설치하시기 바랍니다.<br>
         [수동설치]를 눌러 설치하신 경우 <span class="red bold">새로고침(F5)키</span>를 눌러 진행하시기 바랍니다.
@@ -952,7 +940,10 @@ function get_intall_file()
         새로고침(F5) 한후에도 계속 설치파일이 다운로드 되거나 결제가 되지 않으면 브라우저를 새로 열어서 주문해 주시기 바랍니다.<br>
         브라우저가 익스플로러가 아닌 경우 Payplus Plug-in 설치에 문제가 있을수 있음을 알려 드립니다.
     </p>
+<<<<<<< HEAD
 
+=======
+>>>>>>> 7c3b78792011aaf93dbbe968034d2ca094405b1f
     <div id="display_pay_button" class="btn_confirm" style="display:none">
         <input type="submit" value="주문하기" class="btn_submit">
         <a href="javascript:history.go(-1);" class="btn01">취소</a>
@@ -1010,8 +1001,6 @@ function get_intall_file()
 </div>
 
 <script>
-CheckPayplusInstall();
-
 $(function() {
     var $cp_btn_el;
     var $cp_row_el;
@@ -1623,3 +1612,7 @@ function gumae2baesong(f)
 <?php
 include_once('./_tail.php');
 ?>
+
+<script>
+CheckPayplusInstall();
+</script>
