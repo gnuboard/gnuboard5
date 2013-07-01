92df4e2aeeefb4b1f1969d848142afd1a7740b34
whitedot <whitedot@naver.com>
2013-07-01 16:16:58 +0900

    쇼핑몰: 스킨 동기화

--- skin/poll/shop_basic/poll_result.skin.php
+++ skin/poll/shop_basic/poll_result.skin.php
@@ -1,79 +1,85 @@
-<?
+<?php
 if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
 ?>
 
-<div id="poll_result" class="new_win">
-    <h1><?php echo $g4['title']; ?></h1>
+<!-- 설문조사 결과 시작 { -->
+<link rel="stylesheet" href="<?php echo $poll_skin_url ?>/style.css">
 
+<div id="poll_result" class="new_win">
+    <h1 id="new_win_title"><?php echo $g4['title'] ?></h1>
+
+    <!-- 설문조사 결과 그래프 시작 { -->
     <section id="poll_result_list">
-        <h2><?php echo $po_subject; ?> 결과</h2>
+        <h2><?php echo $po_subject ?> 결과</h2>
 
         <dl>
-            <dt><span>전체 <?php echo $nf_total_po_cnt; ?>표</span></dt>
+            <dt><span>전체 <?php echo $nf_total_po_cnt ?>표</span></dt>
             <dd>
                 <ol>
-                <? for ($i=1; $i<=count($list); $i++) { ?>
+                <?php for ($i=1; $i<=count($list); $i++) {  ?>
                     <li>
                         <p>
-                            <?php echo $list[$i]['content']; ?>
-                            <strong><?php echo $list[$i]['cnt']; ?> 표</strong>
-                            <span><?php echo number_format($list[$i]['rate'], 1); ?> 퍼센트</span>
+                            <?php echo $list[$i]['content'] ?>
+                            <strong><?php echo $list[$i]['cnt'] ?> 표</strong>
+                            <span><?php echo number_format($list[$i]['rate'], 1) ?> 퍼센트</span>
                         </p>
                         <div class="poll_result_graph">
-                            <span style="width:<?php echo number_format($list[$i]['rate'], 1); ?>%"></span>
+                            <span style="width:<?php echo number_format($list[$i]['rate'], 1) ?>%"></span>
                         </div>
                     </li>
-                <? } ?>
+                <?php }  ?>
                 </ol>
             </dd>
         </dl>
     </section>
+    <!-- } 설문조사 결과 그래프 끝 -->
 
-    <? if ($is_etc) { ?>
+    <!-- 설문조사 기타의견 시작 { -->
+    <?php if ($is_etc) {  ?>
     <section id="poll_result_cmt">
         <h2>이 설문에 대한 기타의견</h2>
 
-        <? for ($i=0; $i<count($list2); $i++) { ?>
+        <?php for ($i=0; $i<count($list2); $i++) {  ?>
         <article>
             <header>
-                <h1><?php echo $list2[$i]['pc_name']; ?><span class="sound_only">님의 의견</span></h1>
-                <?php echo $list2[$i]['name']; ?>
-                <span class="poll_datetime"><?php echo $list2[$i]['datetime']; ?></span>
+                <h1><?php echo $list2[$i]['pc_name'] ?><span class="sound_only">님의 의견</span></h1>
+                <?php echo $list2[$i]['name'] ?>
+                <span class="poll_datetime"><?php echo $list2[$i]['datetime'] ?></span>
             </header>
             <p>
-                <?php echo $list2[$i]['idea']; ?>
+                <?php echo $list2[$i]['idea'] ?>
             </p>
             <footer>
-                <span class="poll_cmt_del"><? if ($list2[$i]['del']) { echo $list2[$i]['del']."삭제</a>"; } ?></span>
+                <span class="poll_cmt_del"><?php if ($list2[$i]['del']) { echo $list2[$i]['del']."삭제</a>"; }  ?></span>
             </footer>
         </article>
-        <? } ?>
+        <?php }  ?>
 
-        <? if ($member['mb_level'] >= $po['po_level']) { ?>
+        <?php if ($member['mb_level'] >= $po['po_level']) {  ?>
         <form name="fpollresult" action="./poll_etc_update.php" onsubmit="return fpollresult_submit(this);" method="post" autocomplete="off">
-        <input type="hidden" name="po_id" value="<?php echo $po_id; ?>">
+        <input type="hidden" name="po_id" value="<?php echo $po_id ?>">
         <input type="hidden" name="w" value="">
-        <input type="hidden" name="skin_dir" value="<?php echo $skin_dir; ?>">
-        <? if ($is_member) { ?><input type="hidden" name="pc_name" value="<?php echo cut_str($member['mb_nick'],255); ?>"><? } ?>
-        <h3><?php echo $po_etc; ?></h3>
+        <input type="hidden" name="skin_dir" value="<?php echo $skin_dir ?>">
+        <?php if ($is_member) {  ?><input type="hidden" name="pc_name" value="<?php echo cut_str($member['mb_nick'],255) ?>"><?php }  ?>
+        <h3><?php echo $po_etc ?></h3>
         <table id="poll_result_wcmt" class="frm_tbl">
         <tbody>
-        <? if ($is_guest) { ?>
+        <?php if ($is_guest) {  ?>
         <tr>
             <th scope="row"><label for="pc_name">이름<strong class="sound_only">필수</strong></label></th>
             <td><input type="text" name="pc_name" id="pc_name" required class="frm_input required" size="10"></td>
         </tr>
-        <? } ?>
+        <?php }  ?>
         <tr>
             <th scope="row"><label for="pc_idea">의견<strong class="sound_only">필수</strong></label></th>
             <td><input type="text" id="pc_idea" name="pc_idea" required class="frm_input required" size="47" maxlength="100"></td>
         </tr>
-        <? if ($is_guest) { ?>
+        <?php if ($is_guest) {  ?>
         <tr>
             <th scope="row">자동등록방지</th>
             <td><?php echo captcha_html(); ?></td>
         </tr>
-        <? } ?>
+        <?php }  ?>
         </tbody>
         </table>
 
@@ -81,22 +87,25 @@ if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
             <input type="submit" class="btn_submit" value="의견남기기">
         </div>
         </form>
-        <? } ?>
+        <?php }  ?>
 
     </section>
-    <? } ?>
+    <?php }  ?>
+    <!-- } 설문조사 기타의견 끝 -->
 
-    <section id="poll_result_oth">
+    <!-- 설문조사 다른 결과 보기 시작 { -->
+    <aside id="poll_result_oth">
         <h2>다른 투표 결과 보기</h2>
         <ul>
-            <? for ($i=0; $i<count($list3); $i++) { ?>
-            <li><a href="./poll_result.php?po_id=<?php echo $list3[$i]['po_id']; ?>&amp;skin_dir=<?php echo $skin_dir; ?>">[<?php echo $list3[$i]['date']; ?>] <?php echo $list3[$i]['subject']; ?></a></li>
-            <? } ?>
+            <?php for ($i=0; $i<count($list3); $i++) {  ?>
+            <li><a href="./poll_result.php?po_id=<?php echo $list3[$i]['po_id'] ?>&amp;skin_dir=<?php echo $skin_dir ?>">[<?php echo $list3[$i]['date'] ?>] <?php echo $list3[$i]['subject'] ?></a></li>
+            <?php }  ?>
         </ul>
-    </section>
+    </aside>
+    <!-- } 설문조사 다른 결과 보기 끝 -->
 
     <div class="btn_win">
-        <a href="javascript:;" onclick="window.close();">창닫기</a>
+        <button type="button" onclick="window.close();">창닫기</button>
     </div>
 </div>
 
@@ -110,8 +119,9 @@ $(function() {
 
 function fpollresult_submit(f)
 {
-    <? if ($is_guest) { echo chk_captcha_js(); } ?>
+    <?php if ($is_guest) { echo chk_captcha_js(); }  ?>
 
     return true;
 }
 </script>
+<!-- } 설문조사 결과 끝 -->
