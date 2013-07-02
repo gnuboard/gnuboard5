f6e9e2c75de80c5cce5216bacacc290867b60334
chicpro <chicpro@gmail.com>
2013-07-02 18:25:10 +0900

    KCPCERT_PATH 상수 적용

--- mobile/skin/member/shop_basic/register_form_update.head.skin.php
+++ mobile/skin/member/shop_basic/register_form_update.head.skin.php
@@ -2,6 +2,6 @@
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
 if ($config['cf_kcpcert_use']) {
-    include_once(G4_KCP_PATH.'/kcpcert.head.skin.php');
+    include_once(G4_KCPCERT_PATH.'/kcpcert.head.skin.php');
 }
 ?>
