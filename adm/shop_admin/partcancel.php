c120d092c71b478c79b8681b2d60de0f1ea34a3a
chicpro <chicpro@gmail.com>
2013-07-05 17:59:51 +0900

    #250 취소가능 금액을 총액으로만 표시하도록 수정

--- adm/shop_admin/partcancel.php
+++ adm/shop_admin/partcancel.php
@@ -26,40 +26,6 @@ if($od['od_settle_case'] == '계좌이체')
     $mod_type = 'STPA';
 
 $available_cancel = $od['od_receipt_amount'] - $od['od_cancel_card'];
-
-// 복합과세 사용시 각각 취소가능 금액
-if($default['de_tax_flag_use']) {
-    $sql = " select ct_price, io_price, io_type, ct_qty, ct_notax, cp_amount
-                from {$g4['shop_cart_table']}
-                where uq_id = '{$od['uq_id']}'
-                  and ct_status IN ( '주문', '준비', '배송', '완료' ) ";
-    $result = sql_query($sql);
-
-    $tot_tax_mny = 0;
-    $tot_free_mny = 0;
-
-    for($i=0; $row=sql_fetch_array($result); $i++) {
-        if($row['ct_notax']) {
-            if($row['io_type']) {
-                $tot_free += ($row['io_priece'] * $row['ct_qty']);
-            } else {
-                $tot_free += (($row['ct_price'] + $row['io_price']) * $row['ct_qty']);
-            }
-
-            $tot_free -= $row['cp_amount'];
-        } else {
-            if($row['io_type']) {
-                $tot_tax += ($row['io_priece'] * $row['ct_qty']);
-            } else {
-                $tot_tax += (($row['ct_price'] + $row['io_price']) * $row['ct_qty']);
-            }
-
-            $tot_tax -= $row['cp_amount'];
-        }
-    }
-
-    //$tot_tax -= ($od['od_send_cost'] + $od['od_send_cost2'] - $od['od_coupon'] - $od['od_send_coupon'] - $od['od_receipt_point']);
-}
 ?>
 
 <div>
@@ -72,11 +38,10 @@ if($default['de_tax_flag_use']) {
     <p>
         <?php echo $g4['title']; ?>
     </p>
-    <?php if($default['de_tax_flag_use']) { ?>
     <p>
-        과세 취소가능금액 : <?php echo number_format($tot_tax); ?> 원<br>
-        비과세 취소가능금액 : <?php echo number_format($tot_free); ?> 원
+        취소가능금액 : <?php echo number_format($available_cancel); ?> 원
     </p>
+    <?php if($default['de_tax_flag_use']) { ?>
     <p>
         <label for="tax_mny">과세 취소요청금액</label>
         <input type="text" name="tax_mny" id="tax_mny" size="20"> 원
@@ -86,9 +51,6 @@ if($default['de_tax_flag_use']) {
         <input type="text" name="mod_free_mny" id="mod_free_mny" size="20"> 원
     </p>
     <?php } else { ?>
-    <p>
-        취소가능금액 : <?php echo number_format($available_cancel); ?> 원
-    </p>
     <p>
         <label for="mod_mny">취소요청금액</label>
         <input type="text" name="mod_mny" id="mod_mny" size="20" required class="required"> 원
