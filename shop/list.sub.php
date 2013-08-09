d5ba685fabb43fdcbc0cb94a13920ac0a8b1edae
whitedot <whitedot@naver.com>
2013-08-09 10:48:08 +0900

    쇼핑몰공통: 상품유형보기 버튼 마크업 수정

--- shop/list.sub.php
+++ shop/list.sub.php
@@ -2,7 +2,7 @@
 if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
 ?>
 
-<div id="sct_lst">
-    <button type="button" class="sct_lst_view sct_lst_list">리스트뷰<span></span></button>
-    <button type="button" class="sct_lst_view sct_lst_gallery">갤러리뷰<span></span></button>
-</div>
+<ul id="sct_lst">
+    <li><button type="button" class="sct_lst_view sct_lst_list">리스트뷰<span></span></button></li>
+    <li><button type="button" class="sct_lst_view sct_lst_gallery">갤러리뷰<span></span></button></li>
+</ul>
