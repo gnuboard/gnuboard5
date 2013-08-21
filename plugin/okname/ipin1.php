c284dbd772f6cda45ca44673627f9e1b6bd44e46
chicpro <chicpro@gmail.com>
2013-08-21 16:42:13 +0900

    모듈 실행파일 오류 알림 코드 수정

--- plugin/okname/ipin1.php
+++ plugin/okname/ipin1.php
@@ -10,8 +10,16 @@ $cmd = "$exe $keypath $memid \"{$reserved1}\" \"{$reserved2}\" $EndPointURL $log
 // 실행
 exec($cmd, $out, $ret);
 
-if($ret == 126 || $ret == -1) {
-    alert_close('모듈 파일의 실행권한이 없습니다.\\n\\nchmod 755 okname 과 같이 실행권한을 부여해 주십시오');
+if($ret == 127) {
+    alert_close('모듈실행 파일이 존재하지 않습니다.\\n\\n'.basename($exe).' 파일이 '.G4_PLUGIN_DIR.'/'.G4_OKNAME_DIR.'/bin 안에 있어야 합니다.');
+}
+
+if($ret == 126) {
+    alert_close('모듈실행 파일의 실행권한이 없습니다.\\n\\nchmod 755 '.basename($exe).' 과 같이 실행권한을 부여해 주십시오.');
+}
+
+if($ret == -1) {
+    alert_close('모듈실행 파일의 실행권한이 없습니다.\\n\\ncmd.exe의 IUSER 실행권한이 있는지 확인하여 주십시오.');
 }
 
 $pubkey = "";
