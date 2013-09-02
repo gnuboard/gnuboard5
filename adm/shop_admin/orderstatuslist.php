5991376cc83f21a9446de5b071223f4f36accbd6
chicpro <chicpro@gmail.com>
2013-09-02 10:00:16 +0900

    #284 card_history 전자결제내역 링크 삭제

--- adm/shop_admin/orderstatuslist.php
+++ adm/shop_admin/orderstatuslist.php
@@ -124,7 +124,6 @@ if ($search) // 검색렬일 때만 처음 버튼을 보여줌
 
     <div class="btn_add sort_with">
         <a href="./orderprint.php" class="btn_add_optional">주문내역출력</a>
-        <a href="./ordercardhistory.php" class="btn_add_optional">전자결제내역</a>
     </div>
 
     <ul class="sort_odr">
