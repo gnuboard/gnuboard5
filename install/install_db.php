fd46aefaddaec5335492116f8e65145c7257a6bc
gnuboard <kagla@naver.com>
2013-09-13 11:10:15 +0900

    그누보드5

--- install/install_db.php
+++ install/install_db.php
@@ -7,8 +7,8 @@ header('Cache-Control: no-store, no-cache, must-revalidate'); // HTTP/1.1
 header('Cache-Control: pre-check=0, post-check=0, max-age=0'); // HTTP/1.1
 header('Pragma: no-cache'); // HTTP/1.0
 
-$title = G5_VERSION." 설치 3단계 중 3단계 설치 완료";
 include_once ('../config.php');
+$title = G5_VERSION." 설치 3단계 중 3단계 설치 완료";
 include_once ('./install.inc.php');
 
 //print_r($_POST); exit;
