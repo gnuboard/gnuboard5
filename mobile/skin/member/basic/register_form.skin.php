f2a5866c52b390e006820af9e7bb969d7841254c
whitedot <whitedot@naver.com>
2013-10-14 17:25:47 +0900

    모바일: 리디자인 작업 중

--- mobile/skin/member/basic/register_form.skin.php
+++ mobile/skin/member/basic/register_form.skin.php
@@ -2,6 +2,8 @@
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 ?>
 
+<link rel="stylesheet" href="<?php echo $member_skin_url ?>/style.css">
+
 <div class="mbskin">
     <script src="<?php echo G5_JS_URL ?>/jquery.register_form.js"></script>
     <?php if($config['cf_cert_use'] && ($config['cf_cert_ipin'] || $config['cf_cert_hp'])) { ?>
