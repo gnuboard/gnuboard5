207f45818647c46cc1270a1c94ae711bc3d810ad
gnuboard <kagla@naver.com>
2013-10-16 16:27:34 +0900

    캡챠사용시 ss_bo_table, ss_wr_id 세션이 생기지 않던 오류 수정 & g5_captcha_path 제거

--- plugin/kcaptcha/kcaptcha_image.php
+++ plugin/kcaptcha/kcaptcha_image.php
@@ -1,9 +1,7 @@
 <?php
 include_once("_common.php");
-//error_reporting (E_ALL);
-include('captcha.lib.php');
+include_once('captcha.lib.php');
 
-//session_start();
 $captcha = new KCAPTCHA();
 $captcha->setKeyString(get_session("ss_captcha_key"));
 $captcha->getKeyString();
