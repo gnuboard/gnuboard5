99549c0a52148253e92b846a0fa9598c66a93b39
gnuboard <kagla@naver.com>
2013-10-23 16:19:17 +0900

    $g_conf_site_key 추가

--- shop/kcp/pp_ax_hub.php
+++ shop/kcp/pp_ax_hub.php
@@ -24,7 +24,8 @@ if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
         $g_conf_log_dir   = G5_SHOP_PATH.'/kcp/log';
     }
 
-    $g_conf_site_cd = $_POST['site_cd'];
+    $g_conf_site_cd  = $_POST['site_cd'];
+    $g_conf_site_key = $default['de_kcp_mid'];
 
     if (preg_match("/^T000/", $g_conf_site_cd) || $default['de_card_test']) {
         $g_conf_gw_url  = "testpaygw.kcp.co.kr";                    // real url : paygw.kcp.co.kr , test url : testpaygw.kcp.co.kr
