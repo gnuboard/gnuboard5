be20b7333d3b7451836865712d0c85cbf2332d8f
gnuboard <kagla@naver.com>
2013-11-07 17:51:43 +0900

    관련상품의 출력여부 기능 추가

--- skin/shop/basic/item.info.skin.php
+++ skin/shop/basic/item.info.skin.php
@@ -97,6 +97,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 <!-- } 교환/반품 끝 -->
 <?php } ?>
 
+<?php if ($default['de_rel_list_use']) { ?>
 <!-- 관련상품 시작 { -->
 <section id="sit_rel">
     <h2>관련상품</h2>
@@ -104,15 +105,20 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
     <div class="sct_wrap">
         <?php
-        $sql = " select b.* from {$g5['g5_shop_item_relation_table']} a left join {$g5['g5_shop_item_table']} b on (a.it_id2=b.it_id) where a.it_id = '{$it['it_id']}' and b.it_use='1' ";
+        // set_query() 를 사용하므로 limit 를 지정합니다.
+        if ($default['de_rel_list_row'] < 1) $default['de_rel_list_row'] = 1;
+        
+        $items = $default['de_rel_list_mod'] * $default['de_rel_list_row'];
+        $sql = " select b.* from {$g5['g5_shop_item_relation_table']} a left join {$g5['g5_shop_item_table']} b on (a.it_id2=b.it_id) where a.it_id = '{$it['it_id']}' and b.it_use='1' limit $items ";
 
-        $list = new item_list("relation.skin.php", $default['de_rel_list_mod'], 1, $default['de_rel_img_width'], $default['de_rel_img_height']);
+        $list = new item_list($default['de_rel_list_skin'], $default['de_rel_list_mod'], $default['de_rel_list_row'], $default['de_rel_img_width'], $default['de_rel_img_height']);
         $list->set_query($sql);
         echo $list->run();
         ?>
     </div>
 </section>
 <!-- } 관련상품 끝 -->
+<?php } ?>
 
 <script>
 $(window).on("load", function() {
