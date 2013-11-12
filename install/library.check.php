057bf044261e95cec9d56065bbb2bb3e4b179209
chicpro <chicpro@gmail.com>
2013-11-12 15:35:00 +0900

    설치시 soap 모듈 체크 코드 추가

--- install/library.check.php
+++ install/library.check.php
@@ -10,5 +10,9 @@ function die_utf8($msg)
 }
 
 if(!extension_loaded('gd') || !function_exists('gd_info'))
-    die('GD 라이브러리를 설치하신 후 '.G5_VERSION.' 설치를 진행해 주십시오.');
+    die_utf8('GD 라이브러리를 설치하신 후 '.G5_VERSION.' 설치를 진행해 주십시오.');
+
+// SOAP 모듈체크
+if(!extension_loaded('soap') || !class_exists('SOAPClient'))
+    die_utf8('SOAP 확장모듈을 설치하신 후 '.G5_VERSION.' 설치를 진행해 주십시오.');
 ?>
