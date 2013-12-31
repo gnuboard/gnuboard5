9f109f203ebbce5b1dbd9f475c2090c16139ed56
chicpro <chicpro@gmail.com>
2013-12-31 09:06:33 +0900

    jquery datepicker 적용되지 않는 오류 수정

--- adm/visit.sub.php
+++ adm/visit.sub.php
@@ -1,9 +1,9 @@
 <?php
 if (!defined('_GNUBOARD_')) exit;
 
-include_once(G5_PLUGIN_PATH.'/jquery-ui/datepicker.php');
 include_once(G5_LIB_PATH.'/visit.lib.php');
 include_once('./admin.head.php');
+include_once(G5_PLUGIN_PATH.'/jquery-ui/datepicker.php');
 
 if (empty($fr_date)) $fr_date = G5_TIME_YMD;
 if (empty($to_date)) $to_date = G5_TIME_YMD;
@@ -38,7 +38,7 @@ $query_string = $qstr ? '?'.$qstr : '';
 
 <script>
 $(function(){
-    $("#fr_date, #to_date").datepicker({ changeMonth: true, changeYear: true, dateFormat: "yy-mm-dd", showButtonPanel: true, yearRange: "c-99:c+99", maxDate: "+0d" }); 
+    $("#fr_date, #to_date").datepicker({ changeMonth: true, changeYear: true, dateFormat: "yy-mm-dd", showButtonPanel: true, yearRange: "c-99:c+99", maxDate: "+0d" });
 });
 
 function fvisit_submit(act)
