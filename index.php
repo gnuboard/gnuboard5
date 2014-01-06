491f012828ed758e81d9925db9223b3588fa5460
chicpro <chicpro@gmail.com>
2014-01-06 17:28:58 +0900

    사용자 추가한 include 파일이 먼저 실행되도록 수정

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 define('_INDEX_', true);
 include_once('./_common.php');
 
-if (G5_IS_MOBILE) {
-    include_once(G5_MOBILE_PATH.'/index.php');
-    return;
-}
-
 // 초기화면 파일 경로 지정 : 이 코드는 가능한 삭제하지 마십시오.
 if ($config['cf_include_index']) {
     if (!@include_once($config['cf_include_index'])) {
         die('기본환경 설정에서 초기화면 파일 경로가 잘못 설정되어 있습니다.');
     }
     return; // 이 코드의 아래는 실행을 하지 않습니다.
+}
+
+if (G5_IS_MOBILE) {
+    include_once(G5_MOBILE_PATH.'/index.php');
+    return;
 }
 
 include_once('./_head.php');
