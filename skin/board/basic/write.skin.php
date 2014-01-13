7e7e9c17b7e79f80c94b991440f9a39bc52ca8d1
whitedot <whitedot@naver.com>
2014-01-13 09:21:41 +0900

    공통: 게시판 글쓰기 페이지의 완료 버튼명을 '글쓰기'에서 '작성완료'로 수정, 휴먼(humansoft)님 건의

--- skin/board/basic/write.skin.php
+++ skin/board/basic/write.skin.php
@@ -173,7 +173,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
     </div>
 
     <div class="btn_confirm">
-        <input type="submit" value="글쓰기" id="btn_submit" accesskey="s" class="btn_submit">
+        <input type="submit" value="작성완료" id="btn_submit" accesskey="s" class="btn_submit">
         <a href="./board.php?bo_table=<?php echo $bo_table ?>" class="btn_cancel">취소</a>
     </div>
     </form>
