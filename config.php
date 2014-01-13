0cdaa1fd2d45bb6a965230281f86bfaa1adacf10
chicpro <chicpro@gmail.com>
2014-01-13 10:31:48 +0900

    썸네일 이미지 품질 config.php 에서 설정할 수 있도록 수정

--- config.php
+++ config.php
@@ -156,4 +156,7 @@ define('G5_SMTP', '127.0.0.1');
 // 게시판에서 링크의 기본개수를 말합니다.
 // 필드를 추가하면 이 숫자를 필드수에 맞게 늘려주십시오.
 define('G5_LINK_COUNT', 2);
+
+// 썸네일 jpg Quality 설정
+define('G5_THUMB_JPG_QUALITY', 90);
 ?>
