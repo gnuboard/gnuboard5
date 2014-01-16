05f56175d33a9f962dfc278833a3937c49e60ea8
whitedot <whitedot@naver.com>
2014-01-16 17:25:02 +0900

    쇼핑몰: 관련상품목록 스타일 기본상품목록 스타일과 분리

--- skin/shop/basic/relation.10.skin.php
+++ skin/shop/basic/relation.10.skin.php
@@ -19,7 +19,7 @@ for ($i=1; $row=sql_fetch_array($result); $i++) {
         if ($this->css) {
             echo "<ul class=\"{$this->css}\">\n";
         } else {
-            echo "<ul class=\"sct sct_10\">\n";
+            echo "<ul class=\"sct scr_10\">\n";
         }
     }
 
