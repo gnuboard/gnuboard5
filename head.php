15cb9221dcfdae9aef4c47b5ff8f88613eea26b7
thisgun <thisgun@naver.com>
2014-02-12 11:56:00 +0900

    그누보드5 메뉴중 FAQ메뉴란 추가

--- head.php
+++ head.php
@@ -92,6 +92,7 @@ if (G5_IS_MOBILE) {
             <li><a href="<?php echo G5_BBS_URL ?>/register.php">회원가입</a></li>
             <li><a href="<?php echo G5_BBS_URL ?>/login.php"><b>로그인</b></a></li>
             <?php }  ?>
+            <li><a href="<?php echo G5_BBS_URL ?>/faq.php">FAQ</a></li>
             <li><a href="<?php echo G5_BBS_URL ?>/qalist.php">1:1문의</a></li>
             <li><a href="<?php echo G5_BBS_URL ?>/current_connect.php">접속자 <?php echo connect(); // 현재 접속자수  ?></a></li>
             <li><a href="<?php echo G5_BBS_URL ?>/new.php">새글</a></li>
