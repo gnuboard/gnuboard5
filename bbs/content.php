c4113e661d61bde66aa08315ac9a7175ef03ed52
thisgun <thisgun@naver.com>
2014-02-13 13:38:49 +0900

    내용관리 FAQ관리 수정

--- bbs/content.php
+++ bbs/content.php
@@ -5,6 +5,28 @@ include_once('./_common.php');
 if( !isset($g5['content_table']) ){
     die('<meta charset="utf-8">관리자 모드에서 게시판관리->내용 관리를 먼저 확인해 주세요.');
 }
+//내용(컨텐츠)정보 테이블이 있는지 검사한다.
+if(!sql_query(" DESCRIBE {$g5['content_table']} ", false)) {
+    if(sql_query(" DESCRIBE {$g5['g5_shop_content_table']} ", false)) {
+        sql_query(" ALTER TABLE {$g5['g5_shop_content_table']} RENAME TO `{$g5['content_table']}` ;", false);
+    } else {
+       $query_cp = sql_query(" CREATE TABLE IF NOT EXISTS `{$g5['content_table']}` (
+                      `co_id` varchar(20) NOT NULL DEFAULT '',
+                      `co_html` tinyint(4) NOT NULL DEFAULT '0',
+                      `co_subject` varchar(255) NOT NULL DEFAULT '',
+                      `co_content` longtext NOT NULL,
+                      `co_hit` int(11) NOT NULL DEFAULT '0',
+                      `co_include_head` varchar(255) NOT NULL,
+                      `co_include_tail` varchar(255) NOT NULL,
+                      PRIMARY KEY (`co_id`)
+                    ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ", true);
+
+        // 내용관리 생성
+        sql_query(" insert into `{$g5['content_table']}` set co_id = 'company', co_html = '1', co_subject = '회사소개', co_content= '<p align=center><b>회사소개에 대한 내용을 입력하십시오.</b></p>' ", false );
+        sql_query(" insert into `{$g5['content_table']}` set co_id = 'privacy', co_html = '1', co_subject = '개인정보 처리방침', co_content= '<p align=center><b>개인정보 처리방침에 대한 내용을 입력하십시오.</b></p>' ", false );
+        sql_query(" insert into `{$g5['content_table']}` set co_id = 'provision', co_html = '1', co_subject = '서비스 이용약관', co_content= '<p align=center><b>서비스 이용약관에 대한 내용을 입력하십시오.</b></p>' ", false );
+    }
+}
 
 // 내용
 $sql = " select * from {$g5['content_table']} where co_id = '$co_id' ";
