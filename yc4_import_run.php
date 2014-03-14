34bbb833ca6db39c9c69fdd26522dd6e99f3df65
chicpro <chicpro@gmail.com>
2014-03-14 15:32:05 +0900

    변환프로그램에서는 gnb 메뉴 표시하지 않도록 수정

--- yc4_import_run.php
+++ yc4_import_run.php
@@ -151,25 +151,7 @@ document.onkeydown = noRefresh ;
     <nav id="gnb">
         <h2>메인메뉴</h2>
         <ul id="gnb_1dul">
-            <?php
-            $sql = " select * from {$g5['group_table']} where gr_show_menu = '1' and gr_device <> 'mobile' order by gr_order ";
-            $result = sql_query($sql);
-            for ($gi=0; $row=sql_fetch_array($result); $gi++) { // gi 는 group index
-             ?>
-            <li class="gnb_1dli">
-                <a href="<?php echo G5_BBS_URL ?>/group.php?gr_id=<?php echo $row['gr_id'] ?>" class="gnb_1da"><?php echo $row['gr_subject'] ?></a>
-                <ul class="gnb_2dul">
-                    <?php
-                    $sql2 = " select * from {$g5['board_table']} where gr_id = '{$row['gr_id']}' and bo_show_menu = '1' and bo_device <> 'mobile' order by bo_order ";
-                    $result2 = sql_query($sql2);
-                    for ($bi=0; $row2=sql_fetch_array($result2); $bi++) { // bi 는 board index
-                     ?>
-                    <li class="gnb_2dli"><a href="<?php echo G5_BBS_URL ?>/board.php?bo_table=<?php echo $row2['bo_table'] ?>" class="gnb_2da"><?php echo $row2['bo_subject'] ?></a></li>
-                    <?php } ?>
-                </ul>
-            </li>
-            <?php } ?>
-            <?php if ($gi == 0) {  ?><li class="gnb_empty">생성된 메뉴가 없습니다.</li><?php }  ?>
+            <li class="gnb_empty">메뉴는 표시하지 않습니다.</li>
         </ul>
     </nav>
 </div>
