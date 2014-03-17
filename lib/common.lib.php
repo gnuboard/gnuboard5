6927401c1d87970fbd8342bf8da4580a180f71f7
chicpro <chicpro@gmail.com>
2014-03-17 11:10:42 +0900

    회원발송 코드 수정

--- lib/common.lib.php
+++ lib/common.lib.php
@@ -2619,8 +2619,8 @@ function conv_date_format($format, $date, $add='')
 // unescape nl 얻기
 function conv_unescape_nl($str)
 {
-    $search = array('\\r', '\\\n', '\\n', '\r');
-    $replace = array('', '\n', '\n', '');
+    $search = array('\\r', '\r', '\\n', '\n');
+    $replace = array('', '', "\n", "\n");
 
     return str_replace($search, $replace, $str);
 }
