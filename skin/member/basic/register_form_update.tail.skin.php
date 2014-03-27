45bf83a7d1d3ee08943ebe3ba86ddfe6380960bf
chicpro <chicpro@gmail.com>
2014-03-27 15:20:08 +0900

    회원가입 문자 발송되지 않는 오류 수정

---
+++ skin/member/basic/register_form_update.tail.skin.php
@@ -0,0 +1,32 @@
+<?php
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+
+//----------------------------------------------------------
+// SMS 문자전송 시작
+//----------------------------------------------------------
+
+$sms_contents = $default['de_sms_cont1'];
+$sms_contents = str_replace("{이름}", $mb_name, $sms_contents);
+$sms_contents = str_replace("{회원아이디}", $mb_id, $sms_contents);
+$sms_contents = str_replace("{회사명}", $default['de_admin_company_name'], $sms_contents);
+
+// 핸드폰번호에서 숫자만 취한다
+$receive_number = preg_replace("/[^0-9]/", "", $mb_hp);  // 수신자번호 (회원님의 핸드폰번호)
+$send_number = preg_replace("/[^0-9]/", "", $default['de_admin_company_tel']); // 발신자번호
+
+if ($w == "" && $default['de_sms_use1'] && $receive_number)
+{
+	if ($config['cf_sms_use'] == 'icode')
+	{
+		include_once(G5_LIB_PATH.'/icode.sms.lib.php');
+
+		$SMS = new SMS; // SMS 연결
+        $SMS->SMS_con($config['cf_icode_server_ip'], $config['cf_icode_id'], $config['cf_icode_pw'], $config['cf_icode_server_port']);
+		$SMS->Add($receive_number, $send_number, $config['cf_icode_id'], iconv("utf-8", "euc-kr", stripslashes($sms_contents)), "");
+		$SMS->Send();
+	}
+}
+//----------------------------------------------------------
+// SMS 문자전송 끝
+//----------------------------------------------------------
+?>
