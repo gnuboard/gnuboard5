8a407bebbbc46fd42ab28a9ac36a90785621ae0c
chicpro <chicpro@gmail.com>
2014-04-08 15:06:47 +0900

    접속자 기본 스킨 지정

--- lib/connect.lib.php
+++ lib/connect.lib.php
@@ -2,7 +2,7 @@
 if (!defined('_GNUBOARD_')) exit;
 
 // 현재 접속자수 출력
-function connect($skin_dir='')
+function connect($skin_dir='basic')
 {
     global $config, $g5;
 
@@ -10,8 +10,6 @@ function connect($skin_dir='')
     $sql = " select sum(IF(mb_id<>'',1,0)) as mb_cnt, count(*) as total_cnt from {$g5['login_table']}  where mb_id <> '{$config['cf_admin']}' ";
     $row = sql_fetch($sql);
 
-    if (!$skin_dir) 
-        $skin_dir = $config['cf_connect_skin'];
     if(G5_IS_MOBILE) {
         $connect_skin_path = G5_MOBILE_PATH.'/'.G5_SKIN_DIR.'/connect/'.$skin_dir;
         $connect_skin_url  = G5_MOBILE_URL.'/'.G5_SKIN_DIR.'/connect/'.$skin_dir;
