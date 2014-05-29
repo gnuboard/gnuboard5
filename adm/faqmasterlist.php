e422a938832499cec10d875153dc58c517f3e586
chicpro <chicpro@gmail.com>
2014-05-29 13:28:18 +0900

    관리자 리스트 관리 링크 순서 변경

--- adm/faqmasterlist.php
+++ adm/faqmasterlist.php
@@ -105,8 +105,8 @@ $result = sql_query($sql);
         <td class="td_num"><?php echo $cnt; ?></td>
         <td class="td_num"><?php echo $row['fm_order']?></td>
         <td class="td_mng">
-            <a href="<?php echo G5_BBS_URL; ?>/faq.php?fm_id=<?php echo $row['fm_id']; ?>"><span class="sound_only"><?php echo stripslashes($row['fm_subject']); ?> </span>보기</a>
             <a href="./faqmasterform.php?w=u&amp;fm_id=<?php echo $row['fm_id']; ?>"><span class="sound_only"><?php echo stripslashes($row['fm_subject']); ?> </span>수정</a>
+            <a href="<?php echo G5_BBS_URL; ?>/faq.php?fm_id=<?php echo $row['fm_id']; ?>"><span class="sound_only"><?php echo stripslashes($row['fm_subject']); ?> </span>보기</a>
             <a href="./faqmasterformupdate.php?w=d&amp;fm_id=<?php echo $row['fm_id']; ?>" onclick="return delete_confirm();"><span class="sound_only"><?php echo stripslashes($row['fm_subject']); ?> </span>삭제</a>
         </td>
     </tr>
