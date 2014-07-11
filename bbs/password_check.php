0c79231eb9b0a6f28dafa4569c7311ba807f7bb2
chicpro <chicpro@gmail.com>
2014-07-11 13:57:54 +0900

    비회원 비밀댓글 내용확인 기능 추가

--- bbs/password_check.php
+++ bbs/password_check.php
@@ -15,6 +15,19 @@ if ($w == 's') {
     //set_session("ss_secret", "$bo_table|$wr[wr_num]");
     set_session($ss_name, TRUE);
 
+} else if ($w == 'sc') {
+    $qstr = 'bo_table='.$bo_table.'&amp;sfl='.$sfl.'&amp;stx='.$stx.'&amp;sop='.$sop.'&amp;wr_id='.$wr_id.'&amp;page='.$page;
+
+    $wr = get_write($write_table, $wr_id);
+
+    if (sql_password($wr_password) != $wr['wr_password'])
+        alert('비밀번호가 틀립니다.');
+
+    // 세션에 아래 정보를 저장. 하위번호는 비밀번호없이 보아야 하기 때문임.
+    $ss_name = 'ss_secret_comment_'.$bo_table.'_'.$wr['wr_id'];
+    //set_session("ss_secret", "$bo_table|$wr[wr_num]");
+    set_session($ss_name, TRUE);
+
 } else
     alert('w 값이 제대로 넘어오지 않았습니다.');
 
