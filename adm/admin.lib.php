05b40efddf00d63736652ae120819c1b0a8b9cb7
chicpro <chicpro@gmail.com>
2014-09-25 16:59:06 +0900

    관리자 로그인 알림 후 이동경로 수정

--- adm/admin.lib.php
+++ adm/admin.lib.php
@@ -192,7 +192,7 @@ function order_select($fld, $sel='')
 if (!$member['mb_id'])
 {
     //alert('로그인 하십시오.', '$g5['bbs_path']/login.php?url=' . urlencode('$_SERVER['PHP_SELF']?w=$w&mb_id=$mb_id'));
-    alert('로그인 하십시오.', G5_BBS_URL.'/login.php?url=' . urlencode($_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING']));
+    alert('로그인 하십시오.', G5_BBS_URL.'/login.php?url=' . urlencode(G5_ADMIN_URL));
 }
 else if ($is_admin != 'super')
 {
