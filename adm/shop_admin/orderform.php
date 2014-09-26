cb8c4ae16ec0eed9f22fa9c63694b4372210270a
chicpro <chicpro@gmail.com>
2014-09-26 14:43:18 +0900

    관리자는 무조건 현금영수증 발급하도록 수정

--- adm/shop_admin/orderform.php
+++ adm/shop_admin/orderform.php
@@ -531,8 +531,7 @@ add_javascript(G5_POSTCODE_JS, 0);    //다음 주소 js
                     </td>
                 </tr>
                 <?php
-                if ($default['de_taxsave_use'] && $amount['미수'] == 0) {
-                    if ($od['od_receipt_price'] && ($od['od_settle_case'] == '무통장' || $od['od_settle_case'] == '가상계좌' || $od['od_settle_case'] == '계좌이체')) {
+                if ($od['od_misu'] == 0 && $od['od_receipt_price'] && ($od['od_settle_case'] == '무통장' || $od['od_settle_case'] == '가상계좌' || $od['od_settle_case'] == '계좌이체')) {
                 ?>
                 <tr>
                     <th scope="row">현금영수증</th>
@@ -568,7 +567,6 @@ add_javascript(G5_POSTCODE_JS, 0);    //다음 주소 js
                     </td>
                 </tr>
                 <?php
-                    }
                 }
                 ?>
                 </tbody>
