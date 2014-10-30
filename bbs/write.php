d3154bd94f35a718886b23ea7c77b15902cb34b5
chicpro <chicpro@gmail.com>
2014-10-30 18:28:02 +0900

    오타 수정

--- bbs/write.php
+++ bbs/write.php
@@ -408,7 +408,7 @@ include_once('./board_head.php');
 
 $action_url = https_url(G5_BBS_DIR)."/write_update.php";
 
-echo '<!-- skin : '.(G5_IS_MOBEILE ? $board['bo_mobile_skin'] : $board['bo_skin']).' -->';
+echo '<!-- skin : '.(G5_IS_MOBILE ? $board['bo_mobile_skin'] : $board['bo_skin']).' -->';
 include_once ($board_skin_path.'/write.skin.php');
 
 include_once('./board_tail.php');
