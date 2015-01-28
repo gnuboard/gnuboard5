b34d31f6a742fb40334a3302df0b4f6c985af55d
chicpro <chicpro@gmail.com>
2015-01-28 10:35:36 +0900

    경로 코드 오류 수정

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ define('_INDEX_', true);
 include_once('./_common.php');
 
 // 초기화면 파일 경로 지정 : 이 코드는 가능한 삭제하지 마십시오.
-if ($config['cf_include_index'] && is_file(G5_PATH.'/'.$config['cf_include_index']) {
+if ($config['cf_include_index'] && is_file(G5_PATH.'/'.$config['cf_include_index'])) {
     include_once($config['cf_include_index']);
     return; // 이 코드의 아래는 실행을 하지 않습니다.
 }
