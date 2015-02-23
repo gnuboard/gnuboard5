6695abda2f05cd88dd7ae02a5978fb3a90b04456
chicpro <chicpro@gmail.com>
2015-02-23 16:16:55 +0900

    iOS8 환경에서 접속자 수 표시 오류 수정

--- mobile/skin/connect/basic/connect.skin.php
+++ mobile/skin/connect/basic/connect.skin.php
@@ -5,5 +5,4 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
 add_stylesheet('<link rel="stylesheet" href="'.$connect_skin_url.'/style.css">', 0);
 ?>
-
 <?php echo $row['total_cnt'] ?>
