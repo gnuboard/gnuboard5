cf915e2fa7cd0c34cc49f425527699ffdf557820
chicpro <chicpro@sir.co.kr>
2015-09-16 12:09:19 +0900

    log 폴더 권한 체크 코드 추가

--- adm/shop_admin/configform.php
+++ adm/shop_admin/configform.php
@@ -1593,6 +1593,23 @@ if($default['de_iche_use'] || $default['de_vbank_use'] || $default['de_hp_use'] 
             }
         }
     }
+
+    // 카카오페이의 경우 log 디렉토리 체크
+    if($default['de_kakaopay_mid'] && $default['de_kakaopay_key'] && $default['de_kakaopay_enckey'] && $default['de_kakaopay_hashkey'] && $default['de_kakaopay_cancelpwd']) {
+        $log_path = G5_SHOP_PATH.'/kakaopay/log';
+
+        if(!is_dir($log_path)) {
+            echo '<script>'.PHP_EOL;
+            echo 'alert("'.str_replace(G5_PATH.'/', '', G5_SHOP_PATH).'/kakaopay 폴더 안에 log 폴더를 생성하신 후 쓰기권한을 부여해 주십시오.\n> mkdir log\n> chmod 707 log");'.PHP_EOL;
+            echo '</script>'.PHP_EOL;
+        } else {
+            if(!is_writable($log_path)) {
+                echo '<script>'.PHP_EOL;
+                echo 'alert("'.str_replace(G5_PATH.'/', '',$log_path).' 폴더에 쓰기권한을 부여해 주십시오.\n> chmod 707 log");'.PHP_EOL;
+                echo '</script>'.PHP_EOL;
+            }
+        }
+    }
 }
 
 include_once (G5_ADMIN_PATH.'/admin.tail.php');
