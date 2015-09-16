544b4de21db1b0a992ac921d5d0f79441448678d
chicpro <chicpro@sir.co.kr>
2015-09-16 11:04:57 +0900

    카카오페이 결제요청 레이어 위치 수정

--- shop/kakaopay/orderform.3.php
+++ shop/kakaopay/orderform.3.php
@@ -3,4 +3,8 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 ?>
 
 <!-- TODO :  LayerPopup의 Target DIV 생성 -->
-<div id="kakaopay_layer"  style="display: none"></div>
+<script>
+$(function() {
+    $("body").append('<div id="kakaopay_layer"  style="display: none"></div>');
+});
+</script>
