614dce643c9a182de87344e427728753a6d9a936
chicpro <chicpro@sir.co.kr>
2015-11-17 16:37:30 +0900

    캡챠 오류 수정 및 파일명 변경

--- plugin/kcaptcha/kcaptcha.lib.php
+++ plugin/kcaptcha/kcaptcha.lib.php
@@ -237,14 +237,18 @@ class KCAPTCHA{
 // 캡챠 HTML 코드 출력
 function captcha_html($class="captcha")
 {
+    if(is_mobile())
+        $class .= ' m_captcha';
+
     $html .= "\n".'<script>var g5_captcha_url  = "'.G5_CAPTCHA_URL.'";</script>';
     //$html .= "\n".'<script>var g5_captcha_path = "'.G5_CAPTCHA_PATH.'";</script>';
     $html .= "\n".'<script src="'.G5_CAPTCHA_URL.'/kcaptcha.js"></script>';
     $html .= "\n".'<fieldset id="captcha" class="'.$class.'">';
     $html .= "\n".'<legend><label for="captcha_key">자동등록방지</label></legend>';
+    if (is_mobile()) $html .= '<audio src="#" id="captcha_audio" controls></audio>';
     //$html .= "\n".'<img src="#" alt="" id="captcha_img">';
     $html .= "\n".'<img src="javascript:void(0);" alt="" id="captcha_img">';
-    $html .= "\n".'<button type="button" id="captcha_mp3"><span></span>숫자음성듣기</button>';
+    if (!is_mobile()) $html .= "\n".'<button type="button" id="captcha_mp3"><span></span>숫자음성듣기</button>';
     $html .= "\n".'<button type="button" id="captcha_reload"><span></span>새로고침</button>';
     $html .= '<input type="text" name="captcha_key" id="captcha_key" required class="captcha_box required" size="6" maxlength="6">';
     $html .= "\n".'<span id="captcha_info">자동등록방지 숫자를 순서대로 입력하세요.</span>';
