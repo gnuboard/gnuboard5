287fdd6eb3a33242e65955fe49fa7cccce282f28
chicpro <chicpro@sir.co.kr>
2016-02-04 10:08:27 +0900

    중복된 코드 삭제

--- adm/shop_admin/orderdeliveryupdate.php
+++ adm/shop_admin/orderdeliveryupdate.php
@@ -3,7 +3,6 @@ $sub_menu = '400400';
 include_once('./_common.php');
 include_once('./admin.shop.lib.php');
 include_once(G5_LIB_PATH.'/mailer.lib.php');
-include_once(G5_LIB_PATH.'/icode.sms.lib.php');
 
 auth_check($auth[$sub_menu], "w");
 
