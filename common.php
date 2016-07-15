70f6d92e874ce8da04871068f635d368adc79d58
chicpro <chicpro@sir.co.kr>
2016-07-15 09:25:39 +0900

    XSS 취약점 수정 16-480

--- common.php
+++ common.php
@@ -39,6 +39,7 @@ function g5_path()
     $host = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME'];
     if(isset($_SERVER['HTTP_HOST']) && preg_match('/:[0-9]+$/', $host))
         $host = preg_replace('/:[0-9]+$/', '', $host);
+    $host = preg_replace("/[\<\>\'\"\\\'\\\"\%\=\(\)\/\^\*]/", '', $host);
     $result['url'] = $http.$host.$port.$user.$root;
     return $result;
 }
