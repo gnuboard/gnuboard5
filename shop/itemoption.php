83d13c546e899653929eef018c4a524b31ea4f5e
thisgun <thisgun@naver.com>
2017-01-31 11:45:36 +0900

    부분옵션 검색 관련 부분 수정

--- shop/itemoption.php
+++ shop/itemoption.php
@@ -8,11 +8,16 @@ $opt_id = preg_replace($pattern, '', $_POST['opt_id']);
 $idx    = preg_replace('#[^0-9]#', '', $_POST['idx']);
 $sel_count = preg_replace('#[^0-9]#', '', $_POST['sel_count']);
 
+/*
+옵션명 비슷한 부분 오류 수정
+수정자 : IT FOR ONE
+수정 내용 : and io_id like '$opt_id%' => and io_id like '$opt_id".chr(30)."'
+*/
 $sql = " select * from {$g5['g5_shop_item_option_table']}
                 where io_type = '0'
                   and it_id = '$it_id'
                   and io_use = '1'
-                  and io_id like '$opt_id%'
+                  and io_id like '$opt_id".chr(30)."%'
                 order by io_no asc ";
 $result = sql_query($sql);
 
