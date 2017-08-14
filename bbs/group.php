7e5c171a6d15d852f2af6ecf6baef07a6415e9f9
thisgun <thisgun@naver.com>
2017-08-14 20:30:22 +0900

    리뉴얼소스 수정

--- bbs/group.php
+++ bbs/group.php
@@ -38,14 +38,15 @@ $sql .= " order by bo_order ";
 $result = sql_query($sql);
 for ($i=0; $row=sql_fetch_array($result); $i++) {
     $lt_style = "";
-    if ($i%2==1) $lt_style = "margin-left:20px";
-    else $lt_style = "";
+    if ($i%2==1) $lt_style = "margin-left:15px";
+    if ($i%2==0) $lt_style = "clear:both";
 ?>
     <div style="float:left;<?php echo $lt_style ?>">
     <?php
     // 이 함수가 바로 최신글을 추출하는 역할을 합니다.
     // 사용방법 : latest(스킨, 게시판아이디, 출력라인, 글자수);
-    echo latest('basic', $row['bo_table'], 5, 70);
+    // 테마의 스킨을 사용하려면 theme/basic 과 같이 지정
+    echo latest('basic', $row['bo_table'], 5, 17);
     ?>
     </div>
 <?php
--- index.php
+++ index.php
@@ -29,10 +29,12 @@ if(!$is_admin)
 $sql .= " order by b.gr_order, a.bo_order ";
 $result = sql_query($sql);
 for ($i=0; $row=sql_fetch_array($result); $i++) {
+    $lt_style = "";
+
     if ($i%2==1) $lt_style = "margin-left:15px";
-    else $lt_style = "";
+    if ($i%2==0) $lt_style = "clear:both";
 ?>
-    <div style="float:left;<?php echo $lt_style ?>" class="lt_wr">
+    <div style="float:left;<?php echo $lt_style ?>">
         <?php
         // 이 함수가 바로 최신글을 추출하는 역할을 합니다.
         // 사용방법 : latest(스킨, 게시판아이디, 출력라인, 글자수);
