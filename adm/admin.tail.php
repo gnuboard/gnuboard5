f145b1293246e3d7f857675f7a55b08c2af864a1
thisgun <thisgun@naver.com>
2017-08-25 14:27:54 +0900

    관리자 페이지 수정

--- adm/admin.tail.php
+++ adm/admin.tail.php
@@ -9,15 +9,22 @@ if (!defined('_GNUBOARD_')) exit;
             </p>
         </noscript>
 
+        </div>    
+        <footer id="ft">
+            <p>
+                Copyright &copy; <?php echo $_SERVER['HTTP_HOST']; ?>. All rights reserved.<br>
+               <button type="button" class="scroll_top"><span class="top_img"></span><span class="top_txt">TOP</span></button>
+           </p>
+        </footer>
     </div>
+
 </div>
 
-<footer id="ft">
-    <p>
-        Copyright &copy; <?php echo $_SERVER['HTTP_HOST']; ?>. All rights reserved.<br>
-        <a href="#">상단으로</a>
-    </p>
-</footer>
+<script>
+$(".scroll_top").click(function(){
+     $("body,html").animate({scrollTop:0},400);
+})
+</script>
 
 <!-- <p>실행시간 : <?php echo get_microtime() - $begin_time; ?> -->
 
