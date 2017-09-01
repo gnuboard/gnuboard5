bb896d81aa43fc0f8b39e5e0ede3fd8100666668
thisgun <thisgun@naver.com>
2017-09-01 14:19:06 +0900

    틀린 구문 수정

--- theme/basic/head.php
+++ theme/basic/head.php
@@ -2,7 +2,7 @@
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
 if (G5_IS_MOBILE) {
-    include_once(G5_MOBILE_PATH.'/head.php');
+    include_once(G5_THEME_MOBILE_PATH.'/head.php');
     return;
 }
 
