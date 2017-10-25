43310e615b2acc19a164b5fc32e2f2081dee10d1
thisgun <thisgun@naver.com>
2017-10-25 12:10:51 +0900

    리뉴얼 visit 수정

--- skin/visit/basic/visit.skin.php
+++ skin/visit/basic/visit.skin.php
@@ -11,14 +11,14 @@ add_stylesheet('<link rel="stylesheet" href="'.$visit_skin_url.'/style.css">', 0
 <section id="visit">
     <h2><i class="fa fa-users" aria-hidden="true"></i>  접속자집계</h2>
     <dl>
-        <dt><i class="fa fa-user-o" aria-hidden="true"></i> 오늘</dt>
-        <dd><?php echo number_format($visit[1]) ?></dd>
-        <dt><i class="fa fa-user-o" aria-hidden="true"></i> 어제</dt>
-        <dd><?php echo number_format($visit[2]) ?></dd>
-        <dt><i class="fa fa-user-o" aria-hidden="true"></i> 최대</dt>
-        <dd><?php echo number_format($visit[3]) ?></dd>
-        <dt><i class="fa fa-user-o" aria-hidden="true"></i> 전체</dt>
-        <dd><?php echo number_format($visit[4]) ?></dd>
+        <dt><span class="color_1"></span> 오늘</dt>
+        <dd><strong class="color_1"><?php echo number_format($visit[1]) ?></strong></dd>
+        <dt><span class="color_2"></span> 어제</dt>
+        <dd><strong class="color_2"><?php echo number_format($visit[2]) ?></strong></dd>
+        <dt><span class="color_3"></span> 최대</dt>
+        <dd><strong class="color_3"><?php echo number_format($visit[3]) ?></strong></dd>
+        <dt><span class="color_4"></span> 전체</dt>
+        <dd><strong class="color_4"><?php echo number_format($visit[4]) ?></strong></dd>
     </dl>
     <?php if ($is_admin == "super") {  ?><a href="<?php echo G5_ADMIN_URL ?>/visit_list.php" class="btn_admin">상세보기</a><?php } ?>
 </section>
