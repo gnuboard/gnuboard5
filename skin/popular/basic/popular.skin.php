6c4353fffe78c51ee70d9d028d9b6b164ae72a3c
thisgun <thisgun@naver.com>
2017-12-04 15:56:59 +0900

    php 7.2 버전에서 오류 나는 코드 수정

--- skin/popular/basic/popular.skin.php
+++ skin/popular/basic/popular.skin.php
@@ -10,9 +10,15 @@ add_stylesheet('<link rel="stylesheet" href="'.$popular_skin_url.'/style.css">',
     <div>
         <h2>인기검색어</h2>
         <ul>
-        <?php for ($i=0; $i<count($list); $i++) {  ?>
-            <li><a href="<?php echo G5_BBS_URL ?>/search.php?sfl=wr_subject&amp;sop=and&amp;stx=<?php echo urlencode($list[$i]['pp_word']) ?>"><?php echo get_text($list[$i]['pp_word']); ?></a></li>
-        <?php }  ?>
+        <?php
+        if( isset($list) && is_array($list) ){
+            for ($i=0; $i<count($list); $i++) {
+            ?>
+                <li><a href="<?php echo G5_BBS_URL ?>/search.php?sfl=wr_subject&amp;sop=and&amp;stx=<?php echo urlencode($list[$i]['pp_word']) ?>"><?php echo get_text($list[$i]['pp_word']); ?></a></li>
+            <?php
+            }   //end for
+        }   //end if
+        ?>
         </ul>
     </div>
 </section>
