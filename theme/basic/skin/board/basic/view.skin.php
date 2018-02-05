f9b82cfbfb737635fa7575e29024cb33d6c114a3
thisgun <thisgun@naver.com>
2018-02-05 17:05:11 +0900

    잘못된 주석 수정

--- theme/basic/skin/board/basic/view.skin.php
+++ theme/basic/skin/board/basic/view.skin.php
@@ -149,7 +149,7 @@ add_stylesheet('<link rel="stylesheet" href="'.$board_skin_url.'/style.css">', 0
 
         <!-- 본문 내용 시작 { -->
         <div id="bo_v_con"><?php echo get_view_thumbnail($view['content']); ?></div>
-        <?php//echo $view['rich_content']; // {이미지:0} 과 같은 코드를 사용할 경우 ?>
+        <?php //echo $view['rich_content']; // {이미지:0} 과 같은 코드를 사용할 경우 ?>
         <!-- } 본문 내용 끝 -->
 
         <?php if ($is_signature) { ?><p><?php echo $signature ?></p><?php } ?>
