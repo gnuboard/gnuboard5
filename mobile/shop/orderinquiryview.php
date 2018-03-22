b8eae0d98e624c624d977052bee2ad64218f2d76
thisgun <thisgun@naver.com>
2018-03-22 12:07:29 +0900

    잘못된 태그 수정

--- mobile/shop/orderinquiryview.php
+++ mobile/shop/orderinquiryview.php
@@ -378,7 +378,6 @@ if($od['od_pg'] == 'lg') {
                         <?php
                         }
                         ?>
-                    <td>
                     </td>
                 </tr>
                 <?php
--- shop/orderinquiryview.php
+++ shop/orderinquiryview.php
@@ -421,7 +421,6 @@ if($od['od_pg'] == 'lg') {
                         <?php
                         }
                         ?>
-                    <td>
                     </td>
                 </tr>
                 <?php
