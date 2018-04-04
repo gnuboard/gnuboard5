619ced14129d30ccde9fa7698eb759be323be62f
thisgun <thisgun@naver.com>
2018-04-04 14:11:46 +0900

    쇼핑몰 로그인 페이지에서 비회원 주문조회 미누락 코드 적용

--- mobile/skin/member/basic/login.skin.php
+++ mobile/skin/member/basic/login.skin.php
@@ -40,6 +40,76 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
 
     </form>
 
+
+    <?php // 쇼핑몰 사용시 여기부터 ?>
+    <?php if ($default['de_level_sell'] == 1) { // 상품구입 권한 ?>
+
+        <!-- 주문하기, 신청하기 -->
+        <?php if (preg_match("/orderform.php/", $url)) { ?>
+
+    <section id="mb_login_notmb" class="mbskin">
+        <h2>비회원 구매</h2>
+
+        <p>
+            비회원으로 주문하시는 경우 포인트는 지급하지 않습니다.
+        </p>
+
+        <div id="guest_privacy">
+            <?php echo $default['de_guest_privacy']; ?>
+        </div>
+
+        <label for="agree">개인정보수집에 대한 내용을 읽었으며 이에 동의합니다.</label>
+        <input type="checkbox" id="agree" value="1">
+
+        <div class="btn_confirm">
+            <a href="javascript:guest_submit(document.flogin);" class="btn_submit">비회원으로 구매하기</a>
+        </div>
+
+        <script>
+        function guest_submit(f)
+        {
+            if (document.getElementById('agree')) {
+                if (!document.getElementById('agree').checked) {
+                    alert("개인정보수집에 대한 내용을 읽고 이에 동의하셔야 합니다.");
+                    return;
+                }
+            }
+            f.url.value = "<?php echo $url; ?>";
+            f.action = "<?php echo $url; ?>";
+            f.submit();
+        }
+        </script>
+    </section>
+
+    <?php } else if (preg_match("/orderinquiry.php$/", $url)) { ?>
+    <div class="mbskin" id="mb_login_od_wr">
+        <h2>비회원 주문조회 </h2>
+
+        <fieldset id="mb_login_od">
+            <legend>비회원 주문조회</legend>
+
+            <form name="forderinquiry" method="post" action="<?php echo urldecode($url); ?>" autocomplete="off">
+
+            <label for="od_id" class="od_id sound_only">주문서번호<strong class="sound_only"> 필수</strong></label>
+            <input type="text" name="od_id" value="<?php echo $od_id; ?>" id="od_id" required class="frm_input required" size="20" placeholder="주문서번호">
+            <label for="id_pwd" class="od_pwd sound_only" >비밀번호<strong class="sound_only"> 필수</strong></label>
+            <input type="password" name="od_pwd" size="20" id="od_pwd" required class="frm_input required" placeholder="비밀번호">
+            <input type="submit" value="확인" class="btn_submit">
+
+            </form>
+        </fieldset>
+
+        <section id="mb_login_odinfo">
+            <p>메일로 발송해드린 주문서의 <strong>주문번호</strong> 및 주문 시 입력하신 <strong>비밀번호</strong>를 정확히 입력해주십시오.</p>
+        </section>
+
+    </div>
+    <?php } ?>
+
+    <?php } ?>
+    <?php // 쇼핑몰 사용시 여기까지 반드시 복사해 넣으세요 ?>
+
+
 </div>
 
 <script>
