b4de423fb370fc6100a508a621017b8a76bcf9e8
thisgun <thisgun@naver.com>
2018-04-09 12:04:51 +0900

    모바일 상품정보 나오지 않는 오류 수정

--- mobile/skin/shop/basic/item.form.skin.php
+++ mobile/skin/shop/basic/item.form.skin.php
@@ -354,7 +354,7 @@ add_stylesheet('<link rel="stylesheet" href="'.G5_SHOP_CSS_URL.'/style.css">', 0
             <?php if ($it['it_explan']) { // 상품 상세설명 ?>
             <h3>상품 상세설명</h3>
             <div id="sit_inf_explan">
-                <?php echo conv_content($it['it_explan'], 1); ?>
+                <?php echo ($it['it_mobile_explan'] ? conv_content($it['it_mobile_explan'], 1) : conv_content($it['it_explan'], 1)); ?>
             </div>
             <?php } ?>
 
