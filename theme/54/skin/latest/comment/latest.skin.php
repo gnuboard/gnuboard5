2acac9987592f1dd185ca1cec67f6003e412f8dd
leebokyung <ipuiz202@naver.com>
2018-12-03 17:04:40 +0900

    최신댓글 수정

--- theme/54/skin/latest/comment/latest.skin.php
+++ theme/54/skin/latest/comment/latest.skin.php
@@ -5,16 +5,21 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 add_stylesheet('<link rel="stylesheet" href="'.$latest_skin_url.'/style.css">', 0);
 ?>
 
-<div class="s_lt">
-    <h2 class="s_lt_title"><a href="<?php echo get_pretty_url($bo_table); ?>">최신댓글</a></h2>
+<div class="cm_lt">
+    <h2 class="cm_lt_title"><a href="<?php echo get_pretty_url($bo_table); ?>">최신댓글</a></h2>
     <ul>
     <?php
     $count = count($list);
     for ($i=0; $i<$count; $i++) {
     ?>
         <li>
-            <a href="<?php echo $list[$i]['href']; ?>" class="over"><?php echo $list[$i]['subject']; ?></a>
-            <span class="lt_date"><?php echo $list[$i]['datetime2'] ?></span>
+            <span class="cm_lt_nick"><?php echo get_member_profile_img($member['mb_id']); ?></span>
+            <div class="cm_lt_info">
+            	<a href="<?php echo $list[$i]['href']; ?>" class="over"><?php echo $list[$i]['subject']; ?></a>
+				<br>
+				<span class="lt_nick"><?php echo $list[$i]['name'] ?></span>
+            	<span class="lt_date"><?php echo $list[$i]['datetime2'] ?></span>              
+            </div>
         </li>
     <?php
     }
