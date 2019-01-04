95c5b5ecd8e87813c95c80ffb15af432ee6d48a1
thisgun <thisgun@naver.com>
2019-01-04 17:46:42 +0900

    코드 수정

--- bbs/delete.php
+++ bbs/delete.php
@@ -30,7 +30,7 @@ else if ($is_admin == 'group') { // 그룹관리자
         alert('자신의 글이 아니므로 삭제할 수 없습니다.');
 } else {
     if ($write['mb_id'])
-        alert('로그인 후 삭제하세요.', './login.php?url='.urlencode(get_pretty_url($bo_table, $wr_id)));
+        alert('로그인 후 삭제하세요.', G5_BBS_URL.'/login.php?url='.urlencode(get_pretty_url($bo_table, $wr_id)));
     else if (!check_password($wr_password, $write['wr_password']))
         alert('비밀번호가 틀리므로 삭제할 수 없습니다.');
 }
