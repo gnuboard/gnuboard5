2e84d5fbf4409fc71e8d0028eb72ee9df45a99f1
thisgun <thisgun@naver.com>
2019-01-04 15:30:05 +0900

    필요없는 구문 수정

--- theme/54/index.php
+++ theme/54/index.php
@@ -19,20 +19,6 @@ include_once(G5_THEME_PATH.'/head.php');
     // 테마의 스킨을 사용하려면 theme/basic 과 같이 지정
     echo latest('theme/pic_list', 'free', 4, 23);
     ?>
-
-    <?php
-    // 이 함수가 바로 최신글을 추출하는 역할을 합니다.
-    // 사용방법 : latest(스킨, 게시판아이디, 출력라인, 글자수);
-    // 테마의 스킨을 사용하려면 theme/basic 과 같이 지정
-    echo latest('theme/pic_list', 'free', 4, 23);
-    ?>
-
-    <?php
-    // 이 함수가 바로 최신글을 추출하는 역할을 합니다.
-    // 사용방법 : latest(스킨, 게시판아이디, 출력라인, 글자수);
-    // 테마의 스킨을 사용하려면 theme/basic 과 같이 지정
-    echo latest('theme/pic_list', 'free', 4, 23);
-    ?>
 </div>
 <div class="latest_wr">
     <!-- 사진 최신글2 { -->
