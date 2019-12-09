581587031d5a48cc1604f3def01825b5bdcd006c
thisgun <thisgun@naver.com>
2019-12-09 11:49:10 +0900

    잘못된 상품정렬 url 코드 수정

--- theme/basic/skin/shop/basic/list.sort.skin.php
+++ theme/basic/skin/shop/basic/list.sort.skin.php
@@ -3,10 +3,12 @@ if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
 
 $sct_sort_href = $_SERVER['SCRIPT_NAME'].'?';
 
-if($ca_id)
-    $sct_sort_href = shop_category_url($ca_id).'?1=1';
-else if($ev_id)
+if($ca_id) {
+	$shop_category_url = shop_category_url($ca_id);
+    $sct_sort_href = (strpos($shop_category_url, '?') === false) ? $shop_category_url.'?1=1' : $shop_category_url;
+} else if($ev_id) {
     $sct_sort_href .= 'ev_id='.$ev_id;
+}
 
 if($skin)
     $sct_sort_href .= '&amp;skin='.$skin;
