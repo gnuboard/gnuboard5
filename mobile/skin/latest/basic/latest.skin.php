54c69dcf3510fb76d23d4eed8dacfaedb2d5bdaf
thisgun <thisgun@naver.com>
2019-12-10 17:04:28 +0900

    회원 사이드 뷰 사용시 최신글에서 사용되는 코드 수정

--- mobile/skin/latest/basic/latest.skin.php
+++ mobile/skin/latest/basic/latest.skin.php
@@ -3,11 +3,14 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 include_once(G5_LIB_PATH.'/thumbnail.lib.php');
 
 add_javascript('<script src="'.G5_JS_URL.'/owlcarousel/owl.carousel.min.js"></script>', 10);
-add_javascript('<script src="'.$latest_skin_url.'/latest.carousel.js"></script>', 11);
 add_stylesheet('<link rel="stylesheet" href="'.G5_JS_URL.'/owlcarousel/owl.carousel.min.css">', 10);
 
+add_javascript('<script src="'.G5_JS_URL.'/tooltipster/tooltipster.bundle.min.js"></script>', 11);
+add_stylesheet('<link rel="stylesheet" href="'.G5_JS_URL.'/tooltipster/tooltipster.bundle.min.css">', 11);
+add_javascript('<script src="'.$latest_skin_url.'/latest.carousel.js?v2"></script>', 12);
+
 // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
-add_stylesheet('<link rel="stylesheet" href="'.$latest_skin_url.'/style.css">', 0);
+add_stylesheet('<link rel="stylesheet" href="'.$latest_skin_url.'/style.css?v2">', 1);
 $thumb_width = 138;
 $thumb_height = 80;
 $list_count = (is_array($list) && $list) ? count($list) : 0;
