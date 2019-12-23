32e024f52ef12bae84b7d4511a026b185ab8bd93
thisgun <thisgun@naver.com>
2019-12-23 10:01:29 +0900

    css js 버전 코드 수정

--- extend/version.extend.php
+++ extend/version.extend.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
-define('G5_JS_VER',  '191110');
-define('G5_CSS_VER', '191110');
+define('G5_JS_VER',  '191202');
+define('G5_CSS_VER', '191202');
 ?>
