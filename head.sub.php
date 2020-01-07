ccfe419bbd8d244ff34fbbe176acbd552df3f186
thisgun <thisgun@naver.com>
2020-01-07 14:34:50 +0900

    54버전 잘못된 hook 이벤트 수정

--- head.sub.php
+++ head.sub.php
@@ -2,8 +2,6 @@
 // 이 파일은 새로운 파일 생성시 반드시 포함되어야 함
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
-run_event('head_sub');
-
 // 테마 head.sub.php 파일
 if(!defined('G5_IS_ADMIN') && defined('G5_THEME_PATH') && is_file(G5_THEME_PATH.'/head.sub.php')) {
     require_once(G5_THEME_PATH.'/head.sub.php');
