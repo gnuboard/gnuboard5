cc7d150c2e3e2316679c746a70d85b66edeae048
minsupkr <minsupkr@naver.com>
2020-03-26 09:32:45 +0900

    PC : 쿠폰 새 창 스크립트 처리

--- theme/basic/shop/mypage.php
+++ theme/basic/shop/mypage.php
@@ -162,14 +162,6 @@ for($k=0; $cp=sql_fetch_array($res); $k++) {
 </div>
 
 <script>
-$(function() {
-    $(".win_coupon").click(function() {
-        var new_win = window.open($(this).attr("href"), "win_coupon", "left=100,top=100,width=700, height=600, scrollbars=1");
-        new_win.focus();
-        return false;
-    });
-});
-
 function member_leave()
 {
     return confirm('정말 회원에서 탈퇴 하시겠습니까?')
