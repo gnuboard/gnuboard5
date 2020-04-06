388392425888fe05e8570af279476f1ab6a9f95f
thisgun <thisgun@naver.com>
2020-04-06 17:56:26 +0900

    5.4 버전 게시판 리스트 스킨 인기게시물 아이콘 위치 수정

--- skin/board/basic/list.skin.php
+++ skin/board/basic/list.skin.php
@@ -135,9 +135,9 @@ add_stylesheet('<link rel="stylesheet" href="'.$board_skin_url.'/style.css">', 0
                     <?php
                     if ($list[$i]['icon_new']) echo "<span class=\"new_icon\">N<span class=\"sound_only\">새글</span></span>";
                     // if ($list[$i]['file']['count']) { echo '<'.$list[$i]['file']['count'].'>'; }
+                    if (isset($list[$i]['icon_hot'])) echo rtrim($list[$i]['icon_hot']);
                     if (isset($list[$i]['icon_file'])) echo rtrim($list[$i]['icon_file']);
                     if (isset($list[$i]['icon_link'])) echo rtrim($list[$i]['icon_link']);
-                    if (isset($list[$i]['icon_hot'])) echo rtrim($list[$i]['icon_hot']);
                     ?>
                     <?php if ($list[$i]['comment_cnt']) { ?><span class="sound_only">댓글</span><span class="cnt_cmt"><?php echo $list[$i]['wr_comment']; ?></span><span class="sound_only">개</span><?php } ?>
                 </div>
