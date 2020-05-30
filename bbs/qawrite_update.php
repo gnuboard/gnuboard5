1b5c4d77c0c81961d5630640a2e525971cea8ef0
leedg <thename@requery.me>
2020-05-30 20:14:08 +0900

    파일 업로드시 파일명 랜덤하게

--- bbs/qawrite_update.php
+++ bbs/qawrite_update.php
@@ -129,7 +129,6 @@ if($file_count > 2)
 @mkdir(G5_DATA_PATH.'/qa', G5_DIR_PERMISSION);
 @chmod(G5_DATA_PATH.'/qa', G5_DIR_PERMISSION);
 
-$chars_array = array_merge(range(0,9), range('a','z'), range('A','Z'));
 
 // 가변 파일 업로드
 $file_upload_msg = '';
@@ -203,11 +202,9 @@ for ($i=1; $i<=count($_FILES['bf_file']['name']); $i++) {
         // 아래의 문자열이 들어간 파일은 -x 를 붙여서 웹경로를 알더라도 실행을 하지 못하도록 함
         $filename = preg_replace("/\.(php|pht|phtm|htm|cgi|pl|exe|jsp|asp|inc)/i", "$0-x", $filename);
 
-        shuffle($chars_array);
-        $shuffle = implode('', $chars_array);
 
         // 첨부파일 첨부시 첨부파일명에 공백이 포함되어 있으면 일부 PC에서 보이지 않거나 다운로드 되지 않는 현상이 있습니다. (길상여의 님 090925)
-        $upload[$i]['file'] = abs(ip2long($_SERVER['REMOTE_ADDR'])).'_'.substr($shuffle,0,8).'_'.replace_filename($filename);
+        $upload[$i]['file'] = get_random_hex_string(32).'_'.replace_filename($filename);
 
         $dest_file = G5_DATA_PATH.'/qa/'.$upload[$i]['file'];
 
