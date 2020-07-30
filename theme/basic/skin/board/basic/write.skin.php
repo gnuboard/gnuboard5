48de34217bcc65f2452a6f9926beea43729d5032
Jake Kwak <jakekwak@gmail.com>
2020-07-30 08:57:17 -0500

    markdown에디터를 사용하기 위한 write.skin.php 수정

--- theme/basic/skin/board/basic/write.skin.php
+++ theme/basic/skin/board/basic/write.skin.php
@@ -31,7 +31,7 @@ add_stylesheet('<link rel="stylesheet" href="'.$board_skin_url.'/style.css">', 0
         }
         if ($is_html) {
             if ($is_dhtml_editor) {
-                $option_hidden .= '<input type="hidden" value="html1" name="html">';
+                $option_hidden .= '<input type="hidden" value="markdown" name="html">';
             } else {
                 $option .= PHP_EOL.'<li class="chk_box"><input type="checkbox" id="html" name="html" onclick="html_auto_br(this);" class="selec_chk" value="'.$html_value.'" '.$html_checked.'>'.PHP_EOL.'<label for="html"><span></span>html</label></li>';
             }
