21db2b46e3a82ac52caa890d17ec9694aea08b28
thisgun <thisgun@naver.com>
2021-02-05 11:40:10 +0900

    회원메일발송 선택삭제가 안되는 오류 수정

--- adm/mail_delete.php
+++ adm/mail_delete.php
@@ -10,10 +10,10 @@ check_admin_token();
 
 $post_count_chk = (isset($_POST['chk']) && is_array($_POST['chk'])) ? count($_POST['chk']) : 0;
 
-if(!$count)
+if(! $post_count_chk)
     alert('삭제할 메일목록을 1개이상 선택해 주세요.');
 
-for($i=0; $i<$count; $i++) {
+for($i=0; $i<$post_count_chk; $i++) {
     $ma_id = isset($_POST['chk'][$i]) ? (int) $_POST['chk'][$i] : 0;
 
     $sql = " delete from {$g5['mail_table']} where ma_id = '$ma_id' ";
