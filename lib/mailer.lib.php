c10f1960750444782a31c895df05511941c6f86c
thisgun <thisgun@naver.com>
2021-04-05 15:51:49 +0900

    php 5.3 버전 이하에서 메일을 보낼수 없는 오류 수정 및 HOOK 이벤트 추가

--- lib/mailer.lib.php
+++ lib/mailer.lib.php
@@ -18,35 +18,48 @@ function mailer($fname, $fmail, $to, $subject, $content, $type=0, $file="", $cc=
 
     $result = run_replace('mailer', $fname, $fmail, $to, $subject, $content, $type, $file, $cc, $bcc);
     
-    if( isset($result['return']) ){
+    if( is_array($result) && isset($result['return']) ){
         return $result['return'];
     }
 
-    $mail = new PHPMailer(); // defaults to using php "mail()"
-    if (defined('G5_SMTP') && G5_SMTP) {
-        $mail->IsSMTP(); // telling the class to use SMTP
-        $mail->Host = G5_SMTP; // SMTP server
-        if(defined('G5_SMTP_PORT') && G5_SMTP_PORT)
-            $mail->Port = G5_SMTP_PORT;
-    }
-    $mail->CharSet = 'UTF-8';
-    $mail->From = $fmail;
-    $mail->FromName = $fname;
-    $mail->Subject = $subject;
-    $mail->AltBody = ""; // optional, comment out and test
-    $mail->msgHTML($content);
-    $mail->addAddress($to);
-    if ($cc)
-        $mail->addCC($cc);
-    if ($bcc)
-        $mail->addBCC($bcc);
-    //print_r2($file); exit;
-    if ($file != "") {
-        foreach ($file as $f) {
-            $mail->addAttachment($f['path'], $f['name']);
+    $mail_send_result = false;
+
+    try {
+        $mail = new PHPMailer(); // defaults to using php "mail()"
+        if (defined('G5_SMTP') && G5_SMTP) {
+            $mail->IsSMTP(); // telling the class to use SMTP
+            $mail->Host = G5_SMTP; // SMTP server
+            if(defined('G5_SMTP_PORT') && G5_SMTP_PORT)
+                $mail->Port = G5_SMTP_PORT;
         }
+        $mail->CharSet = 'UTF-8';
+        $mail->From = $fmail;
+        $mail->FromName = $fname;
+        $mail->Subject = $subject;
+        $mail->AltBody = ""; // optional, comment out and test
+        $mail->msgHTML($content);
+        $mail->addAddress($to);
+        if ($cc)
+            $mail->addCC($cc);
+        if ($bcc)
+            $mail->addBCC($bcc);
+        //print_r2($file); exit;
+        if ($file != "") {
+            foreach ($file as $f) {
+                $mail->addAttachment($f['path'], $f['name']);
+            }
+        }
+
+        $mail = run_replace('mail_options', $mail, $fname, $fmail, $to, $subject, $content, $type, $file, $cc, $bcc);
+
+        $mail_send_result = $mail->send();
+
+    } catch (Exception $e) {
     }
-    return $mail->send();
+
+    run_event('mail_send_result', $mail_send_result, $mail, $to, $cc, $bcc);
+
+    return $mail_send_result;
 }
 
 // 파일을 첨부함
