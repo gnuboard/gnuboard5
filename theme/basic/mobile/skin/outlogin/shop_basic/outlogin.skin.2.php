e14e39248834dd647b27715c073a1a1fac6f097e
kagla <kagla@naver.com>
2021-09-07 15:52:59 +0900

    Scripts may close only the windows that were opened by them. 자신이 연 창만 닫을 수 있는 오류 수정. (ifelse님,210907)

--- theme/basic/mobile/skin/outlogin/shop_basic/outlogin.skin.2.php
+++ theme/basic/mobile/skin/outlogin/shop_basic/outlogin.skin.2.php
@@ -15,7 +15,7 @@ add_stylesheet('<link rel="stylesheet" href="'.$outlogin_skin_url.'/style.css">'
             <a href="<?php echo G5_BBS_URL ?>/member_confirm.php?url=register_form.php" id="ol_after_info"><i class="fa fa-cog" aria-hidden="true"></i><span class="sound_only">정보수정</span></a>
         </span>
         <strong class="nickname"><?php echo $nick ?>님</strong>
-        <a href="<?php echo G5_BBS_URL ?>/point.php" target="_blank" class="point"><strong><?php echo $point ?></strong>  포인트
+        <a href="<?php echo G5_BBS_URL ?>/point.php" target="_blank" class="point win_point"><strong><?php echo $point ?></strong>  포인트
         </a>
         <div id="ol_after_btn">
             <?php if ($is_admin == 'super' || $is_auth) { ?><a href="<?php echo G5_ADMIN_URL ?>" class="btn_admin">관리자</a><?php } ?>
@@ -26,13 +26,18 @@ add_stylesheet('<link rel="stylesheet" href="'.$outlogin_skin_url.'/style.css">'
 
     <ul id="ol_after_private">
         <li id="ol_after_memo">
-            <a href="<?php echo G5_BBS_URL ?>/memo.php" target="_blank">
+            <a href="<?php echo G5_BBS_URL ?>/memo.php" target="_blank" class="win_memo">
                 <i class="fa fa-envelope" aria-hidden="true"></i>쪽지
                 <strong><?php echo $memo_not_read; ?></strong>
             </a>
         </li>
 
-        <li><a href="<?php echo G5_SHOP_URL ?>/coupon.php" target="_blank" class="win_coupon"><i class="fa fa-ticket" aria-hidden="true"></i>쿠폰<strong><?php echo number_format(get_shop_member_coupon_count($member['mb_id'], true)); ?></strong></a></li>
+        <li>
+            <a href="<?php echo G5_SHOP_URL ?>/coupon.php" target="_blank" class="win_coupon">
+                <i class="fa fa-ticket" aria-hidden="true"></i>쿠폰
+                <strong><?php echo number_format(get_shop_member_coupon_count($member['mb_id'], true)); ?></strong>
+            </a>
+        </li>
     </ul>
 
 </aside>
