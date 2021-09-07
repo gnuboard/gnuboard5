d97f0d87c3a6cebd4db0fd81edd38c364eadd16f
seeoya <seeoya@naver.com>
2021-09-07 14:19:25 +0900

    통합인증 : 프론트 2차 작업

--- skin/member/basic/password_lost.skin.php
+++ skin/member/basic/password_lost.skin.php
@@ -6,9 +6,11 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
 ?>
 
 <!-- 회원정보 찾기 시작 { -->
+<!-- #TODO 본인인증 사용 시 아래 div에 cert 클래스 추가 -->
 <div id="find_info" class="new_win">
     <div class="new_win_con">
         <form name="fpasswordlost" action="<?php echo $action_url ?>" onsubmit="return fpasswordlost_submit(this);" method="post" autocomplete="off">
+        <h3>이메일로 찾기</h3>
         <fieldset id="info_fs">
             <p>
                 회원가입 시 등록하신 이메일 주소를 입력해 주세요.<br>
@@ -21,10 +23,22 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
 
         <div class="win_btn">
             <button type="submit" class="btn_submit">인증메일 보내기</button>
-            <!-- #TODO 조건 추가 필요 -->
-            <button type="submit" class="btn_close">본인인증으로 찾기</button>
         </div>
         </form>
+    </div>
+
+    <div class="new_win_con">
+        <h3>본인인증으로 찾기</h3>
+        <div class="cert_btn">
+            <button type="submit" class="btn_close">토스 인증</button>
+            <button type="submit" class="btn_close">PASS 인증</button>
+            <button type="submit" class="btn_close">페이코 인증</button>
+            <button type="submit" class="btn_close">금융인증서</button>
+        </div>
+        <div class="win_btn">
+            <button type="submit" class="btn_submit">휴대폰 본인확인</button>
+            <button type="submit" class="btn_submit">아이핀 본인확인</button>
+        </div>
     </div>
 </div>
 
