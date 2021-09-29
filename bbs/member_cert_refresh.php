0d226ef6fbfde06fe92c0dfcea18e0c6cd5836d8
projectSylas <didldldl@naver.com>
2021-09-29 03:26:39 +0000

    재인증 : 스킨파일명 수정

--- bbs/member_cert_refresh.php
+++ bbs/member_cert_refresh.php
@@ -10,6 +10,6 @@ $g5['title'] = '본인인증을 다시 해주세요.';
 include_once(G5_PATH.'/_head.php');
 
 $action_url = G5_HTTPS_BBS_URL."/member_cert_refresh_update.php";
-include_once($member_skin_path.'/register_cert_reset.skin.php');
+include_once($member_skin_path.'/member_cert_reset.skin.php');
 
 include_once(G5_PATH.'/_tail.php');
