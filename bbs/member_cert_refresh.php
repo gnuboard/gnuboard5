950e3ff6c6295985a9838db10411eb3ce7fd629b
projectSylas <didldldl@naver.com>
2021-10-07 06:08:38 +0000

    재인증 : 조건 위치 변경, 이동페이지 지정

--- bbs/member_cert_refresh.php
+++ bbs/member_cert_refresh.php
@@ -2,13 +2,13 @@
 define('G5_CERT_IN_PROG', true);
 include_once('./_common.php');
 
-if (!empty($member['mb_certify']) && strlen($member['mb_dupinfo']) != 64) { // di로 인증되어 있거나 본인인증이 안된 계정일때
+if(!$is_member) { alert("잘못된 접근입니다.", G5_URL); }
+
+if (!empty($member['mb_certify']) && strlen($member['mb_dupinfo']) != 64) { // 본인인증 안된 계정이거나 ci로 인증된 계정일 경우
     alert("잘못된 접근입니다.", G5_URL);
 }
 
-if(!$is_member) { alert("잘못된 접근입니다.", G5_URL); }
-
-if($config['cf_cert_use'] == 0) alert("본인인증을 이용 할 수 없습니다. 관리자에게 문의 하십시오.");
+if($config['cf_cert_use'] == 0) alert("본인인증을 이용 할 수 없습니다. 관리자에게 문의 하십시오.", G5_URL);
 
 $g5['title'] = '본인인증을 다시 해주세요.';
 include_once(G5_PATH.'/_head.php');
