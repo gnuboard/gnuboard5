7a2c29f19255e1e6c699e481f364f4ff9fbd4307
loves2tu <lupin_latte@naver.com>
2021-12-27 05:56:08 +0000

    구글 판매자센터 피드  - query 수정  - 예외처리 추가

--- shop/price/google_feed.php
+++ shop/price/google_feed.php
@@ -1,28 +1,25 @@
 <?php
 include_once("./_common.php");
 
-$sql =" select *, if((SELECT io_stock_qty FROM `{$g5['g5_shop_item_option_table']}` WHERE it_id = a.it_id GROUP BY it_id HAVING io_stock_qty < 1) != null, 0, 1) as in_stock
-        from `{$g5['g5_shop_item_table']}` as a
-        where it_use = '1' and it_soldout = '0' and it_tel_inq = '0' and it_price > '0' order by ca_id";
+$sql =" SELECT *, (SELECT MIN(`io_stock_qty`) FROM `g5_shop_item_option` WHERE `it_id` = a.`it_id` GROUP BY `it_id`) AS in_stock
+        FROM `{$g5['g5_shop_item_table']}` as a
+        where a.`it_use` = '1' and a.`it_soldout` = '0' and a.`it_tel_inq` = '0' and a.`it_price` > '0' order by a.`ca_id`";
 $result = sql_query($sql);
 
-// $xml = new SimpleXMLElement("<rss version=\"2.0\" xmlns:g=\"http://base.google.com/ns/1.0\"></rss>");
 $xml = new SimpleXMLElement("<rss/>");
 $xml->addAttribute("xmlns:xmlns:g", "http://base.google.com/ns/1.0");
 $xml->addAttribute("version", "2.0");
 $channel = $xml->addChild("channel");
-$title = $channel->addChild("title", "타이틀");
-$link = $channel->addChild("link", "링크");
+$title = $channel->addChild("title", "쇼핑몰피드");
+$link = $channel->addChild("link", G5_URL);
 $description = $channel->addChild("description", "몰?루");
 
 for ($i = 0; $row = sql_fetch_array($result); $i++ ) {
-    $item = $channel->addChild("item");
+    if($row['in_stock'] != null) {
+        if($row['in_stock'] <= 0) continue;
+    }    
     if(empty($row['it_img1'])) continue;
     if(!file_exists(G5_DATA_PATH.'/item/'.$row['it_img1'])) continue;
-
-    $item->addChild("title", $row['it_name']);
-    $item->addChild("link", G5_SHOP_URL.'/item.php?it_id='.urlencode($row['it_id']));
-    $item->addChild("description", strip_tags($row['it_basic']));
 
     $ext = explode('.', $row['it_img1'])[1];
 
@@ -43,10 +40,14 @@ for ($i = 0; $row = sql_fetch_array($result); $i++ ) {
     }
 
     if($ext_check == false) continue;
-    $item->addChild("xmlns:g:image_link", G5_DATA_URL.'/item/'.$row['it_img1']);
-
-    $item->addChild("xmlns:g:condition", "new");
-    $item->addChild("xmlns:g:id", $row['it_id']);
+    
+    $item = $channel->addChild("item");
+    $item->addChild("title", $row['it_name']);
+    $item->addChild("link", G5_SHOP_URL.'/item.php?it_id='.urlencode($row['it_id']));
+    $item->addChild("description", strip_tags($row['it_basic']));
+    $item->addChild("g:g:image_link", G5_DATA_URL.'/item/'.$row['it_img1']);
+    $item->addChild("g:g:condition", "new");
+    $item->addChild("g:g:id", $row['it_id']);
 }
 
 header('Content-type: text/xml'); 
