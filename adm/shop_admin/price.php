2fc24b0746c5e82871f10c8747ca3024dc3f0c85
seeoya <centos@ip-172-26-14-111.ap-northeast-2.compute.internal>
2022-01-03 03:16:32 +0000

    구글 판매자센터 피드 항목 추가

--- adm/shop_admin/price.php
+++ adm/shop_admin/price.php
@@ -33,7 +33,7 @@ $pg_anchor = '<ul class="anchor">
         <p>사이트 명을 클릭하시면 해당 사이트로 이동합니다.</p>
 
         <dl class="price_engine">
-            <dt><a href="http://shopping.naver.com/" target="_blank">네이버 지식쇼핑</a></dt>
+            <dt><a href="http://shopping.naver.com/" target="_blank">네이버쇼핑</a></dt>
             <dd>
                 <ul>
                     <li>입점 안내 : <a href="http://join.shopping.naver.com/join/intro.nhn" target="_blank">http://join.shopping.naver.com/join/intro.nhn</a></li>
@@ -41,6 +41,24 @@ $pg_anchor = '<ul class="anchor">
                     <li>요약상품 URL : <a href="<?php echo G5_SHOP_URL; ?>/price/naver_summary.php" target="_blank"><?php echo G5_SHOP_URL; ?>/price/naver_summary.php</a></li>
                 </ul>
             </dd>
+
+            <dt><a href="" target="_blank">구글 쇼핑</a></dt>
+            <dd>
+                <ul>
+                    <li>구글 Merchant Center : <a href="https://www.google.com/intl/ko_kr/retail/solutions/merchant-center" target="_blank">https://www.google.com/intl/ko_kr/retail/solutions/merchant-center</a></li>
+                    <li>파일 이름 : <a href="<?php echo G5_SHOP_URL; ?>/price/" target="_blank"><?php echo G5_SHOP_URL; ?>/price/</a></li>
+                    <li>파일 URL : <a href="<?php echo G5_SHOP_URL; ?>/price/" target="_blank"><?php echo G5_SHOP_URL; ?>/price/</a></li>
+                </ul>
+            </dd>
+            <dt>Feed 설명</dt>
+            <dd>
+                <ul>
+                    <li>판매국가 <b>대한민국</b>, 언어 <b>한국어</b> 설정 기준입니다.</li>
+                    <li>기본 피드 이름 : 쇼핑몰피드</li>
+                    <li>상품 설명 : <b>it_basic</b> (상품기본설명을 필수 입력해주세요. HTML 태그는 자동 제거됩니다.)</li>
+                </ul>
+            </dd>
+
             <dt><a href="http://shopping.daum.net/" target="_blank">다음 쇼핑하우</a></dt>
             <dd>
                 <ul>
