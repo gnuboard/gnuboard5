27a4a98f2928701158be85b7a78faf4cbca154aa
jeonghs2015 <hsjung201558@gmail.com>
2022-01-20 05:17:45 +0000

    popbill - admin.lib에 사업자번호 추가

--- adm/admin.lib.php
+++ adm/admin.lib.php
@@ -616,6 +616,9 @@ require_once './popbill/PopbillMessaging.php';
   // 비밀키
   $SecretKey = $config['cf_popbill_pw'];
 
+  // 사업자번호
+  $CorpNum = $config['cf_popbill_co_no'];
+
   //통신방식 기본은 CURL , curl 사용에 문제가 있을경우 STREAM 사용가능.
   //STREAM 사용시에는 allow_url_fopen = on 으로 설정해야함.
   define('LINKHUB_COMM_MODE','CURL');
