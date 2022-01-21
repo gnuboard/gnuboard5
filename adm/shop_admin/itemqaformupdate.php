6e87350a8d51d6d08b451d415a612d4102abc679
jeonghs2015 <hsjung201558@gmail.com>
2022-01-21 02:30:10 +0000

    popbill - 상품문의 답변등록시 sms 전송 확인

--- adm/shop_admin/itemqaformupdate.php
+++ adm/shop_admin/itemqaformupdate.php
@@ -21,51 +21,75 @@ if ($w == "u")
     sql_query($sql);
 
     if(trim($iq_answer)) {
-        $sql = " select a.iq_email, a.iq_hp, b.it_name
+        $sql = " select a.iq_email, a.iq_hp,a.iq_name, b.it_name
                     from {$g5['g5_shop_item_qa_table']} a left join {$g5['g5_shop_item_table']} b on ( a.it_id = b.it_id )
                     where a.iq_id = '$iq_id' ";
         $row = sql_fetch($sql);
 
         // SMS 알림
-        if($config['cf_sms_use'] == 'icode' && $row['iq_hp']) {
+        if($config['cf_sms_use'] == 'popbill' || 'icode' && $row['iq_hp']) {
             $sms_content = get_text($row['it_name']).' 상품문의에 답변이 등록되었습니다.';
             $send_number = preg_replace('/[^0-9]/', '', $default['de_admin_company_tel']);
+            $send_name = get_text($default['de_admin_company_name']);
             $recv_number = preg_replace('/[^0-9]/', '', $row['iq_hp']);
-
+            $recv_name = get_text($row['iq_name']);
+            //echo  '문자 내용 : '.$sms_content.'<br>발신자 번호 : '.$send_number.'<br>수신자 번호 : '.$recv_number;
             if($recv_number) {
                 if($config['cf_sms_type'] == 'LMS') {
-                    include_once(G5_LIB_PATH.'/icode.lms.lib.php');
+                    if($config['cf_sms_use']=='icode'){
+                        include_once(G5_LIB_PATH.'/icode.lms.lib.php');
 
-                    $port_setting = get_icode_port_type($config['cf_icode_id'], $config['cf_icode_pw']);
+                        $port_setting = get_icode_port_type($config['cf_icode_id'], $config['cf_icode_pw']);
 
-                    // SMS 모듈 클래스 생성
-                    if($port_setting !== false) {
-                        $SMS = new LMS;
-                        $SMS->SMS_con($config['cf_icode_server_ip'], $config['cf_icode_id'], $config['cf_icode_pw'], $port_setting);
+                        // SMS 모듈 클래스 생성
+                        if($port_setting !== false) {
+                            $SMS = new LMS;
+                            $SMS->SMS_con($config['cf_icode_server_ip'], $config['cf_icode_id'], $config['cf_icode_pw'], $port_setting);
 
-                        $strDest     = array();
-                        $strDest[]   = $recv_number;
-                        $strCallBack = $send_number;
-                        $strCaller   = iconv_euckr(trim($default['de_admin_company_name']));
-                        $strSubject  = '';
-                        $strURL      = '';
-                        $strData     = iconv_euckr($sms_content);
-                        $strDate     = '';
-                        $nCount      = count($strDest);
+                            $strDest     = array();
+                            $strDest[]   = $recv_number;
+                            $strCallBack = $send_number;
+                            $strCaller   = iconv_euckr(trim($default['de_admin_company_name']));
+                            $strSubject  = '';
+                            $strURL      = '';
+                            $strData     = iconv_euckr($sms_content);
+                            $strDate     = '';
+                            $nCount      = count($strDest);
 
-                        $res = $SMS->Add($strDest, $strCallBack, $strCaller, $strSubject, $strURL, $strData, $strDate, $nCount);
+                            $res = $SMS->Add($strDest, $strCallBack, $strCaller, $strSubject, $strURL, $strData, $strDate, $nCount);
 
-                        $SMS->Send();
-                        $SMS->Init(); // 보관하고 있던 결과값을 지웁니다.
+                            $SMS->Send();
+                            $SMS->Init(); // 보관하고 있던 결과값을 지웁니다.
+                        }
+                    }elseif($config['cf_sms_use']=='popbill'){
+                        echo 'hello i\'m popbill';
+                        exit;
                     }
-                } else {
-                    include_once(G5_LIB_PATH.'/icode.sms.lib.php');
+                    } else {
+                        if($config['cf_sms_use']=='icode'){
+                            include_once(G5_LIB_PATH.'/icode.sms.lib.php');
 
-                    $SMS = new SMS; // SMS 연결
-                    $SMS->SMS_con($config['cf_icode_server_ip'], $config['cf_icode_id'], $config['cf_icode_pw'], $config['cf_icode_server_port']);
-                    $SMS->Add($recv_number, $send_number, $config['cf_icode_id'], iconv_euckr(stripslashes($sms_content)), "");
-                    $SMS->Send();
-                }
+                            $SMS = new SMS; // SMS 연결
+                            $SMS->SMS_con($config['cf_icode_server_ip'], $config['cf_icode_id'], $config['cf_icode_pw'], $config['cf_icode_server_port']);
+                            $SMS->Add($recv_number, $send_number, $config['cf_icode_id'], iconv_euckr(stripslashes($sms_content)), "");
+                            $SMS->Send();
+                        }elseif($config['cf_sms_use']=='popbill'){
+                           include_once (G5_ADMIN_PATH.'/popbill/popbill_config.php');
+                            $Messages[] = array(
+                                'snd' => $send_number,		// 발신번호
+                                'sndnm' => $send_name,		// 발신자명
+                                'rcv' => $recv_number,		// 수신번호
+                                'rcvnm' => $recv_name,		// 수신자성명
+                                'msg'	=> $sms_content	    // 개별 메시지 내용
+                            );
+                            try {
+                                $receiptNum = $MessagingService->SendSMS($testCorpNum, $send_number, $sms_content, $Messages, $reserveDT, $adsYN, $LinkID, $send_name, '', $requestNum);
+                            } catch(PopbillException $pe) {
+                                $code = $pe->getCode();
+                                $message = $pe->getMessage();
+                            }                     
+                        }
+                    }
             }
         }
 
