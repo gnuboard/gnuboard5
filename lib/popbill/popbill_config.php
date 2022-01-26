e2152199a1611e151b5f22e93f2c5734d29b7536
jeonghs2015 <hsjung201558@gmail.com>
2022-01-26 08:29:51 +0000

    popbill - config파일 주석 추가

--- lib/popbill/popbill_config.php
+++ lib/popbill/popbill_config.php
@@ -1,17 +1,17 @@
 <?php
     require_once G5_LIB_PATH.'/popbill/PopbillMessaging.php';
 
-    // 링크아이디
+    // 링크아이디 (팝빌 회원가입시 사용한 아이디 - 필수값*)
     $linkid = $config['cf_popbill_id'];
-    //echo '링크아이디 : '.$linkid.'<br>';
+    
 
-    // 비밀키
+    // 비밀키 (팝빌 연동회원 신청시 발급받는 비밀키 - 필수값*)
     $secretkey = $config['cf_popbill_pw'];
-    //echo '비밀키 : '.$secretkey.'<br>';
+    
 
-    // 팝빌 회원 사업자번호, "-"제외 10자리
+    // 팝빌 회원 사업자번호, "-"제외 10자리(팝빌 연동회원 신청시 기입한 사업자번호 - 필수값*)
     $corpnum = $config['cf_popbill_co_no'];
-    //echo '사업자번호 : '.$testCorpNum.'<br>';
+    
 
     //통신방식 기본은 CURL , curl 사용에 문제가 있을경우 STREAM 사용가능.
     //STREAM 사용시에는 allow_url_fopen = on 으로 설정해야함.
