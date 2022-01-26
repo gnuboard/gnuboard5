5408418000983068994bcf2fffe714d1d35ce94e
jeonghs2015 <hsjung201558@gmail.com>
2022-01-26 05:21:29 +0000

    popbill - 주문내역상세 - 결제 상세정보에서 입금문자,배송문자 (소스 간소화, 통일)

--- adm/shop_admin/ordersms.inc.php
+++ adm/shop_admin/ordersms.inc.php
@@ -2,11 +2,11 @@
 if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
 if (!defined("_ORDERSMS_")) exit;
 
-$receive_number = preg_replace("/[^0-9]/", "", $od_hp);	// 수신자번호 (받는사람 핸드폰번호 ... 여기서는 주문자님의 핸드폰번호임)
+$recv_number = preg_replace("/[^0-9]/", "", $od_hp);	// 수신자번호 (받는사람 핸드폰번호 ... 여기서는 주문자님의 핸드폰번호임)
 $send_number = preg_replace("/[^0-9]/", "", $default['de_admin_company_tel']); // 발신자번호
 //popbill 데이터 추가
 $send_name = $default['de_admin_company_name'];
-$receive_name = $od_name;
+$recv_name = $od_name;
 
 
 
@@ -23,18 +23,17 @@ if ($config['cf_sms_use']) {
             $sms_contents = str_replace("{주문번호}", $od_id, $sms_contents);
             $sms_contents = str_replace("{회사명}", $default['de_admin_company_name'], $sms_contents);
             
-            //popbill 문자메시지 전송에 필요함
-            if($receive_number)
-                $Messages[] = array(
-                    'snd'   => $send_number,	    // 발신번호
-                    'sndnm' => $send_name,		    // 발신자명
-                    'rcv'   => $receive_number,	    // 수신번호
-                    'rcvnm' => $receive_name,		// 수신자성명
-                    'msg'	=> $sms_contents	    // 개별 메시지 내용
-                    );
-
-            if($receive_number)
-                $sms_messages[] = array('recv' => $receive_number, 'send' => $send_number, 'cont' => $sms_contents);
+            //icode, popbill 같이 사용하도록 배열의 이름들 수정
+            if($recv_number)
+            //팝빌에서 제공한 sms, lms 전송하는 메소드에서는 수신자의 번호와 이름을 아래의 배열에서 추출함.
+                $sms_messages[] = array('rcv'  => $recv_number,        //수신자번호
+                                        'snd'  => $send_number,        //발신자번호
+                                        'msg'  => $sms_contents,       //개별메시지 내용
+                                        'sndnm' => $send_name,         //발신자이름
+                                        'rcvnm' => $recv_name,         //수신자이름
+                                        'sjt'	=> ''	               //LMS 제목
+                                    );   
+        
         }
     }
 
@@ -49,18 +48,16 @@ if ($config['cf_sms_use']) {
             $sms_contents = str_replace("{주문번호}", $od_id, $sms_contents);
             $sms_contents = str_replace("{회사명}", $default['de_admin_company_name'], $sms_contents);
 
-            //popbill 문자메시지 전송에 필요함
-            if($receive_number)
-                $Messages[] = array(
-                    'snd'   => $send_number,	    // 발신번호
-                    'sndnm' => $send_name,		    // 발신자명
-                    'rcv'   => $receive_number,	    // 수신번호
-                    'rcvnm' => $receive_name,		// 수신자성명
-                    'msg'	=> $sms_contents	    // 개별 메시지 내용
-                    );
-
-            if($receive_number)
-                $sms_messages[] = array('recv' => $receive_number, 'send' => $send_number, 'cont' => $sms_contents);
+            //icode, popbill 같이 사용하도록 배열의 이름들 수정
+            if($recv_number)
+            //팝빌에서 제공한 sms, lms 전송하는 메소드에서는 수신자의 번호와 이름을 아래의 배열에서 추출함.
+                $sms_messages[] = array('rcv'  => $recv_number,        //수신자번호
+                                        'snd'  => $send_number,        //발신자번호
+                                        'msg'  => $sms_contents,       //개별메시지 내용
+                                        'sndnm' => $send_name,         //발신자이름
+                                        'rcvnm' => $recv_name,         //수신자이름
+                                        'sjt'	=> ''	               //LMS 제목
+                                    );   
         }
     }
 
@@ -96,22 +93,14 @@ if ($config['cf_sms_use']) {
                 }
             }elseif($config['cf_sms_use']=='popbill'){
                 include_once (G5_LIB_PATH.'/popbill/popbill_config.php');
-                               
-                    $recv_number = $Messages[$s]['rcv'];
-                    $send_number = $Messages[$s]['snd'];
-                    $sms_contents = $Messages[$s]['msg'];
-                    $send_name = $Messages[$s]['rcvnm'];
-                    
-                    echo $s.'<'.$sms_count.'lms <br>';
                     print_r2($Messages[$s]);
                     try {
-                        $receiptNum = $MessagingService->SendLMS($corpnum, $send_number, '', $sms_contents, $Messages, $reserveDT, $adsYN, $linkid, $send_name, '', $requestNum);
+                        $receiptNum = $MessagingService->SendLMS($corpnum, $send_number, '', $sms_contents, $sms_messages, $reserveDT, $adsYN, $linkid, $send_name, '', $requestNum);
                     }
                     catch (PopbillException $pe) {
                         $code = $pe->getCode();
                         $message = $pe->getMessage();
                     }
-                    echo '주문번호 = '.$receiptNum.'<br>';
                
             }
         } else {
@@ -129,19 +118,14 @@ if ($config['cf_sms_use']) {
                     $SMS->Init(); // 보관하고 있던 결과값을 지웁니다.
             }elseif($config['cf_sms_use']=='popbill'){
                 include_once (G5_LIB_PATH.'/popbill/popbill_config.php');
-                
-                    $recv_number = $Messages[$s]['rcv'];
-                    $send_number = $Messages[$s]['snd'];
-                    $sms_contents = $Messages[$s]['msg'];
-                    $send_name = $Messages[$s]['rcvnm']; 
                     try {
-                        $receiptNum = $MessagingService->SendSMS($corpnum, $send_number, $sms_contents, $Messages, $reserveDT, $adsYN, $linkid, $pop_snd_name, '', $requestNum);
+                        $receiptNum = $MessagingService->SendSMS($corpnum, $send_number, $sms_contents, $sms_messages, $reserveDT, $adsYN, $linkid, $pop_snd_name, '', $requestNum);
                         }
                     catch (PopbillException $pe) {
                         $code = $pe->getCode();
                         $message = $pe->getMessage();
                         }
-                    }              
-                }
+            }              
+        }
     }
 }
