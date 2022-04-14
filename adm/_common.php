d1f03aad1b056f6f049a63f48df1d146dac3a4cb
Cloud User <centos@ip-172-26-14-111.ap-northeast-2.compute.internal>
2022-04-14 03:04:55 +0000

    [G5-75] 다운그레이드  - update.lib.php 수정

--- adm/_common.php
+++ adm/_common.php
@@ -2,7 +2,7 @@
 define('G5_IS_ADMIN', true);
 include_once ('../common.php');
 include_once(G5_ADMIN_PATH.'/admin.lib.php');
-include_once(G5_LIB_PATH.'/upgrade.lib.php');
+include_once(G5_LIB_PATH.'/update.lib.php');
 
 if( isset($token) ){
     $token = @htmlspecialchars(strip_tags($token), ENT_QUOTES);
