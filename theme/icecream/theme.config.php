c288aa40caee7fcaa19e7ad202738cb30fb0ac30
jeonghs2015 <hsjung201558@gmail.com>
2022-04-26 15:52:49 +0900

    [g5-84] - 테마설정 페이지 device 정의 오류 수정

--- theme/icecream/theme.config.php
+++ theme/icecream/theme.config.php
@@ -4,7 +4,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 // 테마가 지원하는 장치 설정 pc, mobile
 // 선언하지 않거나 값을 지정하지 않으면 그누보드5의 설정을 따른다.
 // G5_SET_DEVICE 상수 설정 보다 우선 적용됨
-define('G5_THEME_DEVICE', 'mobile');
+if(! defined('G5_THEME_DEVICE')) define('G5_THEME_DEVICE', '');
 
 $theme_config = array();
 
