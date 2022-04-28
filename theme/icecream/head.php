f6542883a7b38cf97dd1860ddcf7d44507002282
jeonghs2015 <hsjung201558@gmail.com>
2022-04-28 16:54:33 +0900

    [g5-84] - 디버깅 코드 제거

--- theme/icecream/head.php
+++ theme/icecream/head.php
@@ -1,7 +1,5 @@
 <?php
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
-error_reporting(E_ALL);
-    ini_set('display_errors', '1');
 if (G5_IS_MOBILE) {
     include_once(G5_THEME_MOBILE_PATH.'/head.php');
     return;
