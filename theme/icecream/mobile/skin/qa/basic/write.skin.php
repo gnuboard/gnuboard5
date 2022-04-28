022696e7fb343eb86829bb77ad08aff096a492f9
jeonghs2015 <hsjung201558@gmail.com>
2022-04-28 12:02:25 +0900

    [g5-84] - 1:1문의 토근값 누락 오류 수정

--- theme/icecream/mobile/skin/qa/basic/write.skin.php
+++ theme/icecream/mobile/skin/qa/basic/write.skin.php
@@ -13,6 +13,7 @@ add_stylesheet('<link rel="stylesheet" href="'.$qa_skin_url.'/style.css">', 0);
     <input type="hidden" name="sca" value="<?php echo $sca ?>">
     <input type="hidden" name="stx" value="<?php echo $stx ?>">
     <input type="hidden" name="page" value="<?php echo $page ?>">
+    <input type="hidden" name="token" value="<?php echo $token ?>">
     <?php
     $option = '';
     $option_hidden = '';
