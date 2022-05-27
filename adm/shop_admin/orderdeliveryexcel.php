d12c1c84e4afc266141b876e6f55dc52582aca20
kit rio <mario64aq@gmail.com>
2022-05-27 10:18:50 +0900

    불필요한 PHP 5.2 미만 삭제

--- adm/shop_admin/orderdeliveryexcel.php
+++ adm/shop_admin/orderdeliveryexcel.php
@@ -21,7 +21,6 @@ if(! function_exists('column_char')) {
     }
 }
 
-if (phpversion() >= '5.2.0') {
     include_once(G5_LIB_PATH.'/PHPExcel.php');
     
     $headers = array('주문번호', '주문자명', '주문자전화1', '주문자전화2', '배송자명', '배송지전화1', '배송지전화2', '배송지주소', '배송회사', '운송장번호');
@@ -58,47 +57,3 @@ if (phpversion() >= '5.2.0') {
 
     $writer = PHPExcel_IOFactory::createWriter($excel, 'Excel5');
     $writer->save('php://output');
-} else {
-    /*================================================================================
-    php_writeexcel http://www.bettina-attack.de/jonny/view.php/projects/php_writeexcel/
-    =================================================================================*/
-
-    include_once(G5_LIB_PATH.'/Excel/php_writeexcel/class.writeexcel_workbook.inc.php');
-    include_once(G5_LIB_PATH.'/Excel/php_writeexcel/class.writeexcel_worksheet.inc.php');
-
-    $fname = tempnam(G5_DATA_PATH, "tmp-deliverylist.xls");
-    $workbook = new writeexcel_workbook($fname);
-    $worksheet = $workbook->addworksheet();
-
-    // Put Excel data
-    $data = array('주문번호', '주문자명', '주문자전화1', '주문자전화2', '배송자명', '배송지전화1', '배송지전화2', '배송지주소', '배송회사', '운송장번호');
-    $data = array_map('iconv_euckr', $data);
-
-    $col = 0;
-    foreach($data as $cell) {
-        $worksheet->write(0, $col++, $cell);
-    }
-
-    for($i=1; $row=sql_fetch_array($result); $i++) {
-        $row = array_map('iconv_euckr', $row);
-
-        $worksheet->write($i, 0, ' '.$row['od_id']);
-        $worksheet->write($i, 1, $row['od_name']);
-        $worksheet->write($i, 2, ' '.$row['od_tel']);
-        $worksheet->write($i, 3, ' '.$row['od_hp']);
-        $worksheet->write($i, 4, $row['od_b_name']);
-        $worksheet->write($i, 5, ' '.$row['od_b_tel']);
-        $worksheet->write($i, 6, ' '.$row['od_b_hp']);
-        $worksheet->write($i, 7, print_address($row['od_b_addr1'], $row['od_b_addr2'], $row['od_b_addr3'], $row['od_b_addr_jibeon']));
-        $worksheet->write($i, 8, $row['od_delivery_company']);
-        $worksheet->write($i, 9, $row['od_invoice']);
-    }
-
-    $workbook->close();
-
-    header("Content-Type: application/x-msexcel; name=\"deliverylist-".date("ymd", time()).".xls\"");
-    header("Content-Disposition: inline; filename=\"deliverylist-".date("ymd", time()).".xls\"");
-    $fh=fopen($fname, "rb");
-    fpassthru($fh);
-    unlink($fname);
-}
