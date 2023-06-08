c503d23783d66bf150154745731428e0a6ea2751
Kim subi <20subi@jbnu.ac.kr>
2023-06-08 14:51:19 +0900

    menu_form update

--- adm/menu_form.php
+++ adm/menu_form.php
@@ -12,7 +12,7 @@ require_once G5_PATH . '/head.sub.php';
 $new    = isset($_GET['new']) ? clean_xss_tags($_GET['new'], 1, 1) : '';
 $code   = isset($_GET['code']) ? (string)preg_replace('/[^0-9a-zA-Z]/', '', $_GET['code']) : '';
 
-// 코드
+
 if ($new == 'new' || !$code) {
     $code = (int)base_convert(substr($code, 0, 2), 36, 10);
     $code += 36;
@@ -25,7 +25,6 @@ if ($new == 'new' || !$code) {
     <h1><?php echo $g5['title']; ?></h1>
 
     <form name="fmenuform" id="fmenuform" class="new_win_con">
-
         <div class="new_win_desc">
             <label for="me_type">대상선택</label>
             <select name="me_type" id="me_type">
@@ -35,7 +34,6 @@ if ($new == 'new' || !$code) {
                 <option value="content">내용관리</option>
             </select>
         </div>
-
         <div id="menu_result"></div>
 
     </form>
@@ -68,6 +66,7 @@ if ($new == 'new' || !$code) {
                 });
             }
 
+            // 메뉴가 존재하면 menu_exists_tip 띄움
             if (menu_exist) {
                 $(".menu_exists_tip").show();
             } else {
@@ -117,20 +116,23 @@ if ($new == 'new' || !$code) {
         });
     });
 
+    // 선택 누르면 이 함수가 실행
     function add_menu_list(name, link, code) {
         var $menulist = $("#menulist", opener.document);
         var ms = new Date().getTime();
         var sub_menu_class;
+
         <?php if ($new == 'new') { ?>
             sub_menu_class = " class=\"td_category\"";
         <?php } else { ?>
-            sub_menu_class = " class=\"td_category sub_menu_class\"";
+            sub_menu_class = " class=\"td_category sub_menu_class\""; 
         <?php } ?>
 
         var list = "<tr class=\"menu_list menu_group_<?php echo $code; ?>\">";
         list += "<td" + sub_menu_class + ">";
         list += "<label for=\"me_name_" + ms + "\"  class=\"sound_only\">메뉴<strong class=\"sound_only\"> 필수</strong></label>";
         list += "<input type=\"hidden\" name=\"code[]\" value=\"<?php echo $code; ?>\">";
+    
         list += "<input type=\"text\" name=\"me_name[]\" value=\"" + name + "\" id=\"me_name_" + ms + "\" required class=\"required frm_input full_input\">";
         list += "</td>";
         list += "<td>";
@@ -164,7 +166,7 @@ if ($new == 'new' || !$code) {
         list += "</td>";
         list += "<td class=\"td_mng\">";
         <?php if ($new == 'new') { ?>
-            list += "<button type=\"button\" class=\"btn_add_submenu btn_03\">추가</button>\n";
+            list += "<button type=\"button\" class=\"btn_add_menu btn_03\">추가</button>\n";
         <?php } ?>
         list += "<button type=\"button\" class=\"btn_del_menu btn_02\">삭제</button>";
         list += "</td>";
@@ -179,19 +181,19 @@ if ($new == 'new' || !$code) {
 
         if ($menu_last.length > 0) {
             $menu_last.after(list);
-        } else {
-            if ($menulist.find("#empty_menu_list").length > 0)
-                $menulist.find("#empty_menu_list").remove();
+        } else { // 리스트가 없을 때(추가한 게 없을 때)
+            if ($menulist.find("#empty_menu_list").length > 0) // 이미 추가된 게 있으면
+                $menulist.find("#empty_menu_list").remove(); // 메뉴리스트가 비지 않았다 
 
-            $menulist.find("table tbody").append(list);
+            $menulist.find("table tbody").append(list); // 리스트를 붙혀 
         }
 
         $menulist.find("tr.menu_list").each(function(index) {
             $(this).removeClass("bg0 bg1")
                 .addClass("bg" + (index % 2));
         });
-
-        window.close();
+        
+        loacation.replace(location.bref);
     }
 </script>
 
