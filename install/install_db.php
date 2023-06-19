44798a4ef524e5fe8ea6a3a558cc8eef72c03e1e
thisgun <thisgun@naver.com>
2023-06-19 17:20:51 +0900

    설치시 자동생성되는 게시판 글읽기 포인트로 인한 문제 수정 #231

--- install/install_db.php
+++ install/install_db.php
@@ -268,6 +268,18 @@ if ($g5_install || $is_install === false) {
 
         $bo_skin = ($tmp_bo_table[$i] === 'gallery') ? 'gallery' : 'basic';
 
+        if (in_array($tmp_bo_table[$i], array('gallery', 'qa'))) {
+            $read_bo_point = -1;
+            $write_bo_point = 5;
+            $comment_bo_point = 1;
+            $download_bo_point = -20;
+        } else {
+            $read_bo_point = $read_point;
+            $write_bo_point = $write_point;
+            $comment_bo_point = $comment_point;
+            $download_bo_point = $download_point;
+        }
+
         $sql = " insert into `{$table_prefix}board`
                     set bo_table = '$tmp_bo_table[$i]',
                         gr_id = '$tmp_gr_id',
@@ -285,10 +297,10 @@ if ($g5_install || $is_install === false) {
                         bo_count_delete     = '1',
                         bo_upload_level     = '1',
                         bo_download_level   = '1',
-                        bo_read_point       = '-1',
-                        bo_write_point      = '5',
-                        bo_comment_point    = '1',
-                        bo_download_point   = '-20',
+                        bo_read_point       = '$read_bo_point',
+                        bo_write_point      = '$write_bo_point',
+                        bo_comment_point    = '$comment_bo_point',
+                        bo_download_point   = '$download_bo_point',
                         bo_use_category     = '0',
                         bo_category_list    = '',
                         bo_use_sideview     = '0',
