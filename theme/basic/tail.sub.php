da89b5f66bc060cbffc9318308638529af347aaf
dungdang39 <sth0701@icloud.com>
2023-12-01 18:30:59 +0900

    [feat] 팝업페이지 다크모드 유무에따른 기능코드 추가

--- theme/basic/tail.sub.php
+++ theme/basic/tail.sub.php
@@ -7,5 +7,23 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 <?php run_event('tail_sub'); ?>
 
 </body>
+<script>
+  // 다크모드 설정
+  function handleDarkModeChange() {
+    if (localStorage.getItem('darkMode') === 'enabled') {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }
+
+  handleDarkModeChange();
+
+  window.addEventListener('storage', function (event) {
+      handleDarkModeChange();
+  });
+
+
+</script>
 </html>
 <?php echo html_end(); // HTML 마지막 처리 함수 : 반드시 넣어주시기 바랍니다.
