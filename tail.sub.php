93705cb5f0f8bfa1ecc6e78b5ed807b482d2fad2
dungdang39 <sth0701@icloud.com>
2023-12-06 12:45:54 +0900

    [feat] 기본테마에 다크모드 레이아웃 설정

--- tail.sub.php
+++ tail.sub.php
@@ -12,6 +12,25 @@ if(!defined('G5_IS_ADMIN') && defined('G5_THEME_PATH') && is_file(G5_THEME_PATH.
 
 <?php run_event('tail_sub'); ?>
 
+<script>
+  // 다크모드 설정
+  function handleDarkModeChange() {
+    if (localStorage.getItem('darkMode') === 'enabled') {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }
+
+  handleDarkModeChange();
+
+  window.addEventListener('storage', function (event) {
+      handleDarkModeChange();
+  });
+
+
+</script>
+
 </body>
 </html>
 <?php echo html_end(); // HTML 마지막 처리 함수 : 반드시 넣어주시기 바랍니다.
