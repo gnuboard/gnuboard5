aa2f6bf335777ffea3f939d49b98c914d3d5e426
dungdang39 <sth0701@icloud.com>
2024-04-02 10:15:44 +0900

    [feat] tailwind 개별 커스텀 스타일 코드 추가

--- theme/tailwind/tailwind.config.js
+++ theme/tailwind/tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./**/*.html", "./**/*.php"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        header: '#212020',
+        mainborder: '#383838',
+        schbg: '#2c2c2c'
+      },
+      height: {
+        140: "140px",
+      },
+      lineHeight: {
+        11: "55px",
+      },
+      top: {
+        13: "54px",
+      }
+    }
   },
   plugins: [],
 }
