a7c74a843cd35b6a7b6fb7a1171f98a0821aa5a8
dungdang39 <sth0701@icloud.com>
2024-04-04 16:23:19 +0900

    [feat] pic_basic 컴포넌트 tailwind 스타일 적용

--- theme/tailwind/skin/latest/pic_basic/latest.skin.php
+++ theme/tailwind/skin/latest/pic_basic/latest.skin.php
@@ -3,14 +3,13 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 include_once(G5_LIB_PATH.'/thumbnail.lib.php');
 
 // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
-add_stylesheet('<link rel="stylesheet" href="'.$latest_skin_url.'/style.css">', 0);
 $thumb_width = 210;
 $thumb_height = 150;
 ?>
 
-<div class="pic_lt">
-    <h2 class="lat_title"><a href="<?php echo get_pretty_url($bo_table); ?>"><?php echo $bo_subject ?></a></h2>
-    <ul>
+<div class="pic_lt relative overflow-hidden bg-white text-black font-bold border border-solid border-gray-200 mb-5">
+    <h2 class="lat_title block bg-zinc-50 leading-45 text-xs border-b border-solid border-gray-200 px-5"><a href="<?php echo get_pretty_url($bo_table); ?>" class="relative text-blue-600 inline-block after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:bg-blue-600 after:content-['']"><?php echo $bo_subject ?></a></h2>
+    <ul class="flex px-3.5 py-5">
     <?php
     for ($i=0; $i<count($list); $i++) {
     $thumb = get_list_thumbnail($bo_table, $list[$i]['wr_id'], $thumb_width, $thumb_height, false, true);
@@ -23,8 +22,8 @@ $thumb_height = 150;
     }
     $img_content = '<img src="'.$img.'" alt="'.$thumb['alt'].'" >';
     ?>
-        <li>
-            <a href="<?php echo $list[$i]['href'] ?>" class="lt_img"><?php echo $img_content; ?></a>
+        <li class="w-1/5 px-2.5">
+            <a href="<?php echo $list[$i]['href'] ?>" class="lt_img block my-1"><?php echo $img_content; ?></a>
             <?php
             if ($list[$i]['icon_secret']) echo "<i class=\"fa fa-lock\" aria-hidden=\"true\"></i><span class=\"sound_only\">비밀글</span> ";
 
@@ -33,7 +32,7 @@ $thumb_height = 150;
             if ($list[$i]['icon_hot']) echo "<span class=\"hot_icon\">H<span class=\"sound_only\">인기글</span></span>";
 
  
-            echo "<a href=\"".$list[$i]['href']."\"> ";
+            echo "<a href=\"".$list[$i]['href']."\" class=\"hover:text-blue-500\"> ";
             if ($list[$i]['is_notice'])
                 echo "<strong>".$list[$i]['subject']."</strong>";
             else
@@ -55,13 +54,13 @@ $thumb_height = 150;
 
             ?>
 
-            <span class="lt_date"><?php echo $list[$i]['datetime2'] ?></span>
+            <span class="lt_date block text-gray-400 mt-1"><?php echo $list[$i]['datetime2'] ?></span>
         </li>
     <?php }  ?>
     <?php if (count($list) == 0) { //게시물이 없을 때  ?>
     <li class="empty_li">게시물이 없습니다.</li>
     <?php }  ?>
     </ul>
-    <a href="<?php echo get_pretty_url($bo_table); ?>" class="lt_more"><span class="sound_only"><?php echo $bo_subject ?></span><i class="fa fa-plus" aria-hidden="true"></i><span class="sound_only"> 더보기</span></a>
+    <a href="<?php echo get_pretty_url($bo_table); ?>" class="lt_more absolute top-2.5 right-2.5 block w-6 leading-6 text-gray-400 text-center hover:text-gray-600"><span class="sound_only"><?php echo $bo_subject ?></span><i class="fa fa-plus" aria-hidden="true"></i><span class="sound_only"> 더보기</span></a>
 
 </div>
