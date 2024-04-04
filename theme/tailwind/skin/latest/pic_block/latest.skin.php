cf010142c784b72b6ef4df0b7bae28e58a5d1d7a
dungdang39 <sth0701@icloud.com>
2024-04-04 14:07:47 +0900

    [feat] pic_block 컴포넌트 tailwind 스타일 적용

--- theme/tailwind/skin/latest/pic_block/latest.skin.php
+++ theme/tailwind/skin/latest/pic_block/latest.skin.php
@@ -3,15 +3,14 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 include_once(G5_LIB_PATH.'/thumbnail.lib.php');
 
 // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
-add_stylesheet('<link rel="stylesheet" href="'.$latest_skin_url.'/style.css">', 0);
 $thumb_width = 210;
 $thumb_height = 150;
 $list_count = (is_array($list) && $list) ? count($list) : 0;
 ?>
 
-<div class="pic_lt">
-    <h2 class="lat_title"><a href="<?php echo get_pretty_url($bo_table); ?>"><?php echo $bo_subject ?></a></h2>
-    <ul>
+<div class="pic_lt relative bg-white mb-5">
+    <h2 class="lat_title block leading-45 text-sm font-bold text-black px-2.5"><a href="<?php echo get_pretty_url($bo_table); ?>" class="relative inline-block"><?php echo $bo_subject ?></a></h2>
+    <ul class="flex">
     <?php
     for ($i=0; $i<$list_count; $i++) {
     $thumb = get_list_thumbnail($bo_table, $list[$i]['wr_id'], $thumb_width, $thumb_height, false, true);
@@ -25,12 +24,12 @@ $list_count = (is_array($list) && $list) ? count($list) : 0;
     $img_content = '<img src="'.$img.'" alt="'.$thumb['alt'].'" >';
     $wr_href = get_pretty_url($bo_table, $list[$i]['wr_id']);
     ?>
-        <li class="galley_li">
-            <a href="<?php echo $wr_href; ?>" class="lt_img"><?php echo run_replace('thumb_image_tag', $img_content, $thumb); ?></a>
+        <li class="galley_li w-1/4 px-2.5">
+            <a href="<?php echo $wr_href; ?>" class="lt_img block my-1 hover:text-blue-500"><?php echo run_replace('thumb_image_tag', $img_content, $thumb); ?></a>
             <?php
             if ($list[$i]['icon_secret']) echo "<i class=\"fa fa-lock\" aria-hidden=\"true\"></i><span class=\"sound_only\">비밀글</span> ";
 
-            echo "<a href=\"".$wr_href."\"> ";
+            echo "<a href=\"".$wr_href."\" class=\"text-sm font-bold\"> ";
             if ($list[$i]['is_notice'])
                 echo "<strong>".$list[$i]['subject']."</strong>";
             else
@@ -52,16 +51,16 @@ $list_count = (is_array($list) && $list) ? count($list) : 0;
 
             ?>
 
-            <div class="lt_info">
+            <div class="lt_info py-2.5">
 				<span class="lt_nick"><?php echo $list[$i]['name'] ?></span>
-            	<span class="lt_date"><?php echo $list[$i]['datetime2'] ?></span>              
+            	<span class="lt_date text-gray-400"><?php echo $list[$i]['datetime2'] ?></span>              
             </div>
         </li>
     <?php }  ?>
     <?php if ($list_count == 0) { //게시물이 없을 때  ?>
-    <li class="empty_li">게시물이 없습니다.</li>
+    <li class="empty_li w-full">게시물이 없습니다.</li>
     <?php }  ?>
     </ul>
-    <a href="<?php echo get_pretty_url($bo_table); ?>" class="lt_more"><span class="sound_only"><?php echo $bo_subject ?></span>더보기</a>
+    <a href="<?php echo get_pretty_url($bo_table); ?>" class="lt_more absolute block top-2.5 right-2.5 leading-6 text-blue-500 hover:text-blue-950"><span class="sound_only"><?php echo $bo_subject ?></span>더보기</a>
 
 </div>
