5cf7e542ed1e40155ac16fe98327e7015d222553
dungdang39 <sth0701@icloud.com>
2024-04-08 10:18:27 +0900

    [feat] 다크모드 버튼 스타일 적용

--- theme/tailwind/tail.php
+++ theme/tailwind/tail.php
@@ -61,9 +61,15 @@ if(G5_COMMUNITY_USE === false) {
     <button type="button" id="top_btn" class="fixed bottom-5 right-5 w-14 leading-52 border-2 border-solid border-gray-700 text-gray-700 text-center text-sm z-50 bg-white bg-opacity-50 hover:border-blue-600 hover:bg-blue-600 hover:text-white">
     	<i class="fa fa-arrow-up" aria-hidden="true"></i><span class="sound_only">상단으로</span>
     </button>
-    <button type="button" id="darkmode-toggle-switch" class="fixed bottom-5 right-20 w-14 leading-52 border-2 border-solid border-gray-700 text-gray-700 text-center text-sm z-50 bg-white bg-opacity-50 hover:border-blue-600 hover:bg-blue-600 hover:text-white">
-      <i class="fa-solid fa-moon"></i>
-      <i class="fa-solid fa-sun hidden dark:block"></i>
+    <button type="button" id="darkmode-toggle-switch" class="group fixed bottom-5 right-20 flex justify-center items-center w-14 h-14 border-2 border-solid border-gray-700 text-center text-sm z-50 bg-white bg-opacity-50 hover:border-blue-600 hover:bg-blue-600">
+      <svg class="dark:hidden" width="18" height="18" xmlns="http://www.w3.org/2000/svg">
+        <path class="fill-gray-500 group-hover:fill-white" d="M7 0h2v2H7zM12.88 1.637l1.414 1.415-1.415 1.413-1.413-1.414zM14 7h2v2h-2zM12.95 14.433l-1.414-1.413 1.413-1.415 1.415 1.414zM7 14h2v2H7zM2.98 14.364l-1.413-1.415 1.414-1.414 1.414 1.415zM0 7h2v2H0zM3.05 1.706 4.463 3.12 3.05 4.535 1.636 3.12z" />
+        <path class="fill-gray-700 group-hover:fill-white" d="M8 4C5.8 4 4 5.8 4 8s1.8 4 4 4 4-1.8 4-4-1.8-4-4-4Z" />
+      </svg>
+      <svg class="hidden dark:block" width="18" height="18" xmlns="http://www.w3.org/2000/svg">
+        <path class="fill-gray-700 group-hover:fill-white" d="M6.2 1C3.2 1.8 1 4.6 1 7.9 1 11.8 4.2 15 8.1 15c3.3 0 6-2.2 6.9-5.2C9.7 11.2 4.8 6.3 6.2 1Z" />
+        <path class="fill-gray-500 group-hover:fill-white" d="M12.5 5a.625.625 0 0 1-.625-.625 1.252 1.252 0 0 0-1.25-1.25.625.625 0 1 1 0-1.25 1.252 1.252 0 0 0 1.25-1.25.625.625 0 1 1 1.25 0c.001.69.56 1.249 1.25 1.25a.625.625 0 1 1 0 1.25c-.69.001-1.249.56-1.25 1.25A.625.625 0 0 1 12.5 5Z" />
+      </svg>
     </button>
     <script>
     $(function() {
--- theme/tailwind/tailwind.config.js
+++ theme/tailwind/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.html", "./**/*.php"],
-  darkMode: "selector",
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
