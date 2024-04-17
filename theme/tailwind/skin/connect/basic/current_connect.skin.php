3373d592fbecd5ed792504ae617fb1975cd02a1d
dungdang39 <sth0701@icloud.com>
2024-04-17 12:56:12 +0900

    [feat] 현재접속자 다크모드 적용

--- theme/tailwind/skin/connect/basic/current_connect.skin.php
+++ theme/tailwind/skin/connect/basic/current_connect.skin.php
@@ -6,7 +6,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
 <!-- 현재접속자 목록 시작 { -->
 <div id="current_connect">
-    <ul class="list-none m-0 p-0 border-t border-gray-200">
+    <ul class="list-none m-0 p-0 border-t border-gray-200 dark:border-mainborder">
     <?php
     for ($i=0; $i<count($list); $i++) {
         //$location = conv_content($list[$i]['lo_location'], 0);
@@ -16,12 +16,12 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
         if ($list[$i]['lo_url'] && $is_admin == 'super') $display_location = "<a href=\"".$list[$i]['lo_url']."\">".$location."</a>";
         else $display_location = $location;
     ?>
-        <li class="relative flex border-b border-gray-200 box-border p-4">
-            <span class="crt_num leading-45 text-gray-500 font-bold mr-5"><?php echo $list[$i]['num'] ?></span>
+        <li class="relative flex border-b border-gray-200 box-border p-4 dark:border-mainborder">
+            <span class="crt_num leading-45 text-gray-500 font-bold mr-5 dark:text-white"><?php echo $list[$i]['num'] ?></span>
             <span class="crt_profile leading-45 mr-5"><?php echo get_member_profile_img($list[$i]['mb_id']); ?></span>
             <div class="crt_info mt-1">
-            	<span class="crt_name block"><?php echo $list[$i]['name'] ?></span>
-            	<span class="crt_lct block"><?php echo $display_location ?></span>  
+            	<span class="crt_name block dark:text-white"><?php echo $list[$i]['name'] ?></span>
+            	<span class="crt_lct block dark:text-zinc-500"><?php echo $display_location ?></span>  
             </div>
         </li>
     <?php
