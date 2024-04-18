8dd8212bdb25e49cc63266da11601622689e9e76
dungdang39 <sth0701@icloud.com>
2024-04-18 17:49:56 +0900

    [feat] 회원 비밀번호 확인 다크모드 적용

--- theme/tailwind/skin/member/basic/member_confirm.skin.php
+++ theme/tailwind/skin/member/basic/member_confirm.skin.php
@@ -6,33 +6,36 @@ add_stylesheet('<link rel="stylesheet" href="'.$member_skin_url.'/style.css">', 
 ?>
 
 <!-- 회원 비밀번호 확인 시작 { -->
-<div id="mb_confirm" class="mbskin">
-    <h1><?php echo $g5['title'] ?></h1>
+  <div id="mb_confirm_wrap" class="w-full min-h-screen overflow-auto dark:bg-zinc-900">
+    <div id="mb_confirm" class="mbskin">
+      <h1 class="text-white"><?php echo $g5['title'] ?></h1>
 
-    <p>
-        <strong>비밀번호를 한번 더 입력해주세요.</strong>
-        <?php if ($url == 'member_leave.php') { ?>
-        비밀번호를 입력하시면 회원탈퇴가 완료됩니다.
-        <?php }else{ ?>
-        회원님의 정보를 안전하게 보호하기 위해 비밀번호를 한번 더 확인합니다.
-        <?php }  ?>
-    </p>
+      <p class="dark:!border-mainborder">
+          <strong>비밀번호를 한번 더 입력해주세요.</strong>
+          <?php if ($url == 'member_leave.php') { ?>
+          비밀번호를 입력하시면 회원탈퇴가 완료됩니다.
+          <?php }else{ ?>
+          회원님의 정보를 안전하게 보호하기 위해 비밀번호를 한번 더 확인합니다.
+          <?php }  ?>
+      </p>
 
-    <form name="fmemberconfirm" action="<?php echo $url ?>" onsubmit="return fmemberconfirm_submit(this);" method="post">
-    <input type="hidden" name="mb_id" value="<?php echo $member['mb_id'] ?>">
-    <input type="hidden" name="w" value="u">
+      <form name="fmemberconfirm" action="<?php echo $url ?>" onsubmit="return fmemberconfirm_submit(this);" method="post">
+      <input type="hidden" name="mb_id" value="<?php echo $member['mb_id'] ?>">
+      <input type="hidden" name="w" value="u">
 
-    <fieldset>
-        <span class="confirm_id">회원아이디</span>
-        <span id="mb_confirm_id"><?php echo $member['mb_id'] ?></span>
-        <label for="confirm_mb_password" class="sound_only">비밀번호<strong>필수</strong></label>
-        <input type="password" name="mb_password" id="confirm_mb_password" required class="required frm_input" size="15" maxLength="20" placeholder="비밀번호">
-        <input type="submit" value="확인" id="btn_submit" class="btn_submit">
-    </fieldset>
+      <fieldset>
+          <span class="confirm_id">회원아이디</span>
+          <span id="mb_confirm_id" class="dark:text-white"><?php echo $member['mb_id'] ?></span>
+          <label for="confirm_mb_password" class="sound_only">비밀번호<strong>필수</strong></label>
+          <input type="password" name="mb_password" id="confirm_mb_password" required class="required frm_input dark:!bg-zinc-800 dark:border-mainborder dark:text-white" size="15" maxLength="20" placeholder="비밀번호">
+          <input type="submit" value="확인" id="btn_submit" class="btn_submit">
+      </fieldset>
 
-    </form>
+      </form>
 
-</div>
+    </div>
+  </div>
+
 
 <script>
 function fmemberconfirm_submit(f)
