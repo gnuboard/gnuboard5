e7f44e09208b6f367bbd09193346d946d41f6d0c
dungdang39 <sth0701@icloud.com>
2024-04-25 12:36:08 +0900

    [feat] 푸터 접속자집계 tailwind 클래스 적용

--- theme/tailwind/skin/visit/basic/visit.skin.php
+++ theme/tailwind/skin/visit/basic/visit.skin.php
@@ -8,18 +8,18 @@ add_stylesheet('<link rel="stylesheet" href="'.$visit_skin_url.'/style.css">', 0
 ?>
 
 <!-- 접속자집계 시작 { -->
-<section id="visit" class="ft_cnt w-1/4 px-5 xl:block hidden">
-    <h2>접속자집계</h2>
-    <dl>
-        <dt><span></span> 오늘</dt>
-        <dd><strong><?php echo number_format($visit[1]) ?></strong></dd>
-        <dt><span></span> 어제</dt>
-        <dd><strong><?php echo number_format($visit[2]) ?></strong></dd>
-        <dt><span></span> 최대</dt>
-        <dd><strong><?php echo number_format($visit[3]) ?></strong></dd>
-        <dt><span></span> 전체</dt>
-        <dd><strong><?php echo number_format($visit[4]) ?></strong></dd>
+<section id="visit" class="ft_cnt relative w-1/4 px-5 xl:block hidden">
+    <h2 class="relative text-sm text-left text-white mb-5">접속자집계</h2>
+    <dl class="py-3 after:block after:invisible after:clear-both after:content-['']">
+        <dt class="float-left w-1/2 text-left leading-6 h-6 text-gray-200"><span class="inline-block w-1 h-1 rounded-full align-middle bg-blue-600 mr-3"></span> 오늘</dt>
+        <dd class="float-left w-1/2 text-right font-bold leading-6 h-6 px-1"><strong class="inline-block rounded-2xl leading-4 text-white px-1"><?php echo number_format($visit[1]) ?></strong></dd>
+        <dt class="float-left w-1/2 text-left leading-6 h-6 text-gray-200"><span class="inline-block w-1 h-1 rounded-full align-middle bg-blue-600 mr-3"></span> 어제</dt>
+        <dd class="float-left w-1/2 text-right font-bold leading-6 h-6 px-1"><strong class="inline-block rounded-2xl leading-4 text-white px-1"><?php echo number_format($visit[2]) ?></strong></dd>
+        <dt class="float-left w-1/2 text-left leading-6 h-6 text-gray-200"><span class="inline-block w-1 h-1 rounded-full align-middle bg-blue-600 mr-3"></span> 최대</dt>
+        <dd class="float-left w-1/2 text-right font-bold leading-6 h-6 px-1"><strong class="inline-block rounded-2xl leading-4 text-white px-1"><?php echo number_format($visit[3]) ?></strong></dd>
+        <dt class="float-left w-1/2 text-left leading-6 h-6 text-gray-200"><span class="inline-block w-1 h-1 rounded-full align-middle bg-blue-600 mr-3"></span> 전체</dt>
+        <dd class="float-left w-1/2 text-right font-bold leading-6 h-6 px-1"><strong class="inline-block rounded-2xl leading-4 text-white px-1"><?php echo number_format($visit[4]) ?></strong></dd>
     </dl>
-    <?php if ($is_admin == "super") {  ?><a href="<?php echo G5_ADMIN_URL ?>/visit_list.php" class="btn_admin btn"><i class="fa fa-cog fa-spin fa-fw"></i><span class="sound_only">관리자</span></a><?php } ?>
+    <?php if ($is_admin == "super") {  ?><a href="<?php echo G5_ADMIN_URL ?>/visit_list.php" class="btn_admin btn absolute top-0 right-5 h-6 leading-6 rounded px-1"><i class="fa fa-cog fa-spin fa-fw"></i><span class="sound_only">관리자</span></a><?php } ?>
 </section>
-<!-- } 접속자집계 끝 -->
+<!-- } 접속자집계 끝 -->                                                                                                                                                                  
