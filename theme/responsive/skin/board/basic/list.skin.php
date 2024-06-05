0cf644092f1ca72385d3173257cbc1b2addb2b6b
dungdang39 <sth0701@icloud.com>
2024-06-05 09:47:45 +0900

    [fix] 모바일 넓이에서 게시판 번호 제거

--- theme/responsive/skin/board/basic/list.skin.php
+++ theme/responsive/skin/board/basic/list.skin.php
@@ -82,7 +82,7 @@ add_stylesheet('<link rel="stylesheet" href="'.$board_skin_url.'/style.css">', 0
 				</label>
             </th>
             <?php } ?>
-            <th scope="col">번호</th>
+            <th scope="col" class="list_num">번호</th>
             <th scope="col">제목</th>
             <th scope="col">글쓴이</th>
             <th scope="col"><?php echo subject_sort_link('wr_hit', $qstr2, 1) ?>조회 </a></th>
