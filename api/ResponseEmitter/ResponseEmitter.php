8a8d69b50228f3037e8544ac491d4c66567b38d3
kit rio <mario64aq@gmail.com>
2024-09-27 10:36:14 +0900

    [add] REST API > env 의 cors 도메인 설정 활성화
    CORS_ALLOW_ORIGIN,CORS_ALLOW_METHODS,CORS_ALLOW_CREDENTIALS 등록

--- api/ResponseEmitter/ResponseEmitter.php
+++ api/ResponseEmitter/ResponseEmitter.php
@@ -12,17 +12,16 @@ class ResponseEmitter extends SlimResponseEmitter
      */
     public function emit(ResponseInterface $response): void
     {
-        // This variable should be set to the allowed host from which your API can be accessed with
-        $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
-
+        $allow_origin = explode(',', trim($_ENV['CORS_ALLOW_ORIGIN'] ?? ''));
+        $allow_methds = explode(',', trim($_ENV['CORS_ALLOW_METHODS'] ?? ''));
         $response = $response
-            ->withHeader('Access-Control-Allow-Credentials', 'true')
-            ->withHeader('Access-Control-Allow-Origin', $origin)
+            ->withHeader('Access-Control-Allow-Credentials', trim($_ENV['CORS_ALLOW_CREDENTIALS'] ?? 'false'))
+            ->withHeader('Access-Control-Allow-Origin', $allow_origin)
             ->withHeader(
                 'Access-Control-Allow-Headers',
                 'X-Requested-With, Content-Type, Accept, Origin, Authorization',
             )
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+            ->withHeader('Access-Control-Allow-Methods', $allow_methds)
             ->withHeader('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
             ->withAddedHeader('Cache-Control', 'post-check=0, pre-check=0');
 
